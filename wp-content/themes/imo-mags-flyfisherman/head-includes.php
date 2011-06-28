92d152dda9b6f492588543d5294111b8ff7cd839
Jacob Angel <jacob.angel@gmail.com>
2011-06-28 15:57:22 -0400

    Updating flyfisherman.

---
+++ wp-content/themes/imo-mags-flyfisherman/head-includes.php
@@ -0,0 +1,28 @@
+<?php
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+?>
+<!-- +++++++++ IMO MODS ++++++++ -->
+	
+	<!-- FONTS -->
+	<link href='http://fonts.googleapis.com/css?family=Arvo:regular,bold&v1' rel='stylesheet' type='text/css'>
+
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-5816642-20']);
+  _gaq.push(['_setDomainName', 'none']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+_gaq.push(['_setAccount', 'UA-2409437-4']);
+  _gaq.push(['_setDomainName', '.flyfisherman.com']);
+  _gaq.push(['_trackPageview']);
+
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
