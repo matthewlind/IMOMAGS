6e7f4d9b5ac51df043a008157a415b46a8b836ce
Jacob Angel <jacob.angel@gmail.com>
2011-06-28 17:35:36 -0400

    Updated PH for the refactored header subsribe.

---
+++ wp-content/themes/imo-mags-petersenshunting/head-includes.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * head.includes
+ *
+ * All the cool, custom stuff that the site needs.
+*/
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+?>
+
+<!-- +++++++++ IMO MODS ++++++++ -->
+	
+	<!-- FONTS -->
+	<link href='http://fonts.googleapis.com/css?family=Lato:100,regular,bold&v1' rel='stylesheet' type='text/css'>
+
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-5816642-20']);
+  _gaq.push(['_setDomainName', 'none']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+  _gaq.push(['_setAccount', 'UA-2409437-11']);
+  _gaq.push(['_setDomainName', '.petersenshunting.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
