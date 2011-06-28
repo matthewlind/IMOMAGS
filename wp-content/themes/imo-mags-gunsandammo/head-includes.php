899414110cd0ff1de0b22755fa50960e15ba7087
Jacob Angel <jacob.angel@gmail.com>
2011-06-28 16:50:36 -0400

    Turns out we use light text a large majority of the time. Making it default so we can roll back a few customizations.

---
+++ wp-content/themes/imo-mags-gunsandammo/head-includes.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * head-includes.php
+ * sets up all the cool site specific stylings in an easy to find place.i
+*/
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+?>
+	<link href='http://fonts.googleapis.com/css?family=Kameron:regular,bold' rel='stylesheet' type='text/css'>
+	
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-5816642-20']);
+  _gaq.push(['_setDomainName', 'none']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+  _gaq.push(['_setAccount', 'UA-2409437-1']);
+  _gaq.push(['_setDomainName', '.gunsandammo.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
