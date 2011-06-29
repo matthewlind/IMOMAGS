74d850b71160569e279c3bc8235eeb9a735d522f
Jacob Angel <jacob.angel@gmail.com>
2011-06-29 12:06:30 -0400

    Updated for wildfowl.

---
+++ wp-content/themes/imo-mags-wildfowl/head-includes.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * head-includes.php
+ *
+ * all customizations to tspecific theme per site.
+*/
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+?>
+	<!-- FONTS -->
+	<link href='http://fonts.googleapis.com/css?family=EB+Garamond&v1' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Arvo:regular,bold&v1' rel='stylesheet' type='text/css'>
+
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-5816642-20']);
+  _gaq.push(['_setDomainName', 'none']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+  _gaq.push(['_setAccount', 'UA-2409437-17']);
+  _gaq.push(['_setDomainName', '.wildfowlmag.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
