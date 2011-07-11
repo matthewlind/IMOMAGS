18042c817e483264ac8d4497f1cc68448ffc75ad
Jacob Angel <jacob.angel@gmail.com>
2011-07-11 17:19:13 -0400

    Updating module; needs a few pieces of polish, but works.

---
+++ wp-content/plugins/bc_import/bc-import-options.tpl.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * bc-import-options.tpl.php
+ *
+ * variables
+ * $resp - the formatted response from form submissions.
+ */
+?>
+<div class="wrap">
+<style>.red { color:#C00; } .green { color: #0C0;} </style>
+<h2>Brightcove Importer</h2>
+<?php print $resp; ?>
+<form action="" method="post">
+<p><label for="meth">Search 
+<select name="meth" id="meth">
+<?php 
+    $opts = array(
+        "tag"=>"By Tags",
+        "search"=>"By Title",
+        "id"=>"By Id",
+    );
+    foreach ($opts as $option=>$value) {
+        print "<option value='$option'>$value</option>";
+    }
+?>
+</select>
+</label>
+<label for="bc_query">for
+<input name="bc_query" id="bc_query" type='text'/></label> and
+<select name="bc_action" id="bc_action">
+    <option value="preview">Preview</option>
+    <option value="import">Import</option>
+</select>
+</p>
+<p class="submit">
+    <input class="button-primary" value="Submit" type="submit" />
+</p>
+</form>
+</div>
--- wp-content/plugins/bc_import/plugin.php
+++ wp-content/plugins/bc_import/plugin.php
@@ -9,11 +9,13 @@
  */
 
 // Requires the PHP MAPI Wrapper
-require('./bc_mapi/bc-mapi.php');
+require('bc_mapi/bc-mapi.php');
+require('bc-config.php');
+
 
 /**
- * createa  post programatically.
- * @param $title - stri
+ * create a post programatically.
+ * @param $title - string
  * @param $description - string
  * @param $id - integer
  * @param $tags - array of strings
@@ -24,7 +26,7 @@ function _bc_import_create_post($title, $description, $id, $tags=array()){
     global $user_ID;
     $post_content = "http://brightcove=" . $id . " \n " . $description;
 
-    $page = array(
+    $post = array(
         "post_type" => "post",
         "post_content" => $post_content,
         "post_parent" => 0,
@@ -34,9 +36,7 @@ function _bc_import_create_post($title, $description, $id, $tags=array()){
         "tags_input" => $tags,
     );
 
-    $postid = wp_insert_post($post);
-
-    return (bool) $postid;
+    return wp_insert_post($post);
 }
 
 
@@ -48,20 +48,69 @@ function _bc_import_create_post($title, $description, $id, $tags=array()){
  * @return - array of video assets
  */
 function _bc_import_gather_videos($method = 'all', $query){
+    if(empty($query)) {
+        return "";
+    }
     $bc = new BCMAPI(BC_READ_TOKEN, BC_WRITE_TOKEN);
 
     switch($method) {
-        case "tag":
-            break;
-        case "search":
-            break;
-        case "id":
-            break;
-        case "all":
-            break;
-        default:
-            //pass
+    case "tag":
+        $tags = is_array($query) ? implode(",", $query) : $query; 
+        $videos = $bc->search("video", array(), array('all'=>"tag:" . $tags));
+        break;
+    case "search":
+        $videos = $bc->search("video", array(), array('all'=>"display_name:" . $query));
+        break;
+    case "id":
+        // returns a single object, wrapping.
+        $videos = array($bc->find("find_video_by_id", $query));
+        break;
+    case "all":
+        // only grabs the first 100 videos.
+        $videos = $bc->find("find_all_videos");
+        break;
+    default:
+        // pass
     } 
+    return $videos;
 }
 
 
+/**
+ * create an admin page.
+ */
+
+function bc_import_menu(){
+    add_options_page('Brightcove Importer', 'Brightcove Import', "manage_options", "bc_import", "bc_import_options");
+}
+
+function bc_import_options() {
+    if (!current_user_can('manage_options'))  {
+        wp_die( __('You do not have sufficient permissions to access this page.') );
+    }
+    $videos = _bc_import_gather_videos($_POST['meth'], $_POST['bc_query']);
+    if ( isset($_POST['bc_query']) && $_POST['bc_action'] == "import") {
+        $resp="<h3>Videos Imported</h3><ul>";
+        foreach ($videos as $video) {
+            if( $bid = _bc_import_create_post($video->name, $video->shortDescription, $video->id, implode( ',', $video->tags )) )
+            {
+                //success
+                $resp .= "<li class='green'>Added: $video->name with bid $bid</li>";
+            }
+            else {
+                //failure
+                $resp .="<li class='red'>Failure: $video->name</li>";
+            }
+        }
+    }
+    else { // show default action.
+        $resp = "<h3>Videos Found</h3><ul>";
+        foreach ($videos as $video) {
+            $resp .= "<li class='green'> $video->name</li>";
+        }        
+        $resp .="</ul>";
+    }
+    include("bc-import-options.tpl.php");
+}
+
+add_action("admin_menu", "bc_import_menu");
