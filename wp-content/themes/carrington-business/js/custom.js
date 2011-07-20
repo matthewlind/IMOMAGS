e917f06dac39a3bbf70287f8ee48be6ed282687b
Jacob Angel <jacob.angel@gmail.com>
2011-07-20 12:17:20 -0400

    Pulled out inline scripts from footer-default, fixed inclusion mechanism for js/custom.js and corrected Conservation Menu css.

--- wp-content/themes/carrington-business/js/custom.js
+++ wp-content/themes/carrington-business/js/custom.js
@@ -5,22 +5,37 @@
  */
 
 
-(function($){$( function() {
-    /**
-     * Automatically remove default text and replace it if nothing is entered.
-     */
-    $("input[type=text]").each(function(i, o) {
-        var input_default = $(o).val(); 
-        $(o).focus(
-            function() {
-                if($(this).val() == input_default ) {
-                    $(this).val("");
-               }
-            }).blur(
+(function($){
+    $( function() {
+        $("#conservation-link").hover(
+            function () {
+                $('#conservation-lists').fadeIn();
+            },
+            function () {
+            }
+            );
+        $("#conservation-lists").hover(
+            function () {
+            },
+            function () {
+                $('#conservation-lists').fadeOut();
+            }
+            );
+        /**
+         * Automatically remove default text and replace it if nothing is entered.
+         */
+        $("input[type=text]").each(function(i, o) {
+            var input_default = $(o).val(); 
+            $(o).focus(
                 function() {
-                    if($(this).val() == "") {
-                        $(this).val(input_default);
+                    if($(this).val() == input_default ) {
+                        $(this).val("");
                     }
-                });
-    });
-});})(jQuery);
+                }).blur(
+                    function() {
+                        if($(this).val() == "") {
+                            $(this).val(input_default);
+                        }
+                    });
+        });
+    });})(jQuery);
