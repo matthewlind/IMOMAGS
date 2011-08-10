392d96936d0581e772fb1d4dd566780bb9299978
Jacob Angel <jacob.angel@gmail.com>
2011-08-10 15:58:25 -0400

    Fixing up imo-dart to allow finer-grained constrol of ride share ads.

--- wp-content/plugins/imo-dart/AdvertWidget.php
+++ wp-content/plugins/imo-dart/AdvertWidget.php
@@ -7,6 +7,9 @@
  */
 namespace imo;
 class AdvertWidget extends \WP_Widget {
+    public $intervals = array(
+        45, 60, 75, 90, 105, 120, 135, 150, 165, 180
+    );
     public $sizes = array(
         "Box Ad" => "300x250", "Skyscraper" => "160x600", "Leaderboard" => "728x90",
         "Rectangle" => "180x150", "Wide Skyscraper" => "300x600", "Button2" => "120x60",
@@ -26,6 +29,7 @@ class AdvertWidget extends \WP_Widget {
         $instance = $old_instance;
         $instance['size'] = $new_instance['size'];
         $instance['iframe'] = $new_instance['iframe'];
+        $instance['refresh'] = $new_instance['refresh'];
         return $instance;
     }
 
@@ -36,7 +40,7 @@ class AdvertWidget extends \WP_Widget {
     function widget($args, $instance) {
         extract( $args );
         print $before_widget;
-        print imo_dart_tag($instance['size'], $instance['iframe']=="iframe");
+        print imo_dart_tag($instance['size'], $instance['iframe']=="iframe", array("refresh"=>$instance['refresh']));
         print $after_widget;
     }
 
@@ -65,6 +69,23 @@ class AdvertWidget extends \WP_Widget {
 <label><input type="radio" name="<?php echo $this->get_field_name('iframe'); ?>" value="normal" <?php if($instance['iframe']!=="iframe") print "checked='true'"; ?> /> Standard Ad</label><br />
 <label><input type="radio" name="<?php echo $this->get_field_name('iframe'); ?>" value="iframe" <?php if($instance['iframe']=="iframe") print "checked='true'"; ?> /> Ride-Share Ad</label>
 </p> 
+<p>
+<label><strong>Refresh Rate</strong></label>
+
+    <select name="<?php echo $this->get_field_name('refresh'); ?>">
+ <?php 
+        foreach($this->intervals as $key=>$time) {
+            if ($time == $instance['refresh']) {
+                print "<option value='$time' selected='selected' default>";
+            }
+            else {
+                print "<option value='$time'>";
+            }
+            print "$time seconds</option>";
+        }
+?>
+    </select>
+</p>
 <?php
     }
 }
