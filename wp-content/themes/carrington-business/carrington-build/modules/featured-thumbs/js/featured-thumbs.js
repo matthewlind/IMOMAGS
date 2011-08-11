9c18cd863641108fb8dc97e8ad2b50f723232e17
Aaron Baker <aaron.baker@imoutdoors.com>
2011-08-11 15:02:41 -0400

    Small fixes to thumbs feature

--- wp-content/themes/carrington-business/carrington-build/modules/featured-thumbs/js/featured-thumbs.js
+++ wp-content/themes/carrington-business/carrington-build/modules/featured-thumbs/js/featured-thumbs.js
@@ -5,7 +5,7 @@
     $("#loadcover").fadeOut("slow");
     $(".featured-articles").tabs("rotate", 4250);
          
-    $(".featured-articles .tab-content").each(function(i, o){
+    $(".featured-articles .featured-item-pane").each(function(i, o){
         $(o).click(function(){
           window.location=$("h2 > a",this).attr("href");  
         });
