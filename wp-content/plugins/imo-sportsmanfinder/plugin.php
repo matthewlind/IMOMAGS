1f4cc6ead9556abc4ddcda14a6e3350997facfc8
Aaron Baker <aaron.baker@imoutdoors.com>
2011-09-07 14:39:00 -0400

    New style for Channel Finder

--- wp-content/plugins/imo-sportsmanfinder/plugin.php
+++ wp-content/plugins/imo-sportsmanfinder/plugin.php
@@ -25,7 +25,17 @@ class IMOSportsmanWidget extends \WP_Widget {
      * renders administrative form for the widget
      */
     function form($instance) {
-        
+	/* Set up some default widget settings. */
+	$defaults = array( 'header' => __('Guns & Ammo TV is Available on', 'example'),);
+	$instance = wp_parse_args( (array) $instance, $defaults ); ?>
+
+	<!-- Widget Header: Text Input -->
+	<p>
+		<label for="<?php echo $this->get_field_id( 'header' ); ?>"><?php _e('Header:', 'example'); ?></label>
+		<input id="<?php echo $this->get_field_id( 'header' ); ?>" name="<?php echo $this->get_field_name( 'header' ); ?>" value="<?php echo $instance['header']; ?>" style="width:100%;" />
+	</p>
+
+<?php
     }
 
     /**
@@ -33,6 +43,13 @@ class IMOSportsmanWidget extends \WP_Widget {
      * @See WP_Widget:update
      */
     function update($new_instance, $old_instance) {
+	
+		$instance = $old_instance;
+
+
+		$instance['header'] = $new_instance['header'];
+
+		return $instance;
     }
 
     /**
@@ -41,6 +58,9 @@ class IMOSportsmanWidget extends \WP_Widget {
      */
     function widget($args, $instance) {
         extract( $args );
+	
+	$header = $instance['header'];
+	
         print $before_widget;
 ?>
 <script>
@@ -49,10 +69,14 @@ function popwin(loc,winname,w,h,scroll,resize) {
 } // function..popwin
 </script>
 <div id="locateChannel">
-			<h3>Get Sportsman Channel</h3>
-			<input type="text" name="zip" id="zip" onfocus="if(this.value == this.defaultValue) this.value = '';" value="Enter ZIP" class="searchbox" />
-			<a href="#" onclick="javascript:popwin('http://thesportsmanchannel.viewerlink.tv/?zip='+document.getElementById('zip').value,'indicator',615,550,'yes','yes');"><input type="submit" value="GO&raquo;" class="button" /></a>
-		</div>
+    <div id="channelControlsContainer">
+	<h3><?php print $header;?></h3>
+	<div id="channelControls">
+	    <input type="text" name="zip" id="zip" onfocus="if(this.value == this.defaultValue) this.value = '';" value="Enter Your ZIP" class="searchbox" />
+	    <a href="#" onclick="javascript:popwin('http://thesportsmanchannel.viewerlink.tv/?zip='+document.getElementById('zip').value,'indicator',615,550,'yes','yes');"><input type="submit" value="GO!" class="button" /></a>
+	</div>
+    </div>
+</div>
 
 <?php
         print $after_widget;
