1560c757b182a241edffb88eca2c3c7cc8fc09cd
Aaron Baker <aaron.baker@imoutdoors.com>
2011-09-27 17:18:19 -0400

    Small fix for jetpack on wildfowl

--- wp-content/themes/imo-mags-wildfowl/functions.php
+++ wp-content/themes/imo-mags-wildfowl/functions.php
@@ -3,7 +3,7 @@
  * functions.php 
  */
 
-#define("JETPACK_SITE", "handgunsmag");
+define("JETPACK_SITE", "wildfowlmag");
 define("SUBS_LINK", "https://secure.palmcoastd.com/pcd/eSv?iMagId=014HT&i4Ky=IBZN");
 define("GIFT_LINK", "https://secure.palmcoastd.com/pcd/eSv?iMagId=014HT&i4Ky=IGZN");
 define("SERVICE_LINK", "https://secure.palmcoastd.com/pcd/eServ?iServ=MDE0SFQ0NDY5OCZpVHlwZT1FTlRFUg==");
