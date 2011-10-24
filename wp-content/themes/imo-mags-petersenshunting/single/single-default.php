8643eac29749bcc9b49e377825f4a02563efa23e
Aaron Baker <aaron.baker@imoutdoors.com>
2011-10-24 17:45:59 -0400

    Lil bug fix

--- wp-content/themes/imo-mags-petersenshunting/single/single-default.php
+++ wp-content/themes/imo-mags-petersenshunting/single/single-default.php
@@ -27,40 +27,32 @@ get_header();
 
 $current_category = single_cat_title("", false);
 
-$dirtylens_slug = "dirty-lens";
-$dd_slug = "deadly-draw";
-$gc_slug = "gun-crush";
-$uh_slug = "urban-huntress";
-$eal_slug = "editor-at-large";
-$fg_slug = "fare-game";
-$bw_slug = "buck-wild";
-$nb_slug = "news-brief";
 
 ?>
 
-<?php if (in_category($dl_slug)) { ?>
-<a href="/blogs/<?php echo $dl_slug; ?>/" title="<?php echo $current_category; ?>"><div class="dl-header-mini"></div></a>
+<?php if (in_category("the-dirty-lens")) { ?>
+<a href="/blogs/<?php echo "the-dirty-lens"; ?>/" title="<?php echo $current_category; ?>"><div class="dl-header-mini"></div></a>
 
-<?php } else if (in_category($dd_slug)) { ?>
-<a href="/blogs/<?php echo $dd_slug; ?>/" title="<?php echo $current_category; ?>"><div class="dd-header-mini"></div></a>
+<?php } else if (in_category("deadly-passion")) { ?>
+<a href="/blogs/<?php echo "deadly-passion"; ?>/" title="<?php echo $current_category; ?>"><div class="dd-header-mini"></div></a>
 
-<?php } else if (in_category($gc_slug)) { ?>
-<a href="/blogs/<?php echo $gc_slug; ?>/" title="<?php echo $current_category; ?>"><div class="gc-header-mini"></div></a>
+<?php } else if (in_category("the-guns-crush")) { ?>
+<a href="/blogs/<?php echo "the-guns-crush"; ?>/" title="<?php echo $current_category; ?>"><div class="gc-header-mini"></div></a>
 
-<?php } else if (in_category($uh_slug)) { ?>
-<a href="/blogs/<?php echo $uh_slug; ?>/" title="<?php echo $current_category; ?>"><div class="uh-header-mini"></div></a>
+<?php } else if (in_category("the-urban-huntress")) { ?>
+<a href="/blogs/<?php echo "the-urban-huntress"; ?>/" title="<?php echo $current_category; ?>"><div class="uh-header-mini"></div></a>
 
-<?php } else if (in_category($eal_slug)) { ?>
-<a href="/blogs/<?php echo $eal_slug; ?>/" title="<?php echo $current_category; ?>"><div class="eal-header-mini"></div></a>
+<?php } else if (in_category("editor-at-large")) { ?>
+<a href="/blogs/<?php echo "editor-at-large"; ?>/" title="<?php echo $current_category; ?>"><div class="eal-header-mini"></div></a>
 
-<?php } else if (in_category($fg_slug)) { ?>
-<a href="/blogs/<?php echo $fg_slug; ?>/" title="<?php echo $current_category; ?>"><div class="fg-header-mini"></div></a>
+<?php } else if (in_category("fare-game")) { ?>
+<a href="/blogs/<?php echo "fare-game"; ?>/" title="<?php echo $current_category; ?>"><div class="fg-header-mini"></div></a>
 
-<?php } else if (in_category($bw_slug)) { ?>
-<a href="/blogs/<?php echo $bw_slug; ?>/" title="<?php echo $current_category; ?>"><div class="bw-header-mini"></div></a>
+<?php } else if (in_category("buck-wild")) { ?>
+<a href="/blogs/<?php echo "buck-wild"; ?>/" title="<?php echo $current_category; ?>"><div class="bw-header-mini"></div></a>
 
-<?php } else if (in_category($nb_slug)) { ?>
-<a href="/blogs/<?php echo $nb_slug; ?>/" title="<?php echo $current_category; ?>"><div class="nb-header-mini"></div></a>
+<?php } else if (in_category("news-brief")) { ?>
+<a href="/blogs/<?php echo "news-brief"; ?>/" title="<?php echo $current_category; ?>"><div class="nb-header-mini"></div></a>
 
 <?php } ?>
 
