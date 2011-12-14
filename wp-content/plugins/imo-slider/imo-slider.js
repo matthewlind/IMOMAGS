a8f129273df6ba949873731b3496ff522bed54c8
Aaron Baker <aaron.baker@imoutdoors.com>
2011-12-14 12:54:25 -0500

    IMO slider goodness (cherry picked from commit 88382c5346d5726265c21cb4aef1db4c6976379a)

--- wp-content/plugins/imo-slider/imo-slider.js
+++ wp-content/plugins/imo-slider/imo-slider.js
@@ -1,28 +1,13 @@
-;(function($) {$(function() {
 
 
 
-    $(document).ready(function () {
-      $('#scroll').buffet({
-        scroll_by : 2,
+    jQuery(document).ready(function () {
+      jQuery('#scroll').buffet({
+        scroll_by : 4,
         speed     : 400,
         easing    : 'easeOutExpo',
-        next      : $('#next'),
-        prev      : $('#prev'),
-        before : function () {
-          console.log("before_" + this);
-        },
-        after : function () {
-          console.log("after_" + this);
-        }
+        next      : jQuery('#next'),
+        prev      : jQuery('#prev'),
       });
     });  
-
-
-});
-})(jQuery);
-
-
-
-
 
