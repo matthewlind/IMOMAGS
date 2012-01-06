5be5e1d0227435ec7efe084bcc811a5a7de6a36e
Aaron Baker <aaron.baker@imoutdoors.com>
2012-01-06 15:16:35 -0500

    Fix for google analytics on mobile page template

--- wp-content/themes/carrington-business/pages/mobile-page.php
+++ wp-content/themes/carrington-business/pages/mobile-page.php
@@ -26,6 +26,26 @@ the_post();
 		<title><?php the_title(); ?></title>
 		<link rel='stylesheet' id='mobile-css'  href='/wp-content/themes/carrington-business/css/mobile-page.css' type='text/css' media='all' />
 		<meta name="viewport" content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
+			<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-5816642-20']);
+  _gaq.push(['_setDomainName', 'none']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+
+  _gaq.push(['_setAccount', 'UA-12158262-1']);
+  _gaq.push(['_setDomainName', 'none']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 			
 	</head>
 	<body>
