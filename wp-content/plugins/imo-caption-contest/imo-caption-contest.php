566c8aec709b788e2c70f3e93d8b4782e51ec1d6
Aaron Baker <aaron.baker@imoutdoors.com>
2012-01-11 11:10:40 -0500

    Caption contest now has a field for uploading product images

--- wp-content/plugins/imo-caption-contest/imo-caption-contest.php
+++ wp-content/plugins/imo-caption-contest/imo-caption-contest.php
@@ -59,6 +59,129 @@ function imo_caption_contest_init() {
 	  ); 
 	  register_post_type('imo_caption_contest',$args);
 	  flush_rewrite_rules();
+
+        if ( !class_exists( 'RWMB_Caption_Field' ) ) {
+        class RWMB_Caption_Field {
+
+          /**
+           * Add default value for 'taxonomy' field
+           * @param $field
+           * @return array
+           */
+          static function normalize_field( $field ) {
+            // Default query arguments for get_terms() function
+            $default_args = array(
+              'hide_empty' => false
+            );
+            if ( !isset( $field['options']['args'] ) )
+              $field['options']['args'] = $default_args;
+            else
+              $field['options']['args'] = wp_parse_args( $field['options']['args'], $default_args );
+
+            // Show field as checkbox list by default
+            if ( !isset( $field['options']['type'] ) )
+              $field['options']['type'] = 'checkbox_list';
+
+            // If field is shown as checkbox list, add multiple value
+            if ( 'checkbox_list' == $field['options']['type'] )
+              $field['multiple'] = true;
+
+            return $field;
+          }
+
+          /**
+           * Get field HTML
+           * @param $html
+           * @param $field
+           * @param $meta
+           * @return string
+           */
+          static function html( $html, $meta, $field ) {
+            global $post;
+
+            $options = $field['options'];
+
+            $meta = wp_get_post_terms( $post->ID, $options['taxonomy'], array( 'fields' => 'ids' ) );
+            $meta = is_array( $meta ) ? $meta : ( array ) $meta;
+            $terms = get_terms( $options['taxonomy'], $options['args'] );
+
+            $html = '';
+            // Checkbox_list
+            if ( 'checkbox_list' == $options['type'] ) {
+              foreach ( $terms as $term ) {
+                $html .= "<input type='checkbox' name='{$field['id']}[]' value='{$term->term_id}'" . checked( in_array( $term->term_id, $meta ), true, false ) . " /> {$term->name}<br/>";
+              }
+            }
+            // Select
+            else {
+              $html .= "<select name='{$field['id']}" . ( $field['multiple'] ? "[]' multiple='multiple' style='height: auto;'" : "'" ) . ">";
+              foreach ( $terms as $term ) {
+                $html .= "<option value='{$term->term_id}'" . selected( in_array( $term->term_id, $meta ), true, false ) . ">{$term->name}</option>";
+              }
+              $html .= "</select>";
+            }
+
+            return $html;
+          }
+
+          /**
+           * Save post taxonomy
+           * @param $post_id
+           * @param $field
+           * @param $old
+           * @param $new
+           */
+          static function save( $new, $old, $post_id, $field ) {
+            wp_set_post_terms( $post_id, $new, $field['options']['taxonomy'] );
+          }
+        }
+      }
+
+
+      /********************* META BOXES DEFINITION ***********************/
+
+    /**
+     * Prefix of meta keys (optional)
+     * Wse underscore (_) at the beginning to make keys hidden
+     * You also can make prefix empty to disable it
+     */
+    $prefix = '';
+
+    $meta_boxes = array( );
+
+
+    // Second meta box
+    $meta_boxes[] = array(
+      'id' => 'product_photo',
+      'title' => 'Upload Product Photo',
+      'pages' => array( 'imo_caption_contest' ),
+
+      'fields' => array(
+
+        array(
+          'name' => 'Product Photo',
+          'desc' => 'Photo of the Featured Product',
+          'id' => $prefix . 'product',
+          'type' => 'image'                // Field type: image upload
+        )
+      )
+    );
+
+    /**
+     * Register meta boxes
+     * Make sure there's no errors when the plugin is deactivated or during upgrade
+     */
+
+    if ( class_exists( 'RW_Meta_Box' ) ) {
+      foreach ( $meta_boxes as $meta_box ) {
+        new RW_Meta_Box( $meta_box );
+      }
+    }
+
+
+
+
+
 }
 
 function imo_caption_contest_flush() {
@@ -89,6 +212,10 @@ function imo_caption_contest_add_custom_box() {
         'normal',
         'high' 
     );
+
+
+
+    
 
 
 }
