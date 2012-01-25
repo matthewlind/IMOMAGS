d45ae3959362f48ecc7afcebadf7e014f0ea467f
Aaron Baker <aaron.baker@imoutdoors.com>
2012-01-25 10:58:34 -0500

    Adds additional legacy content type

--- wp-content/plugins/imo-legacy-posts/imo-legacy-posts.php
+++ wp-content/plugins/imo-legacy-posts/imo-legacy-posts.php
@@ -17,11 +17,50 @@ License: IMO
 
 
 add_action('init', 'imo_legacy_post_init');
+add_action('init', 'imo_more_legacy_init');
+
+
+function imo_more_legacy_init() {
+	$labels = array(
+		'name' => _x('Legacy Articles 2', 'post type general name'),
+		'singular_name' => _x('More Legacy', 'post type singular name'),
+		'add_new' => _x('Add New', 'More Legacy'),
+		'add_new_item' => __("Add New More Legacy"),
+		'edit_item' => __("Edit More Legacy"),
+		'new_item' => __("New More Legacy"),
+		'view_item' => __("View More Legacy"),
+		'search_items' => __("Search More Legacys"),
+		'not_found' =>  __('No More Legacys found'),
+		'not_found_in_trash' => __('No More Legacys found in Trash'), 
+		'parent_item_colon' => ''
+	  );
+	  $args = array(
+		'labels' => $labels,
+		'public' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true, 
+		'query_var' => true,
+		'capability_type' => 'post',
+		'hierarchical' => false,
+		'menu_position' => null,
+		'supports' => array('title','thumbnail','excerpt','editor','author'),
+		'rewrite' => array('slug' => 'article',FALSE),
+		'taxonomies' => array('blog_tax','post_tag','activity','location','gear','species'),
+	  ); 
+	register_post_type('imo_more_legacy',$args);
+
+
+    flush_rewrite_rules();
+}
+
 
 function imo_legacy_post_init() {
 
 	wp_register_script('imo-legacy-posts', plugins_url('imo-legacy-posts.js', __FILE__), array('jquery'), '1.0');
 	wp_enqueue_script('imo-legacy-posts');
+
+
+
 
 
 	$labels = array(
@@ -51,6 +90,14 @@ function imo_legacy_post_init() {
 		'taxonomies' => array('blog_tax','post_tag','activity','location','gear','species','category','post_family'),
 	  ); 
 	  register_post_type('imo_legacy_post',$args);
+
+
+	
+
+
+
+
+
     flush_rewrite_rules();
 }
 
