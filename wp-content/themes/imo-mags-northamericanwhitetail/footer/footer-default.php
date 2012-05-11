6b3baeccea39e9e440d1ea7196ace92c8ce58107
Fox Bowden <fox.bowden@imoutdoors.com>
2012-05-11 11:47:19 -0400

    site relaunch

---
+++ wp-content/themes/imo-mags-northamericanwhitetail/footer/footer-default.php
@@ -0,0 +1,160 @@
+<?php
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+
+// For cross-blog queries
+global $switched;
+$args = array(
+  'post_type'			  =>	'post',
+	'posts_per_page'	=>	3,
+	'orderby'			    =>	'date',
+	'order'				    =>	'DESC'
+); ?>
+
+</section><!-- .container -->
+
+  <footer id="footer">
+    
+      <section id="imo-network">
+        <div class="container">
+          <!-- <h4><span class="white">Guns &amp; Ammo</span> Shooting Network <span class="tail"></span></h4> -->
+          <div id="mag-bowhunter" class="mag first">
+            <a class="site-link" href="http://www.bowhunter.com/" title="Visit www.bowhunter.com">
+              <img class="cover" src="<?php print get_stylesheet_directory_uri(); ?>/img/mags_bowhunter.jpg" alt />
+              <h5>Bowhunter</h5>
+            </a>
+            <?php switch_to_blog(3);
+            $blog = new WP_Query($args);
+            if ($blog->have_posts()) :?>
+            <ul class="recent-posts">
+              <?php while ($blog->have_posts()) : $blog->the_post(); ?>
+              <li class="date"><?php the_date('F jS'); ?></li>
+              <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+              <?php endwhile ?>
+            </ul>
+            <?php endif; restore_current_blog(); ?>
+          </div>
+          <div id="mag-gf" class="mag">
+            <a class="site-link" href="http://www.gameandfishmag.com/" title="Visit www.gameandfishmag.com">
+              <img class="cover" src="<?php print get_stylesheet_directory_uri(); ?>/img/mags_gf.jpg" alt />
+              <h5>Game & Fish Illinois</h5>
+            </a>
+            <?php switch_to_blog(14);
+            $blog = new WP_Query($args);
+            if ($blog->have_posts()) :?>
+            <ul class="recent-posts">
+              <?php while ($blog->have_posts()) : $blog->the_post(); ?>
+              <li class="date"><?php the_date('F jS'); ?></li>
+              <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+              <?php endwhile ?>
+            </ul>
+            <?php endif; restore_current_blog(); ?>
+          </div>
+          <div id="mag-petersen" class="mag">
+            <a class="site-link" href="http://www.petersenshunting.com/" title="Visit www.petersenshunting.com">
+              <img class="cover" src="<?php print get_stylesheet_directory_uri(); ?>/img/mags_petersen.jpg" alt />
+              <h5>Petersen's Hunting</h5>
+            </a>
+            <?php switch_to_blog(7);
+            $blog = new WP_Query($args);
+            if ($blog->have_posts()) :?>
+            <ul class="recent-posts">
+              <?php while ($blog->have_posts()) : $blog->the_post(); ?>
+              <li class="date"><?php the_date('F jS'); ?></li>
+              <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+              <?php endwhile ?>
+            </ul>
+            <?php endif; restore_current_blog(); ?>
+          </div>
+                 
+          <div style="clear:both"></div>
+        
+                
+		
+     <script type="text/javascript" src="/wp-content/themes/imo-mags-gunsandammo/js/jquery.jcarousel.js"></script>
+     <script type="text/javascript">
+ 		$(document).ready(function(){
+
+ 			jQuery('#slides-footer').jcarousel({
+ 				easing: 'easeOutBack',
+ 				animation:1000});	
+			
+ 		});
+	 </script>
+      <div class="intermedia-network">
+            	<div class="network-title">THE INTERMEDIA OUTDOORS NETWORK</div>
+                <div class="otd-networkblock">
+                	 <div class="slides-container-f">
+                        <ul id="slides-footer" class="jcarousel-skin-tango">
+                        	<li><a href="http://www.thesportsmanchannel.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/sportsman_channel.png" alt="" /></a></li>
+							<li><a href="http://www.petersenshunting.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/hunting.png" alt="" /></a></li>
+							<li><a href="http://www.floridasportsman.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/florida_sportsman.png" alt="" /></a></li>
+							<li><a href="http://www.flyfisherman.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/fly_fisherman.png" alt="" /></a></li>
+							<li><a href="http://www.gunsandammo.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/gunsandammo.png" alt="" /></a></li>
+							
+							<li><a href="http://www.in-fisherman.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/in_fisherman.png" alt="" /></a></li>
+							<li><a href="http://www.gameandfishmag.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/game_and_fish.png" alt="" /></a></li>
+							<li><a href="http://www.gundogmag.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/gun_dog.png" alt="" /></a></li>
+							<li><a href="http://www.handgunsmag.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/handguns.png" alt="" /></a></li>
+							<li><a href="http://www.bowhunter.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/bowhunter.png" alt="" /></a></li>
+							<li><a href="http://www.bowhuntingmag.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/bowhunting.png" alt="" /></a></li>
+							
+							<li><a href="http://www.rifleshootermag.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/rifleshooter.png" alt="" /></a></li>
+							<li><a href="http://www.shootingtimes.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/shooting.png" alt="" /></a></li>
+							<li><a href="http://www.shotgunnews.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/shotgun_news.png" alt="" /></a></li>
+							
+							<li><a href="http://www.northamericanwhitetail.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/whitetail.png" alt="" /></a></li>
+							<li><a href="http://www.wildfowlmag.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/wildfowl.png" alt="" /></a></li>
+							 <li><a href="http://www.bassfan.com/"><img src="/wp-content/themes/imo-mags-gunsandammo/img/imo_logos/new/bassfan.png" alt="" /></a></li>
+							 							<li><a href="http://store.intermediaoutdoors.com/"><img src="http://www.gunsandammo.com/wp-content/themes/carrington-business/imo-footer/images/imo-store.png" alt="" /></a></li>
+                        </ul>
+                      </div>    
+                </div>
+            </div>
+
+        </div>
+      </section>
+      <section class="stay-connected">
+        <div class="container">
+          
+          <div class="top">
+            <h4>Stay Connected</h4>
+            <ul class="connections">
+              <li class="facebook"><a href="http://www.facebook.com/GunsAndAmmoMag" title="Find us on Facebook">Facebook</a></li>
+              <li class="twitter"><a href="http://twitter.com/gunsandammomag" title="Follow us on Twitter">Twitter</a></li>
+              <li class="news"><a href="/newsletter/">Newsletter</a></li>
+              <li class="apps"><a href="/apps/">Apps</a></li>
+              <li class="mags"><a href="https://secure.palmcoastd.com/pcd/eSv?iMagId=0145V&i4Ky=IBZN">Get the Magazine</a></li>
+            </ul>
+          </div>
+        
+          <hr />
+          <div class="extras">
+           		Copyright &copy; <?php echo date('Y'); ?>, Intermedia Outdoors. All Rights Reserved.</div>
+          	<div class="bottom">
+         
+            <div class="utility"> 
+              <a href="http://www.imoutdoorsmedia.com/IM3/" title="">About</a>
+              <a href="#">Conservation Partners</a>
+              <a href="http://www.imoutdoorsmedia.com" title="">Advertise</a>
+             <!-- <a href="/privacy" title="">Privacy Policy</a> &middot;-->
+              <!--<a href="/terms" title="">Terms &amp; Conditions</a>-->
+            </div>
+          </div>
+
+        </div>
+      </section>
+      
+      
+
+    
+  </footer>
+
+  <?php if (CFCT_DEBUG) : ?>
+  <div style="border: 1px solid #ccc; background: #ffc; padding: 20px;">The <code>CFCT_DEBUG</code> setting is currently enabled, which shows the filepath of each included template file. To hide the file paths, edit this setting in the <?php echo CFCT_PATH; ?>functions.php file.</div>
+  <?php endif; ?>
+
+  <?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/imo-mags-northamericanwhitetail/header/header-default.php
@@ -0,0 +1,241 @@
+<?php
+
+/**
+ * @package carrington-business
+ *
+ * This file is part of the Carrington Business Theme for WordPress
+ * http://crowdfavorite.com/wordpress/themes/carrington-business/
+ *
+ * Copyright (c) 2008-2011 Crowd Favorite, Ltd. All rights reserved.
+ * http://crowdfavorite.com
+ *
+ * **********************************************************************
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * **********************************************************************
+ */
+ 
+
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+?>
+<!DOCTYPE html>
+<!-- bid: <?php global $blog_id; print $blog_id ?>; env: <?php if(defined("WEB_ENV")) { print WEB_ENV; } else { print "production"; } ?> -->
+<!--[if IE 6]><![endif]-->
+<html <?php language_attributes() ?>>
+<head>
+	
+	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
+
+	<title><?php wp_title( '-', true, 'right' ); esc_attr_e(get_bloginfo('name')); ?></title>
+
+	<meta http-equiv="X-UA-Compatible" content="chrome=1" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
+	<?php wp_get_archives(array('type' => 'monthly', 'format' => 'link')); ?>
+	
+	<link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo('template_url'); ?>/css/css.php?ver=<?php echo CFCT_URL_VERSION; ?>" />
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<!--[if lte IE 7]>
+		<link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo('template_url'); ?>/css/lte-ie7.css?ver=<?php echo CFCT_URL_VERSION; ?>" />
+	<![endif]-->
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.0.6/modernizr.min.js"></script>
+	<?php
+	// Include javascript for threaded comments if needed
+	if ( is_singular() && get_option('thread_comments') ) { wp_enqueue_script( 'comment-reply' ); }
+	wp_enqueue_script('my_scripts_method');
+	wp_head();
+	?>
+	<link href='http://fonts.googleapis.com/css?family=Rokkitt:400,700' rel='stylesheet' type='text/css'>
+	<!--
+/* @license
+ * MyFonts Webfont Build ID 2288984, 2012-05-09T13:35:45-0400
+ * 
+ * The fonts listed in this notice are subject to the End User License
+ * Agreement(s) entered into by the website owner. All other parties are 
+ * explicitly restricted from using the Licensed Webfonts(s).
+ * 
+ * You may obtain a valid license at the URLs below.
+ * 
+ * Webfont: Proxima Nova A Thin by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/a-thin/
+ * Licensed pageviews: 100,000
+ * 
+ * Webfont: Proxima Nova S Thin by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/s-thin/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova A Extrabold by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/a-extrabld/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova S Extrabold by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/s-extrabld/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova S Semibold by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/s-semibold/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova S Black by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/s-black/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova A Regular by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/a-regular/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova A Black by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/a-black/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova S Light by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/s-light/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova A Light by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/a-light/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova S Bold by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/s-bold/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova A Bold by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/a-bold/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova A Semibold by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/a-semibold/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova S Regular by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/s-regular/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova Thin by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/thin/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova Extrabold by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/extrabld/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova Regular by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/regular/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova Semibold by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/semibold/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova Black by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/black/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova Light by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/light/
+ * Licensed pageviews: unspecified
+ * 
+ * Webfont: Proxima Nova Bold by Mark Simonson
+ * URL: http://www.myfonts.com/fonts/marksimonson/proxima-nova/bold/
+ * Licensed pageviews: unspecified
+ * 
+ * 
+ * License: http://www.myfonts.com/viewlicense?type=web&buildid=2288984
+ * Webfonts copyright: Copyright (c) Mark Simonson, 2005. All rights reserved.
+ * 
+ * © 2012 Bitstream Inc
+*/
+
+-->
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('url'); ?>/wp-content/themes/imo-mags-northamericanwhitetail/MyFontsWebfontsKit.css">
+	 <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/scripts.js"></script>
+	<!-- IMO MODS -->
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-5816642-20']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+	
+</head>
+<body <?php body_class(); ?>>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<header id="header">
+	<div id="tophat">
+    		<div class="user-bar">
+    			<ul id="tophat-left">
+    				<li><a href="#">+ Post</a></li>
+    				<li><a href="#">People</a></li>
+    				<li><a href="#">Messages (<?php echo '3'; //some sort of fun code for Aaron. :) ?>)</a></li>
+    				<li><a href="#">Saved Articles</a></li>
+    				<li><a href="#">Your Profile</a></li>
+    			</ul>
+    			<ul id="tophat-right">
+    				<li><img src="<?php bloginfo('url'); ?>/wp-content/themes/imo-mags-northamericanwhitetail/img/user-temp.jpg" alt="user photo" /></li>
+    				<li>Hello, <a href="#"><?php echo 'Batman'; //OMG, it's BATMAN!!!! ?><span></span></a></li>
+    				<li><a href="#">Sign Out</a></li>
+    			</ul>
+    		</div>
+		</div>
+    <div class="container">
+    	    	
+    	
+	<div class="centered">
+       	<a href="<?php echo home_url('/'); ?>" class="brand" title="<?php _e('Home', 'carrington-business') ?>"><img src="<?php print get_stylesheet_directory_uri(); ?>/img/NAW-logo.png" alt="<?php bloginfo('name'); ?>" /></a>
+      	<div id="header-search">
+      		<?php cfct_form('search'); ?>
+		</div>
+		<?php get_template_part('head', 'subscribe') ?>
+
+   	</div>
+			</div>
+		<div class="uber-nav">
+	    <?php wp_nav_menu(array( 'theme_location' => 'featured' )); ?>
+	  </div>
+		
+		<div id="site-menu">
+		  <div class="container">
+		    <?php wp_nav_menu(array( 
+  				'theme_location' => 'main',
+  				'container' => 'nav',
+  				'container_class' => 'nav-main nav',
+  				'depth' => 2,
+  			)); ?>
+  			
+  			<div class="stay-connected">
+  			 
+  			 <span class="label">FOLLOW US ON</span>
+  			 <ul class="connections">
+  			   <li><a class="facebook" href="http://www.facebook.com/americanwhitetail" title="Find us on Facebook">Facebook</a></li>
+  			   <li><a class="twitter" href="http://twitter.com/americanwhitetail" title="Follow us on Twitter">Twitter</a></li>
+  			   <li><a class="feed" href="http://feeds.feedburner.com/americanwhitetail" title="Get the RSS Feed">RSS Feed</a></li>
+  			   <li><a class="like" href="#" title="Get the RSS Feed">Like</a></li>
+  			 </ul>
+  			</div>
+		  </div>
+		</div>
+	</header>
+	<hr class="accessibility" />
+	<section id="main" class="str-container">
+		<div id="main-content">
+			<div class="str-content clearfix">
+		
