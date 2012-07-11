c1f625bc01a5b64a83bcb6808ad17d57045c1b0d
Aaron Baker <aaron.baker@imoutdoors.com>
2012-07-11 12:06:34 -0400

    Fixing another merge conflict

--- wp-content/themes/carrington-business/functions.php
+++ wp-content/themes/carrington-business/functions.php
@@ -333,32 +333,32 @@ function insert_image_src_rel_in_head() {
 }
 add_action( 'wp_head', 'insert_image_src_rel_in_head', 5 );
 
-function insert_image_src_rel_in_head() {
+// function insert_image_src_rel_in_head() {
 
-    global $post;
+//     global $post;
 
-    if ( !is_singular()) //if it is not a post or a page
+//     if ( !is_singular()) //if it is not a post or a page
 
-        return;
-    if(!has_post_thumbnail( $post->ID )) { //the post does not have featured image, use a default image
+//         return;
+//     if(!has_post_thumbnail( $post->ID )) { //the post does not have featured image, use a default image
 
-        $default_image="http://example.com/image.jpg"; //replace this with a default image on your server or an image in your media library
+//         $default_image="http://example.com/image.jpg"; //replace this with a default image on your server or an image in your media library
 
-        echo '<meta property="og:image" content="' . $default_image . '"/>';
+//         echo '<meta property="og:image" content="' . $default_image . '"/>';
 
-    }
+//     }
 
-    else{
+//     else{
 
-        $thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+//         $thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
 
-        echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '"/>';
+//         echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '"/>';
 
-    }
+//     }
 
-    echo "\n";
+//     echo "\n";
 
-}
+// }
 
 add_action( 'wp_head', 'insert_image_src_rel_in_head', 5 );
 
