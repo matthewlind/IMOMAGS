be2c6c28736de9ead3315a90a44c84967fcf5b26
Fox Bowden <fox.bowden@imoutdoors.com>
2012-09-11 14:53:01 -0400

    small url fix in network nav

--- wp-content/themes/imo-mags-rifleshooter/header/header-default.php
+++ wp-content/themes/imo-mags-rifleshooter/header/header-default.php
@@ -72,7 +72,7 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 		<div class="network-nav">
 			<ul>
 				<li class="ga"><a href="http://gunsandammo.com"><div></div></a></li>
-				<li class="hg"><a href="http://rifleshootermag.com"><div></div></a></li>
+				<li class="hg"><a href="http://handgunsmag.com"><div></div></a></li>
 				<li class="st"><a href="http://shootingtimes.com"><div></div></a></li>
 				<li class="rs-active active"><div></div></li>
 				<li class="sn"><a href="http://shotgunnews.com"><div></div></a></li>
