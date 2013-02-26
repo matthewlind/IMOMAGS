c48b97bef4b896f298519dfa4ca204c97d04b65d
Fox Bowden <fox.bowden@imoutdoors.com>
2013-02-26 14:53:58 -0500

    adding sticky to default homepage

--- wp-content/themes/carrington-business/pages/right-sidebar.php
+++ wp-content/themes/carrington-business/pages/right-sidebar.php
@@ -28,7 +28,28 @@ the_post();
 		<div class="bonus">
 			<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('bonus_sidebar')) : else : ?><?php endif; ?>
 		</div>
+		<div id="responderfollow"></div>
+		<div class="sidebar advert">
+			<?php if (function_exists("imo_dart_tag")) {
+	            imo_dart_tag("300x250");
+	          } else { ?>
+	            <script type="text/javascript">
+	              document.write(unescape('%3Cscript src="http://ad.doubleclick.net/adj/imo.'+dartadsgen_site+'/;sect=;page=index;pos=btf;subs=;sz=728x90;dcopt=;tile='+pr_tile+';ord='+dartadsgen_rand+'?"%3E%3C/script%3E'));
+	            </script>
+	            <script type="text/javascript">
+	              ++pr_tile;
+	            </script>
+	            <noscript>
+	              <a href="http://ad.doubleclick.net/adj/imo.outdoorsbest/;sect=;page=index;pos=btf;subs=;sz=300x250;dcopt=;tile=1;ord=7391727509?">
+	                <img src="http://ad.doubleclick.net/ad/imo.outdoorsbest/home;sect=;page=index;pos=btf;subs=;sz=300x250;dcopt=;tile=1;ord=7391727509?" border="0" />
+	              </a>
+	            </noscript>
+	          <?php } ?>
+	
+			<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('scroll-sidebar')) : else : endif; ?>
+		</div>
 	</div>
+
 	<div class="col-abc">
 		<div <?php post_class('entry entry-full clearfix'); ?>>
 			<div class="entry-content">
