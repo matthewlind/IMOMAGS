74578b7bdd16b4bab0ddafe34cef4a6c19df9654
Fox Bowden <fox.bowden@imoutdoors.com>
2013-02-26 12:33:57 -0500

    fixes for mid level add and facebook recommendation boxes

--- wp-content/themes/imo-mags-handguns/single/single-default.php
+++ wp-content/themes/imo-mags-handguns/single/single-default.php
@@ -23,28 +23,8 @@ get_header();
 ?>
 <div class="col-ab">
 	<?php
-	cfct_loop(); ?>
-	<div class="post-content-area">
-		<div style="float:left">
-			<?php if (function_exists("imo_dart_tag")) {
-	            imo_dart_tag("300x250");
-	          } else { ?>
-	            <script type="text/javascript">
-	              document.write(unescape('%3Cscript src="http://ad.doubleclick.net/adj/imo.'+dartadsgen_site+'/;sect=;page=index;subs=;sz=728x90;dcopt=;tile='+pr_tile+';ord='+dartadsgen_rand+'?"%3E%3C/script%3E'));
-	            </script>
-	            <script type="text/javascript">
-	              ++pr_tile;
-	            </script>
-	            <noscript>
-	              <a href="http://ad.doubleclick.net/adj/imo.outdoorsbest/;sect=;page=index;pos=mid;subs=;sz=300x250;dcopt=;tile=1;ord=7391727509?">
-	                <img src="http://ad.doubleclick.net/ad/imo.outdoorsbest/home;sect=;page=index;pos=mid;subs=;sz=300x250;dcopt=;tile=1;ord=7391727509?" border="0" />
-	              </a>
-	            </noscript>
-	          <?php } ?>
-			</div>		
-		<div class="fb-recommendations recommendations" data-site="www.handgunsmag.com" data-width="300" data-height="250" data-header="true"></div>
-	</div>
-	<?php comments_template();
+	cfct_loop(); 
+	comments_template();
 	?>
 </div>
 <div id="sidebar" class="col-c">
