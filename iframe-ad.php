61672ffcdcdbcc5400abcdc5d07897830f236ae2
Aaron Baker <aaron.baker@imoutdoors.com>
2013-03-15 16:23:54 -0400

    Iframe ads now allow for campaigns

--- iframe-ad.php
+++ iframe-ad.php
@@ -10,6 +10,14 @@ if (!empty($_GET['size'])) {
 }
 
 
+if (!empty($_GET['camp'])) {
+  $campString = "camp=" . $_GET['camp'] . ";" ;
+} else {
+  $campString = "";
+}
+
+
+
 ?>
 <html>
 <head>
@@ -19,7 +27,7 @@ var randomdate=new Date();
 var randomtime=randomdate.getTime();
 var pr_tile=1;
 var dartadsgen_rand=randomtime;
-document.write(unescape('%3Cscript src="http://ad.doubleclick.net/adj/<?php echo $code; ?>/;sect=ajax_gallery;page=index;subs=;sz=<?php echo $size; ?>;dcopt=;tile='+pr_tile+';ord='+dartadsgen_rand+'?"%3E%3C/script%3E'));
+document.write(unescape('%3Cscript src="http://ad.doubleclick.net/adj/<?php echo $code; ?>/;sect=ajax_gallery;page=index;<?php echo $campString; ?>subs=;sz=<?php echo $size; ?>;dcopt=;tile='+pr_tile+';ord='+dartadsgen_rand+'?"%3E%3C/script%3E'));
 -->
 
 </script>
