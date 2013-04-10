8cdbee2af15ad015d76b815c0c12b2aabba8f39e
Aaron Baker <aaron.baker@imoutdoors.com>
2013-04-10 16:28:39 -0400

    Solunar calendar updates

--- solunar-testing/js/script.js
+++ solunar-testing/js/script.js
@@ -3,33 +3,33 @@ jQuery(document).ready(function($) {
 	var d = new Date();
 	var currentMonth = d.getMonth() + 1;
 	var year = 2013; //This needs to be changed later if we extend our contract
-	
-	
-	
+
+
+
 	//set the selected month to the current month
 	$("select#month option[value=" +currentMonth+ "]").attr("selected","selected");
-	
+
 	//Update calendar on Form Submit
 	$(".solunar-submit").on("click",function(ev){
 		ev.preventDefault();
-		
+
 		var selectedMonth = $('div#zf-select-month .scrollable li.selected').attr("val");
 		var location = $("#solunar-location").val();
 
-		
-		updateCalendar(selectedMonth,year,location); 
+
+		updateCalendar(selectedMonth,year,location);
 	});
-	
-	
+
+
 	//Update calendar on month change
 	$('div#zf-select-month').on("change",function(ev){
 		var selectedMonth = $('div#zf-select-month .scrollable li.selected').attr("val");
 		var location = $("#solunar-location").val();
-		
+
 		if (location)
-			updateCalendar(selectedMonth,year,location); 
+			updateCalendar(selectedMonth,year,location);
 	});
-	
+
 	//Use HTML5 Geolocation to detect location and update calendar
 	if (navigator.geolocation)
     {
@@ -37,146 +37,146 @@ jQuery(document).ready(function($) {
 	    	console.log(position);
 	    	var lat = position.coords.latitude;
 	    	var url = "/wpdb/gps-to-zip.php?lat=" + position.coords.latitude +  "&lon=" + position.coords.longitude ;
-	    	
+
 	    	$.getJSON(url,function(closestLocation){
-	    	
+
 	    		var selectedMonth = $('div#zf-select-month .scrollable li.selected').attr("val");
-	    		
+
 		    	$("#solunar-location").val(closestLocation.zip)
-		    	updateCalendar(selectedMonth,year,closestLocation.zip); 
+		    	updateCalendar(selectedMonth,year,closestLocation.zip);
 	    	});
     	});
     }
 
-	
-	
-	
-	
+
+
+
+
 	function updateCalendar(month,year,location) {
-	
+
 		$(".calendar-holder").fadeTo("normal",0.20);
-		
-		
+
+
 		var url = "/wpdb/solunar.php?month=" + month +  "&year=" + year +  "&location=" + location ;
-		
-		
-		
+
+
+
 		$.getJSON(url,function(solunarDays){
-		
+
 			$(".calendar-holder").fadeTo("normal",1.0);
-		
+
 			var monthNames = [ "nothing","Jan.", "Feb.", "Mar.", "Apr.", "May", "June",
 		    "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec." ];
-			
+
 			var dayOffset = solunarDays[0].weekdaycode;
 			var locationName = solunarDays[0].city + ", " + solunarDays[0].state;
 
-			$("h1.location-header").html(locationName);
-			
+			$("h1.location-header").html("Current Location: " + locationName);
+
 			var currentDay = 0;
 			var currentCell = 0;
-			
+
 			$(".calendar-data td").html("").addClass("other-month");
 
 
-			
+
 			$(".calendar-data td").each(function(key,dataCell){
 				currentCell++;
-				
+
 				var $dataCell = $(dataCell);
-				
+
 				//dayOffset determines the day fo the week in which we start
 				if (currentCell > dayOffset) {
-					
-				   var currentDayData = solunarDays[currentDay];		
-					
+
+				   var currentDayData = solunarDays[currentDay];
+
 					if (currentDayData) {
-					
+
 						$dataCell.removeClass("other-month");
-						
+
 						//format the sun/moon up down times
 						currentDayData.sunrise = currentDayData.sunrise.toUpperCase().replace("A","<span>A</span>").replace("P","<span>P</span>");
 						currentDayData.sunset = currentDayData.sunset.toUpperCase().replace("A","<span>A</span>").replace("P","<span>P</span>");
 						currentDayData.moonrise = currentDayData.moonrise.toUpperCase().replace("A","<span>A</span>").replace("P","<span>P</span>");
-						currentDayData.moonset = currentDayData.moonset.toUpperCase().replace("A","<span>A</span>").replace("P","<span>P</span>");				
-						
+						currentDayData.moonset = currentDayData.moonset.toUpperCase().replace("A","<span>A</span>").replace("P","<span>P</span>");
+
 						//Format the peaktimes
 						currentDayData.monthname = monthNames[currentDayData.month];
 						currentDayData.ammajorstart = currentDayData.ammajorstart.replace("M","").replace("A","<span>A</span>").replace("P","<span>P</span>");
 						currentDayData.ammajorend = currentDayData.ammajorend.replace("M","").replace("A","<span>A</span>").replace("P","<span>P</span>");
 						currentDayData.amminorstart = currentDayData.amminorstart.replace("M","").replace("A","<span>A</span>").replace("P","<span>P</span>");
-						currentDayData.amminorend = currentDayData.amminorend.replace("M","").replace("A","<span>A</span>").replace("P","<span>P</span>");				
+						currentDayData.amminorend = currentDayData.amminorend.replace("M","").replace("A","<span>A</span>").replace("P","<span>P</span>");
 						currentDayData.pmmajorstart = currentDayData.pmmajorstart.replace("M","").replace("A","<span>A</span>").replace("P","<span>P</span>");
 						currentDayData.pmmajorend = currentDayData.pmmajorend.replace("M","").replace("A","<span>A</span>").replace("P","<span>P</span>");
 						currentDayData.pmminorstart = currentDayData.pmminorstart.replace("M","").replace("A","<span>A</span>").replace("P","<span>P</span>");
-						currentDayData.pmminorend = currentDayData.pmminorend.replace("M","").replace("A","<span>A</span>").replace("P","<span>P</span>");	
-						
-						
+						currentDayData.pmminorend = currentDayData.pmminorend.replace("M","").replace("A","<span>A</span>").replace("P","<span>P</span>");
+
+
 						//Sometimes a day only has 3 good times. If this is the case, we trash the missing time. if not, we just trash the last one.
-						var time1 = {start: currentDayData.ammajorstart, end: currentDayData.ammajorend, official: currentDayData.ammajor};				
-						var time2 = {start: currentDayData.pmmajorstart, end: currentDayData.pmmajorend, official: currentDayData.pmmajor};	
-						var time3 = {start: currentDayData.amminorstart, end: currentDayData.amminorend, official: currentDayData.amminor};				
-						var time4 = {start: currentDayData.pmminorstart, end: currentDayData.pmminorend, official: currentDayData.pmminor};	
-						
+						var time1 = {start: currentDayData.ammajorstart, end: currentDayData.ammajorend, official: currentDayData.ammajor};
+						var time2 = {start: currentDayData.pmmajorstart, end: currentDayData.pmmajorend, official: currentDayData.pmmajor};
+						var time3 = {start: currentDayData.amminorstart, end: currentDayData.amminorend, official: currentDayData.amminor};
+						var time4 = {start: currentDayData.pmminorstart, end: currentDayData.pmminorend, official: currentDayData.pmminor};
+
 						var times = [time1,time2,time3,time4];
-						
+
 						//Remove the bad times
 						var keyToRemove = null;
 						_.each(times,function(time,key){
-				
+
 							if (time.official == "-----") {
-							
+
 								keyToRemove = key;
-			
+
 							}
-							
+
 						});
-						
+
 						//If there are not bad times, scrap the last time.
 						if (keyToRemove == null)
 							keyToRemove = 3;
 						times.splice(keyToRemove,1);
-						
+
 						currentDayData.times = times;
-		
+
 						var template = _.template($("#td-template").html(),{data:currentDayData});
-					
+
 						$dataCell.append(template);
 					}
-		
-					
-					
-					
+
+
+
+
 					currentDay++;
 				}//end iff
-			
-				
-				
-				
-				
-		
+
+
+
+
+
+
 			}); //end each
-			
+
 			//Attach the events
 			$(".a-event").click(function () {
-			
+
 				//First, hide the other popups
 				$(".cal-popup").hide();
 			    //$(".a-event").parent("div").removeClass("active");
-			    
+
 			    //then show the popup
 			   $(this).siblings(".cal-popup").show();
 			   //$(this).parent("div").addClass("active");
 			});
-			
-			
+
+
 			$(".a-event").parent("div").hover(function(ev){
 				$(this).addClass("active");
-				
+
 			},function(ev){
 				$(this).removeClass("active");
 			});
-		
+
 			//also hide popup on close button click
 			$(".close-popup").on('click', function () {
 			    $(".cal-popup").hide();
@@ -188,14 +188,14 @@ jQuery(document).ready(function($) {
 
 			if ($("tr.last-row td").first().hasClass("other-month"))
 				$("tr.last-row").hide();
-		
-			
-			
+
+
+
 		}).fail(function(){
 			alert("Location not found. Please try again.")
 		});
-		
-		
+
+
 	}
 
 
@@ -204,8 +204,10 @@ jQuery(document).ready(function($) {
 
 
 
+
 $(".jq-custom-form select").zfselect({
-    rows: 10
+    rows: 30,
+    width:250
 });
 
 
--- wpdb/solunar.php
+++ wpdb/solunar.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 //include mysql account info
 include 'mysql.php';
@@ -6,10 +6,10 @@ include 'moon-phase.php';
 include 'statelist.php';
 
 //Set headers for json output
-header('Access-Control-Allow-Origin: *');  
-date_default_timezone_set('America/New_York'); 
+header('Access-Control-Allow-Origin: *');
+date_default_timezone_set('America/New_York');
 
-//Default Settings 
+//Default Settings
 $month = 3;
 $year = 2013;
 $location = 30309;
@@ -24,21 +24,23 @@ extract($params,EXTR_OVERWRITE);
 if (is_numeric($location)) { //if location is zip
 
 	importSolunarData($month,$year,$location);
-	querySolunarData($month,$year,$location);	
-	
+	querySolunarData($month,$year,$location);
+
 } else {//if it's not a zip code, parse the location.
 
 	$locationParts = explode(",",$location);
-	
+
 	$city = $locationParts[0];
 	$state = strtolower(trim($locationParts[1]));
-	
+
 	if (!empty($statelist[$state])) {//convert state name to 2 character state
 		$state = $statelist[$state];
 	}
 
 	$location = $city . $state;
-	
+
+	$location = urlencode($location);
+
 	importSolunarData($month,$year,$location);
 	querySolunarData($month,$year,$location);
 }
@@ -49,25 +51,24 @@ extract($params,EXTR_OVERWRITE);
 $hash = md5("h4m9az01" . $dateString);
 
 function querySolunarData($month,$year,$zip) {
-	
-	
+
+
 	$sql = "SELECT * FROM solunar WHERE postalcode = $zip AND month = $month and year = $year";
-	
+
 	//echo $sql;
-	
+
 	$db = dbConnect();
 	$stmt = $db->prepare($sql);
 	$stmt->execute(array());
 	$solunarData = $stmt->fetchAll(PDO::FETCH_OBJ);
-	
+
 	$db = "";
-	
+
 	if (count($solunarData)>0)
 		print_r(json_encode($solunarData));
 	else
 		header("HTTP/1.0 404 Not Found");
-	
-	
+
 }
 
 /////////////////////////////
@@ -78,42 +79,43 @@ function importSolunarData($month,$year,$location) {
 	//Generate a secure hash for authentication with solunar.com
 	$dateString = gmdate("Y-m-d");
 	$hash = md5("h4m9az01" . $dateString);
-	
+
 	if (is_numeric($location))
 		$url = "http://www.solunar.com/solunarwebservice.asmx/ComputeForPostalCode?_Token=$hash&_PostalCode=$location&_Month=$month&_Year=$year&_UseCache=false";
 	else
 		$url = "http://www.solunar.com/solunarwebservice.asmx/ComputeForCityState?_Token=$hash&_CityState=$location&_Month=$month&_Year=$year&_UseCache=false";
-		
-	//echo $url;
+
+
 	//echo "<br>";
-		
-	//Grab the XML		
-	$xml = file_get_contents($url,FALSE);
-	
 	//echo $url;
-	
+
+	//Grab the XML
+	$xml = file_get_contents($url,FALSE);
+
+	//echo $url;
+
 	$parsedXML = new SimpleXMLElement($xml);
 	$solunarData = $parsedXML->SolunarData;
-	
+
 	//print_r($xml);
-	
-	
+
+
 	//Set the location to the found zipcode so that we can query the db by zipcode
 	global $location;
 	$location = $solunarData->PostalCode;
-	
-	
-	
+
+
+
 	//Parse and insert the XML feed results
 	foreach ($solunarData as $key => $dayData) {
-	
+
 		//Existing values are replaced
 		$sql = "REPLACE INTO solunar ";
 		$sqlColumns = "(";
 		$sqlValues = "(";
-		
+
 		//print_r($dayData);
-		
+
 		//Generate day of week
 		$day = intval($dayData->Day);
 		$timestamp = mktime(null,null,null,$month,$day,$year);
@@ -121,14 +123,14 @@ function importSolunarData($month,$year,$location) {
 		$dayOfWeekNumber = date("w",$timestamp);
 		$dayData->weekday = $dayOfWeek;
 		$dayData->weekdaycode = intval($dayOfWeekNumber);
-		
+
 		$dayData->mooncode = get_moon_code($timestamp);
-				
-		
+
+
 		//Generate color code and correct the moon codes
 		$peakDayFlag = $dayData->PeakDayFlag;
 		$dayData->peakcode = 0;
-		
+
 		if ($peakDayFlag == "Q") {
 			$dayData->peakcode = 3;
 		}
@@ -139,7 +141,7 @@ function importSolunarData($month,$year,$location) {
 		if ($peakDayFlag == "F") {
 			$dayData->peakcode = 4;
 			$dayData->mooncode = 12;
-		}					
+		}
 		if ($peakDayFlag == ">") {
 			$dayData->peakcode = 1;
 		}
@@ -148,58 +150,58 @@ function importSolunarData($month,$year,$location) {
 
 		if ($dayData->mooncode == 1 || $dayData->mooncode == 12)
 			$dayData->peakcode = 4;
-		
+
 		if ($dayData->mooncode == 13 || $dayData->mooncode == 11)
 			$dayData->peakcode = 3;
-					
+
 		if ($dayData->mooncode == 2 || $dayData->mooncode == 24)
-			$dayData->peakcode = 3;		
+			$dayData->peakcode = 3;
 
 		if ($dayData->mooncode == 3 || $dayData->mooncode == 23)
-			$dayData->peakcode = 2;	
+			$dayData->peakcode = 2;
 
 		if ($dayData->mooncode == 14 || $dayData->mooncode == 10)
-			$dayData->peakcode = 2;	
+			$dayData->peakcode = 2;
 
 		if ($dayData->mooncode == 4 || $dayData->mooncode == 22)
-			$dayData->peakcode = 1;	
+			$dayData->peakcode = 1;
 
 		if ($dayData->mooncode == 15 || $dayData->mooncode == 9)
-			$dayData->peakcode = 1;	
+			$dayData->peakcode = 1;
 
 		if ($dayData->mooncode < 1)
 			$dayData->mooncode = 1;
-			
+
 		if ($dayData->mooncode > 24)
 			$dayData->mooncode = 24;
-			
-	
+
+
 		//print_r($dayData);
-	
+
 		//Generate the timestamps for the major and minor times
 		$dayData->ammajortimestamp = get_timestamp($dayData->AMMajor,"AM",$month,$day,$year);
 		$dayData->pmmajortimestamp = get_timestamp($dayData->PMMajor,"PM",$month,$day,$year);
 		$dayData->amminortimestamp = get_timestamp($dayData->AMMinor,"AM",$month,$day,$year);
-		$dayData->pmminortimestamp = get_timestamp($dayData->PMMinor,"PM",$month,$day,$year);	
-		
+		$dayData->pmminortimestamp = get_timestamp($dayData->PMMinor,"PM",$month,$day,$year);
+
 		//Generate the start times and end times by adding and subtracting an hour
 		$dayData->ammajorstart = date("g:iA",$dayData->ammajortimestamp - 3600);
 		$dayData->ammajorend =   date("g:iA",$dayData->ammajortimestamp + 3600);
 		$dayData->pmmajorstart = date("g:iA",$dayData->pmmajortimestamp - 3600);
-		$dayData->pmmajorend =   date("g:iA",$dayData->pmmajortimestamp + 3600);		
+		$dayData->pmmajorend =   date("g:iA",$dayData->pmmajortimestamp + 3600);
 		$dayData->amminorstart = date("g:iA",$dayData->amminortimestamp - 3600);
 		$dayData->amminorend =   date("g:iA",$dayData->amminortimestamp + 3600);
 		$dayData->pmminorstart = date("g:iA",$dayData->pmminortimestamp - 3600);
-		$dayData->pmminorend =   date("g:iA",$dayData->pmminortimestamp + 3600);	
-		
-		
+		$dayData->pmminorend =   date("g:iA",$dayData->pmminortimestamp + 3600);
+
+
 		//set the ----- to null
 
-		
-		
-		
-		
-		
+
+
+
+
+
 		//Setup counters for calendar cell start day
 		$total = count($dayData);
 		$count = 0;
@@ -207,72 +209,72 @@ function importSolunarData($month,$year,$location) {
 
 		foreach ($dayData as $colName => $value) {
 			$count++;
-			
-			
+
+
 			$lowercaseColName = strtolower($colName);
-			
+
 			$sqlColumns .= $lowercaseColName;
 			$sqlValues .= "'" . $value ."'";
-			
+
 			if ($count != $total) {
 				$sqlColumns .= ", ";
 				$sqlValues .= ", ";
 			}
-					
-		}	
+
+		}
 		$sqlColumns .= ")";
 		$sqlValues .= ")";
-		
+
 		$sql .= $sqlColumns . " VALUES " . $sqlValues;
-		
-		//echo $sql;	
-		
+
+		//echo $sql;
+
 		$db = dbConnect();
 		$stmt = $db->prepare($sql);
 		$stmt->execute(array());
 		$db = "";
-	
+
 	}
-	
+
 }//End function download solunar data
 
 /////////////////////////////
 //calc moon phase
 function get_moon_code($timestamp){
 	$moon = new MoonPhase($timestamp);
-	
+
 	$illumination = $moon->illumination();
 	$rIllumination = 1 - $illumination;//Reverse the number to better fit our moon filenames
-	
+
 	$phase = $moon->phase();
-	
-		
-	return round(($moon->phase() * 24) + .5);
+
+
+	return round(($moon->phase() * 24) + 0.9);
 
 }
 
 /////////////////////////////
 //calc get timestamp
 function get_timestamp($timestring,$period,$month,$day,$year) {
-	
+
 	//print_r($timestring);
-	
+
 	$timeparts = explode(":",$timestring);
 	$hours = $timeparts[0];
 	$minutes = $timeparts[1];
-	
+
 	//print_r($timeparts);
-	
+
 	if ($period = "PM")
 		$hours = $hours + 12;
-	
+
 	$time = mktime($hours,$minutes,0,$month,$day,$year);
-	
+
 	return $time;
-	
-	
-	
-	
+
+
+
+
 }
 
 
