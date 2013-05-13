557e063bb0d500feb4d320b5241e1b6413b81daf
Aaron Baker <aaron.baker@imoutdoors.com>
2013-05-13 11:53:51 -0400

    Theme updates for cabelas product widget

--- wp-content/themes/carrington-business/content/content-default.php
+++ wp-content/themes/carrington-business/content/content-default.php
@@ -12,7 +12,7 @@
  * **********************************************************************
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * **********************************************************************
  */
 
@@ -23,7 +23,7 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 $postID = get_the_ID();
 $categoryID = get_post_meta($postID);
 $catID = $categoryID["_category_permalink"];
-$categoryName = get_term_by('id', $catID[0], 'category'); 
+$categoryName = get_term_by('id', $catID[0], 'category');
 
 //set the primary category urls
 $url="/category/".$categoryName->slug;
@@ -31,19 +31,19 @@ $url="/category/".$categoryName->slug;
 ?>
 <div <?php post_class('entry entry-full clearfix') ?>>
 	<div class="entry-header">
-		<?php 
+		<?php
 		if(in_category("shot-show-2013")){
-			echo '<div class="primary-shot-show">'; 
-				echo '<a class="primary-cat" href="/shot-show-2013">'.$categoryName->name.'</a>'; 
-				echo '<div class="presented-by">Presented By</div>'; 
-					echo '<div class="sponsor-logo">'; 
+			echo '<div class="primary-shot-show">';
+				echo '<a class="primary-cat" href="/shot-show-2013">'.$categoryName->name.'</a>';
+				echo '<div class="presented-by">Presented By</div>';
+					echo '<div class="sponsor-logo">';
 						if($_SERVER['SERVER_NAME'] == "www.petersenshunting.com" || $_SERVER['SERVER_NAME'] == "www.northamericanwhitetail.com" || $_SERVER['SERVER_NAME'] == "www.bowhuntingmag.com/" || $_SERVER['SERVER_NAME'] == "www.gundogmag.com/" || $_SERVER['SERVER_NAME'] == "www.wildfowlmag.com/" || $_SERVER['SERVER_NAME'] =="www.bowhunter.com/" || $_SERVER['SERVER_NAME'] == "www.gameandfishmag.com/" ){
-							echo '<a href="http://www.realtree.com/huntallseason/index.html" target="_blank"><img src="/wp-content/themes/imo-mags-gunsandammo/img/realtree-logo.png" alt="Realtree Xtra" title="Realtree Xtra" /></a>'; 
+							echo '<a href="http://www.realtree.com/huntallseason/index.html" target="_blank"><img src="/wp-content/themes/imo-mags-gunsandammo/img/realtree-logo.png" alt="Realtree Xtra" title="Realtree Xtra" /></a>';
 							}else{
-								echo '<a href="http://springfield-armory.com/" target="_blank"><img src="/wp-content/themes/imo-mags-gunsandammo/img/sausa.png" alt="Springfield Amory USA" title="Springfield Amory USA" /></a>';  
+								echo '<a href="http://springfield-armory.com/" target="_blank"><img src="/wp-content/themes/imo-mags-gunsandammo/img/sausa.png" alt="Springfield Amory USA" title="Springfield Amory USA" /></a>';
 							}
-					echo '</div>'; 
-				echo '</div>'; 
+					echo '</div>';
+				echo '</div>';
 		}
 		// If we're not showing this particular single post page, link the title
 		$this_post_is_not_single = (!is_single(get_the_ID()));
@@ -62,8 +62,8 @@ $url="/category/".$categoryName->slug;
 			<span class="spacer">&bull;</span>
             <?php endif; ?>
 			<abbr class="published" title="<?php the_time('Y-m-d\TH:i'); ?>"><?php the_time('F j, Y'); ?></abbr>
-			
-			
+
+
 			<?php
 			//if ($this_post_is_not_single) {
 				echo ' <span class="spacer">&bull;</span> ';
@@ -82,7 +82,24 @@ $url="/category/".$categoryName->slug;
 		the_content(__('Continued&hellip;', 'carrington-business'));
 		?>
 	</div><!-- entry content -->
-	<?php if (function_exists('imo_add_this')) {imo_add_this();} ?>	
+
+
+
+
+
+	<?php if (function_exists('imo_add_this')) {imo_add_this();} ?>
+<script type="text/javascript">
+  var ord = window.ord || Math.floor(Math.random() * 1e16);
+  document.write('<script type="text/javascript" src="http://ad.doubleclick.net/N4930/adj/imo.in-fisherman;sz=564x252;ord=' + ord + '?"><\/script>');
+</script>
+<noscript>
+<a href="http://ad.doubleclick.net/N4930/jump/imo.in-fisherman;sz=564x252;ord=[timestamp]?">
+<img src="http://ad.doubleclick.net/N4930/ad/imo.in-fisherman;sz=564x252;ord=[timestamp]?" width="630" height="320" />
+</a>
+</noscript>
+
+
+
 		<!-- Site - Hunting -->
 		<script type="text/javascript">
 		  var ord = window.ord || Math.floor(Math.random() * 1e16);
@@ -93,16 +110,16 @@ $url="/category/".$categoryName->slug;
 		<img src="http://ad.doubleclick.net/N4930/ad/imo.hunting;sz=1x1;ord=[timestamp]?" width="1" height="1" />
 		</a>
 		</noscript>
-		<div class="post-content-area">		
+		<div class="post-content-area">
 			<div style="float:left">
 			   <?php imo_dart_tag("300x250",false,array("pos"=>"mid")); ?>
-			</div>		
-	
+			</div>
+
 			<div class="fb-recommendations recommendations" data-site="<?php echo $_SERVER['SERVER_NAME']; ?>" data-width="300" data-height="250" data-header="true"></div>
 			<div class="divider"></div>
-			
+
 		</div>
-		
+
 	<div class="entry-footer">
 		<?php _e('In', 'carrington-business'); ?>
 		<?php
