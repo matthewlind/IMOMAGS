78bd2e3fa2c439670e082fc393538efa7ed89f1c
Fox Bowden <fox.bowden@imoutdoors.com>
2013-05-22 14:27:40 -0400

    fix in small error for primary cats

--- wp-content/themes/imo-mags-gunsandammo/content/content-default.php
+++ wp-content/themes/imo-mags-gunsandammo/content/content-default.php
@@ -23,7 +23,7 @@ $url="/shooting/".$categoryName->slug;
 		<div class="blog-headers shot-show nra-show">
 			<div class="blog-border"></div>
 			<h1>NRA SHOW 2013</h1>
-			
+
 			<div class="presented-by">Presented By</div>
 			<div class="desc">G&A brings you all the guns, gear and politics from the floor of the 2013 NRA Annual Meetings in Houston, Texas.  </div>
 			<div class="sponsor-logo" style="">
@@ -37,11 +37,9 @@ $url="/shooting/".$categoryName->slug;
 				<img src="http://ad.doubleclick.net/N4930/ad/imo.gunsandammo;sz=200x48;ord=[timestamp]?" width="200" height="48" />
 				</a>
 				</noscript></div>
-				
 		</div>
-		<?php echo the_primary_category("/shooting"); ?>
 		<?php }else{
-			echo the_primary_category("/shooting");	
+			echo '<a class="primary-cat" href="'.$url.'">'.$categoryName->name.'</a>';
 		}
 
 		// If we're not showing this particular single post page, link the title
