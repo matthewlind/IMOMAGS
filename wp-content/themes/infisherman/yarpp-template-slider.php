fd7b289b57497df82d036731f594e8c684e82286
Fox Bowden <fox.bowden@imoutdoors.com>
2013-06-19 14:20:21 -0400

    yarrp template updates for thumbnails

--- wp-content/themes/infisherman/yarpp-template-slider.php
+++ wp-content/themes/infisherman/yarpp-template-slider.php
@@ -13,13 +13,23 @@ Description: A Slider YARPP template.
         <li>
             <div class="paging-post">
                 <div class="paging-post-inner">
+                    <?php if(has_post_thumbnail()){ ?>
                     <div class="paging-image">
                         <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('index-thumb'); ?></a>
                     </div>
+                    <?php } ?>
                     <div class="paging-post-holder">
                         
                         <h3 class="entry-title">
-                            <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php $title = the_title('','',FALSE); echo substr($title, 0, 50); if (strlen($title) > 50) echo "..."; ?></a>
+                            <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
+                            	<?php 
+	                            	if(has_post_thumbnail()){ 
+										$title = the_title('','',FALSE); echo substr($title, 0, 50); if (strlen($title) > 50) echo "..."; 
+									}else{ 
+										$title = the_title('','',FALSE); echo substr($title, 0, 100); if (strlen($title) > 100) echo "..."; 
+									} 
+								?>
+							</a>
                         </h3>
                       
                     </div>
