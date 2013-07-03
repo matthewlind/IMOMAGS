bcc7231686cd5ede3cddeb12317e8a44855754db
Aaron Baker <aaron.baker@imoutdoors.com>
2013-07-03 11:20:48 -0400

    cabelas promo email date, ford widget size fix

--- wp-content/themes/carrington-business/widgets/ford-widget.php
+++ wp-content/themes/carrington-business/widgets/ford-widget.php
@@ -1,26 +1,26 @@
-<?php // Ford 300 x 600 Widget
+<?php // Ford 300 x 602 Widget
 
 class Ford_Widget extends WP_Widget {
 	function Ford_Widget() {
-		$widget_ops = array('classname' => 'widget_ford', 'description' => 'Ford 300 x 600 Widget' );
+		$widget_ops = array('classname' => 'widget_ford', 'description' => 'Ford 300 x 602 Widget' );
 		$this->WP_Widget('ford_widget', 'Ford Widget', $widget_ops);
 	}
-	
+
 	function widget() {
-	
+
 	$dartDomain = get_option("dart_domain", $default = false);
 ?>
-<div class="widget ford-outfitters-widget">	
+<div class="widget ford-outfitters-widget">
 	<script type="text/javascript">
 	 var ord = window.ord || Math.floor(Math.random() * 1e16);
-	 document.write('<iframe src="http://ad.doubleclick.net/N4930/adi/<?php echo $dartDomain; ?>;sz=300x600;ord=' + ord + '?" width="300" height="600" marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no"></iframe>');
+	 document.write('<iframe src="http://ad.doubleclick.net/N4930/adi/<?php echo $dartDomain; ?>;sz=300x602;ord=' + ord + '?" width="300" height="602" marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no"></iframe>');
 	</script>
 	<noscript>
-	<a href="http://ad.doubleclick.net/N4930/jump/<?php echo $dartDomain; ?>;sz=300x600;ord=[timestamp]?">
-	<img src="http://ad.doubleclick.net/N4930/ad/<?php echo $dartDomain; ?>;sz=300x600;ord=[timestamp]?" width="300" height="600" />
+	<a href="http://ad.doubleclick.net/N4930/jump/<?php echo $dartDomain; ?>;sz=300x602;ord=[timestamp]?">
+	<img src="http://ad.doubleclick.net/N4930/ad/<?php echo $dartDomain; ?>;sz=300x602;ord=[timestamp]?" width="300" height="602" />
 	</a>
 	</noscript>
 </div>
-<?php } 
+<?php }
 }
 register_widget('Ford_Widget');
