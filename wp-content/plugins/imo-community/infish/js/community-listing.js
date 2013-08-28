32a6703670afefe4a9a1c885c6be5df20fa21a38
Aaron Baker <aaron.baker@imoutdoors.com>
2013-08-28 16:49:47 -0400

    Improvements to community photo listing page

--- wp-content/plugins/imo-community/infish/js/community-listing.js
+++ wp-content/plugins/imo-community/infish/js/community-listing.js
@@ -1,18 +1,114 @@
 jQuery(document).ready(function($) {
 
-	var url = "http://" + document.domain + "/community-api/posts?skip=0&per_page=21&order_by=created&sort=DESC";
 
-	$.getJSON(url,function(posts){
+    //Set the filter to the default settings
+    filter = {};
+    filterReset();
+    function filterReset() {
+        filter.order_by = "created";
+        filter.sort = "DESC";
+        filter.master = 0;
+        filter.skip = 0;
+        filter.post_type = "all";
+        filter.per_page=10;
+        filter.post_count = 10000000;
+    }
 
-		$.each(posts,function(index,post){
+    //Highlight the default menu item
+    $("ul.filter #filter-menu-default").addClass("active");
 
-			var postHTML = _.template( $('#post-template').html() , { post: post });
-			$("#posts-container").append(postHTML);
+    //Get the JSON using the above filter configuration and append the photos.
+    getPhotosAndAppend();
+    loadMoreCheck();
+    function getPhotosAndAppend() {
+        var url = "http://" + document.domain + "/community-api/posts?skip="+filter.skip+"&per_page="+filter.per_page+"&order_by="+filter.order_by+"&sort="+filter.sort+"&master="+filter.master+"&post_type="+filter.post_type;
 
-		});
+        $.getJSON(url,function(posts){
 
-	});
-	
+            $.each(posts,function(index,post){
+
+                var postHTML = _.template( $('#post-template').html() , { post: post });
+                $("#posts-container").append(postHTML);
+
+            });
+
+            //hide the ajax loading spinner
+            $("#ajax-loader").hide();
+
+        });
+    }
+
+    //Change the filter configuration
+    $(".filter-menu").click(function(ev){
+
+        ev.preventDefault();
+
+        //Grab the selected menu item
+        var $menuItem = $(ev.currentTarget);
+
+        //Change the actively selected menu item
+        $("ul.filter li.active").removeClass("active");
+        $menuItem.closest("li").addClass("active");
+
+        //reset the filter
+        filterReset();
+
+
+        //Change the filter configuration according to the attributes of the clicked menu item
+        if ($menuItem.attr("sort") != undefined) { filter.sort = $menuItem.attr("sort"); }
+        if ($menuItem.attr("order_by") != undefined) { filter.order_by = $menuItem.attr("order_by"); }
+        if ($menuItem.attr("master") != undefined) { filter.master = $menuItem.attr("master"); }
+        if ($menuItem.attr("skip") != undefined) { filter.skip = $menuItem.attr("skip"); }
+        if ($menuItem.attr("post_type") != undefined) { filter.post_type = $menuItem.attr("post_type"); }
+        if ($menuItem.attr("per_page") != undefined) { filter.per_page = $menuItem.attr("per_page"); }
+
+
+        //Clear the HTML and append posts
+        $("#posts-container").html("");
+        getPhotosAndAppend();
+
+        //Change menu title to reflect filter
+        $(".menu-title.browse-community").html($menuItem.html());
+
+        loadMoreCheck();
+    });
+
+    //Loadmore button
+    $("a.load-more").click(function(ev){
+        ev.preventDefault();
+
+        filter.skip = filter.skip + filter.per_page;
+        getPhotosAndAppend();
+
+        loadMoreCheck();
+    });
+
+    //CHeck to see if loadmore needs to be hidden
+    function loadMoreCheck() {
+        var url = "http://" + document.domain + "/community-api/posts/counts?skip="+filter.skip+"&per_page="+filter.per_page+"&order_by="+filter.order_by+"&sort="+filter.sort+"&master="+filter.master+"&post_type="+filter.post_type;
+
+
+        $.getJSON(url,function(countData){
+
+
+            var totalPostCount = countData[0].post_count;
+
+            console.log(totalPostCount,filter.skip);
+
+            if (filter.skip + filter.per_page >= totalPostCount ) {
+                $("a.load-more").hide();
+            } else {
+                $("a.load-more").show();
+            }
+
+        });
+    }
+
+
+
+
+
+
     jQuery('.browse-panel').on("click", ".jq-open-filer", function(){
         jQuery(".browse-item").removeClass("item-active");
         jQuery(this).addClass("item-active");
@@ -22,7 +118,7 @@ jQuery(document).ready(function($) {
         jQuery('html, body').animate({scrollTop:0}, 'slow');
         return false;
     });
-    
+
     jQuery('body').on("click", ".jq-filter", function(e){
         jQuery(".browse-holder").addClass("browse-panel-opened");
         jQuery(".filter-fade-out").addClass("filter-fade-in");
@@ -30,7 +126,7 @@ jQuery(document).ready(function($) {
         jQuery('html, body').animate({scrollTop:0}, 'slow');
         return false;
     });
-    
+
     jQuery('.browse-panel').on("click", ".jq-close-filter", function(e){
         jQuery(".browse-item").removeClass("item-active");
         jQuery(".browse-holder").removeClass("browse-panel-opened");
@@ -38,14 +134,14 @@ jQuery(document).ready(function($) {
         jQuery(".layout-frame").removeClass("filter-popup-opened");
         e.preventDefault();
     });
-    
+
     jQuery('.basic-popup').on("click", ".jq-close-popup", function(e){
         jQuery(".basic-popup").removeClass("popup-opened");
         jQuery(".filter-fade-out").removeClass("filter-fade-in");
         jQuery(".layout-frame").removeClass("filter-popup-opened");
         e.preventDefault();
     });
-    
+
     jQuery('body').on("click", ".jq-open-reg-popup", function(e){
         jQuery(".reg-popup").addClass("popup-opened");
         jQuery(".filter-fade-out").addClass("filter-fade-in");
@@ -53,5 +149,5 @@ jQuery(document).ready(function($) {
         jQuery('html, body').animate({scrollTop:0}, 'slow');
         return false;
     });
-    
+
  });
