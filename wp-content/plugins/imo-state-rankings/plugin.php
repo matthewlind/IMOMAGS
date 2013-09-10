5a15e124d5dd5caa42947945bc8b4b4d2a5def6b
Salah <salah@salahstudios.com>
2013-09-10 13:55:41 -0400

    State rankings widget added

--- wp-content/plugins/imo-state-rankings/plugin.php
+++ wp-content/plugins/imo-state-rankings/plugin.php
@@ -10,7 +10,7 @@ require_once dirname( __FILE__ ) . '/settings-api-class.php';
 
 add_shortcode('rankings', 'state_rankings');
 function state_rankings($atts) {
-	wp_enqueue_style('state-rankings',plugins_url('state-rankings.css', __FILE__));
+	//wp_enqueue_style('state-rankings',plugins_url('state-rankings.css', __FILE__));
 	extract(
 		shortcode_atts(
 			array(
@@ -24,7 +24,7 @@ function state_rankings($atts) {
 	return state_rankings_display($week, $show, $initials);
 }
 
-function state_rankings_display($week, $show, $initials) {
+function state_rankings_display($week, $show, $initials, $widget) {
 	if($initials == 'true' || $initials == 'yes' || $initials == 'on' || $initials == 1 || $initials == '1') {$initials = 'true';} else {$initials = 'false';}
 	$rankingData = get_option('week_'.$week.'_data');
 	$dynamic_array_loop = '';
@@ -52,124 +52,244 @@ function state_rankings_display($week, $show, $initials) {
 	$dynamic_array_loop = 'array('.$dynamic_array_loop.')';
 	$dynamic_array = '$state_rankings_data = '.$dynamic_array_loop.';';
 	eval($dynamic_array);
-	
-	function rankSorting($a, $b) {
-		return $a['rank'] - $b['rank'];
-	}
-	
-	function rankListing($state_rankings_data, $show, $initials){
-		$rankTable = '
-		<style type="text/css">
-		table.rt-table, .rt-table td, .rt-table tr {
-			border: none;
-			background: #fff;
-			margin: 10px 0 10px 0;
-		}
-		.rt-table td {
-			padding: 25px;
-			background: #EDEDED;
-			vertical-align:top;
-			width: 45px;
-			height: 25px;
-		}
-		tr.rt-header {
-			border: 1px solid #D2D2D2;
-			color: #333;
-		}
-		.rt-name, .rt-points, .rt-trend, .rt-rank {
-			font-weight: bold;
-			size: 18px;
-			font-family: "Rokkitt", "stagregular", "StagMedium", serif;
-			text-shadow: 1px 1px #fff;
-			/*text-align: center;*/
-		
-		}
-		td.rt-trend {
-			text-align: right;
-		}
-		td.rt-notes {
-			width: 100%;
-			font-size: 14px;
-		}
-		.rt-name {
-			color: #22628C;
-			text-decoration:underline;
-		}
-		.rt-table .rt-header td {
-			font-size: 16px;
-			padding: 15px;
-			background: #fff;
-		}
-		tr.rt-divider, tr.rt-divider td {
-			background: #fff;
-			height: 10px;
-			padding: 0;
-		}
-		.rt-arrow-up {
-			float: left;
-			position: relative;
-			margin: 7px 16px -37px 0;
-			width: 15px;
-			height: 8px;
-			display: inline-block;
-		}
-		.rt-arrow-down {
-			float: left;
-			position: relative;
-			margin: 7px 16px -37px 0;
-			width: 15px;
-			height: 8px;
-			display: inline-block;
-		}
-		.center {
-			display: block;
-			text-align:center;
-			margin:0 auto;
-		}
-
-		</style>
-		<table id="rank-table" class="rt-table">
-			<tr class="rt-header"><td>Rank</td><td>State</td><td>Points</td><td>Trend</td><td>Notes</td></tr>
-			<tr class="rt-divider"><td></td><td></td><td></td><td></td><td></td></tr>
-		';
-		usort($state_rankings_data, 'rankSorting');
-		$count = 1;
-		foreach($state_rankings_data as $state_data) {
-			$rank = intval($state_data['rank']);
-			$name = $state_data['name'];
-			$points = $state_data['points'];
-			if($initials == 'true') {$label = $state_data['initials'];} else {$label = $name;}
-			if($state_data['notes']) {$notes = $state_data['notes'];} else {$notes = '&nbsp';}
-			$trendUp = 'trendUp';
-			$trendDown = 'trendDown';
-			if($state_data['trend']) {
-				$trend = $state_data['trend'];
-				$trend = str_replace('+', $trendUp, $trend);
-				$trend = str_replace('-', $trendDown, $trend);
-				$trend = str_replace('trendDown', '<span class="rt-arrow-down"></span>', $trend);
-				$trend = str_replace('trendUp', '<span class="rt-arrow-up"></span>', $trend);
-			} else {
-				$trend = '<span class="center">--</span>';
-			}
-
-			
-			if($count <= $show && $rank > 0) {
-				$rankTable .= '
-					<tr><td class="rt-rank">'.$rank.'</td><td class="rt-name">'.$label.'</td><td class="rt-points">'.$points.'</td><td class="rt-trend">'.$trend.'</td><td class="rt-notes">'.$notes.'</td></tr>
-					<tr class="rt-divider"><td></td><td></td><td></td><td></td><td></td></tr>
-				';
-				$count++;
-			}
-		}
-		$rankTable .= '
-		</table>
-		';
-		echo $rankTable;
-	}
 	//Default display by rank
-	rankListing($state_rankings_data, $show, $initials);
+	return rankListing($state_rankings_data, $week, $show, $initials, $widget);
 }
 
+function rankSorting($a, $b) {
+	return $a['rank'] - $b['rank'];
+}
+
+function rankListing($state_rankings_data, $week, $show, $initials, $widget){
+	$show = intval($show);
+	$ranksUrl = get_bloginfo('url').'/state-rankings';
+	$rankTable = '
+	<style type="text/css">
+	.right {float: right;}
+	.rt-logo {
+		width: 224px;
+		height: 127px;
+		margin: 0 auto 17px auto;
+		background: url("'.plugins_url( 'images/state-rankings-logo.jpg' , __FILE__ ).'") no-repeat;
+	}
+	table.rt-table, .rt-table td, .rt-table tr {
+		border: none;
+		border-spacing: 0px;
+		background: #fff;
+		margin: 10px 0 10px 0;
+		width: 99.5%;
+	}
+	.rt-table tr {
+		padding: 0px;
+	}
+	.rt-table td {
+		padding: 25px;
+		background: #EDEDED;
+		vertical-align:top;
+		width: 45px;
+		height: 25px;
+	}
+	table.rt-table tr.rt-header {
+		outline: 1px solid #D2D2D2;
+		color: #333;
+		font-size: 1.1em;
+	}
+	.rt-name, .rt-points, .rt-trend, .rt-rank {
+		font-weight: bold;
+		font-size: 1.35em;
+		font-family: "Rokkitt", "stagregular", "StagMedium", serif;
+		text-shadow: 1px 1px #fff;
+		/*text-align: center;*/
+	
+	}
+	.rt-rank, .rt-points {
+		text-align: center;
+	}
+	td.rt-trend {
+		text-align: right;
+	}
+	td.rt-notes {
+		width: 100%;
+		font-size: 0.9em;
+	}
+	.rt-name a {
+		color: #22628C;
+		text-decoration:underline;
+	}
+	.rt-table .rt-header td {
+		font-size: 0.9em;
+		padding: 15px 25px 15px 25px;
+		background: #fff;
+	}
+	tr.rt-divider, tr.rt-divider td {
+		background: #fff;
+
+		height: 10px;
+		padding: 0;
+	}
+	.rt-arrow-up {
+		float: left;
+		position: relative;
+		margin: 12px 16px -42px 0;
+		width: 15px;
+		height: 8px;
+		background: url("'.plugins_url( 'images/up.png' , __FILE__ ).'") no-repeat;
+		display: inline-block;
+	}
+	.rt-arrow-down {
+		float: left;
+		position: relative;
+		margin: 12px 16px -42px 0;
+		width: 15px;
+		height: 8px;
+		background: url("'.plugins_url( 'images/down.png' , __FILE__ ).'") no-repeat;
+		display: inline-block;
+	}
+	.center {
+		display: block;
+		text-align:center;
+		margin:0 auto;
+	}
+	
+	.widget_state_rankings_widget {
+		border: 1px solid #D2D2D2;
+		padding-bottom: 10px !important;
+	}
+	.widget_state_rankings_widget table.rt-table {
+		border: 1px solid #D2D2D2;
+		width: 295px;
+		margin: 10px auto 10px auto;
+	}
+	
+	.widget_state_rankings_widget .rt-table td {
+		padding: 10px;
+		width: auto;
+		height: auto;
+	}
+		
+	.widget_state_rankings_widget table.rt-table tr.rt-header {
+		outline: none;
+	}
+	.widget_state_rankings_widget .rt-name,
+	.widget_state_rankings_widget .rt-points,
+	.widget_state_rankings_widget .rt-trend,
+	.widget_state_rankings_widget .rt-rank {
+		font-weight: bold;
+		font-size: 1em !important;
+	}
+	.widget_state_rankings_widget .rt-trend {
+		padding-right: 22px !important;
+	}
+	.widget_state_rankings_widget .rt-rank {
+		padding-right: 12px !important;
+	}
+	.widget_state_rankings_widget .rt-points {
+		padding-right: 17px !important;
+	}
+	.widget_state_rankings_widget .rt-name {
+		padding-left: 12px !important;
+	}
+	.widget_state_rankings_widget tr.rt-divider,
+	.widget_state_rankings_widget tr.rt-divider td {
+		padding: 0;
+		height: 2px;
+	}
+	tr.rt-divider-last,
+	tr.rt-divider-last td {
+		height: 0px !important;
+	}
+	.widget_state_rankings_widget .rt-arrow-up {
+		margin: 8px 0px -42px 4px;
+	}
+	.widget_state_rankings_widget .rt-arrow-down {
+		margin: 8px 0px -42px 4px;
+	}
+	.widget_state_rankings_widget .rt-table .rt-header td {
+		font-family: Helvetica, Arial, sans-serif;
+		font-size: 0.8em;
+	}
+	.rt-widget-top {
+		font-weight: bold;
+		font-family: Helvetica, Arial, sans-serif;
+		font-size: 0.8em;
+		text-transform: uppercase;
+		padding: 0 2px 0 1px;
+	}
+	.rt-widget-bottom {
+		font-weight: bold;
+		font-size: 1em;
+		line-height: 1em;
+		margin: 5px 0 10px 0;
+	}
+	a.rt-bottom-link {
+		margin: 10px 2px 0;
+		font-family: Helvetica, Arial, sans-serif;
+		font-size: 0.6em;
+	}
+	</style>';
+	if(!empty($widget)) $rankTable .= '
+	<div class="rt-logo"></div>
+	<div class="rt-widget-top">
+		Week '.$week.'<a href="'.$ranksUrl.'/week-'.$week.'"class="right">See All 20 Rankings</a>
+	</div>
+	';
+	$rankTable .= '
+	<table class="rank-table rt-table">
+		<tr class="rt-header"><td>Rank</td><td>State</td><td>Points</td><td>Trend</td>';
+	if(empty($widget)) $rankTable .= '<td>Notes</td>';
+	$rankTable .= '</tr>
+		<tr class="rt-divider"><td></td><td></td><td></td><td></td>
+	';
+	if(empty($widget)) $rankTable .= '<td></td>';
+	$rankTable .= '</tr>';
+	usort($state_rankings_data, 'rankSorting');
+	$count = 1;
+	foreach($state_rankings_data as $state_data) {
+		$rank = intval($state_data['rank']);
+		$name = $state_data['name'];
+		$stateUrl = get_bloginfo('url').'/community/report/'.str_replace(' ', '-', strtolower($name));
+		$points = $state_data['points'];
+		if($initials == 'true') {$label = $state_data['initials'];} else {$label = $name;}
+		if($state_data['notes']) {$notes = $state_data['notes'];} else {$notes = '&nbsp';}
+		$trendUp = 'trendUp';
+		$trendDown = 'trendDown';
+		if($state_data['trend']) {
+		$trend = $state_data['trend'];
+		$trend = str_replace('+', $trendUp, $trend);
+		$trend = str_replace('-', $trendDown, $trend);
+		$trend = str_replace('trendDown', '<span class="rt-arrow-down"></span>', $trend);
+		$trend = str_replace('trendUp', '<span class="rt-arrow-up"></span>', $trend);
+		} else {
+		$trend = '<span class="center">--</span>';
+		}
+
+		
+		if($count <= $show && $rank > 0) {
+		$rankTable .= '
+			<tr><td class="rt-rank">'.$rank.'</td><td class="rt-name"><a href="'.$stateUrl.'">'.$label.'</a></td><td class="rt-points">'.$points.'</td><td class="rt-trend">'.$trend.'</td>';
+		if(empty($widget)) $rankTable .= '<td class="rt-notes">'.$notes.'</td></tr>';
+		if($show == $count) $rtDividerLast = ' rt-divider-last';
+		$rankTable .= '
+			<tr class="rt-divider'.$rtDividerLast.'"><td></td><td></td><td></td><td></td>
+		';
+		if(empty($widget)) $rankTable .= '<td></td>';
+		$rankTable .= '</tr>';
+		$count++;
+		}
+	}
+	$rankTable .= '
+	</table>
+	';
+	$widgetBottom = '
+	<div class="rt-widget-bottom"><a href="'.get_bloginfo('url').'/community/report">Share your activity</a> to push YOUR state to the top of the list!<a href="'.$ranksUrl.'" class="rt-bottom-link right">What the heck is this?</a></div>
+	';
+	if(!empty($widget)) $rankTable .= $widgetBottom;
+	return $rankTable;	
+}
+
+/**
+ * Options Page Settings Class
+ */
 class imo_state_rankings_initiate {
     private $settings_api;
 
@@ -388,3 +508,62 @@ class imo_state_rankings_initiate {
 }
 
 $settings = new imo_state_rankings_initiate();
+
+
+
+/**
+ * Widget Class
+ */
+class state_rankings_widget extends WP_Widget {
+    /** constructor -- name this the same as the class above */
+    function state_rankings_widget() {
+        parent::WP_Widget(false, $name = 'IMO State Rankings');	
+    }
+ 
+    /** @see WP_Widget::widget -- do not rename this */
+    function widget($args, $instance) {	
+        extract( $args );
+        $week 		= $instance['week'];
+		$show 		= $instance['show'];
+        $initials 	= $instance['initials'];
+        
+		echo $before_widget;
+		echo state_rankings_display($week, $show, $initials, 'yes');
+		echo $after_widget;
+    }
+ 
+    /** @see WP_Widget::update -- do not rename this */
+    function update($new_instance, $old_instance) {		
+		$instance = $old_instance;
+		$instance['week'] = strip_tags($new_instance['week']);
+		$instance['show'] = strip_tags($new_instance['show']);
+		$instance['initials'] = strip_tags($new_instance['initials']);
+        return $instance;
+    }
+ 
+    /** @see WP_Widget::form -- do not rename this */
+    function form($instance) {	
+ 
+        $week 		= esc_attr($instance['week']);
+		$show 		= esc_attr($instance['show']);
+        $initials	= esc_attr($instance['initials']);
+        ?>
+		<p>
+			Which week? <input style="width: 30px;" id="<?php echo $this->get_field_id('week'); ?>" name="<?php echo $this->get_field_name('week'); ?>" type="text" value="<?php echo $week; ?>" /><br/>
+			How many? <input style="width: 30px;margin-left: 9px;"  id="<?php echo $this->get_field_id('show'); ?>" name="<?php echo $this->get_field_name('show'); ?>" type="text" value="<?php echo $show; ?>" />
+		</p>
+		<p>
+        Use initials? 
+        <?php if($initials == 'yes') {?>
+        <input type="radio" name="<?php echo $this->get_field_name('initials'); ?>" value="yes" checked="checked">Yes &nbsp;<input type="radio" name="<?php echo $this->get_field_name('initials'); ?>" value="no" />No
+        <?php } else { ?>
+        <input type="radio" name="<?php echo $this->get_field_name('initials'); ?>" value="yes">Yes &nbsp;<input type="radio" name="<?php echo $this->get_field_name('initials'); ?>" value="no" checked="checked" />No
+        <?php } ?>
+        </p>
+        <?php 
+    }
+ 
+ 
+} // end class state_rankings_widget
+add_action('widgets_init', create_function('', 'return register_widget("state_rankings_widget");'));
+?>
