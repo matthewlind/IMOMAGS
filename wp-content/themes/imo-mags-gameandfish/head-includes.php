e74963bc33b565409da34c3939cf3c86f95aa27e
Fox Bowden <fox.bowden@imoutdoors.com>
2013-09-23 16:01:18 -0400

    removing GF heatmap tracking

--- wp-content/themes/imo-mags-gameandfish/head-includes.php
+++ wp-content/themes/imo-mags-gameandfish/head-includes.php
@@ -46,9 +46,3 @@ if(document.location.protocol=='http:'){
 }
 </script>
 <!-- END Tynt Script -->
-<script type="text/javascript">
-setTimeout(function(){var a=document.createElement("script");
-var b=document.getElementsByTagName("script")[0];
-a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0010/7765.js?"+Math.floor(new Date().getTime()/3600000);
-a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
-</script>
