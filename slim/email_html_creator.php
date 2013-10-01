d0308186e6c0cbb55a1cc7b9f5fc14bc060da45b
Aaron Baker <aaron.baker@imoutdoors.com>
2013-10-01 14:56:25 -0400

    small bug fix

--- slim/email_html_creator.php
+++ slim/email_html_creator.php
@@ -3,49 +3,49 @@
 function generateEmailSubject($emailData){
 	$subject = "Whitetail Community Update";
 	$commentCount = $emailData['highest_comment_count'];
-	
+
 	$featuredEventCommentCount = $emailData['featured_event_comment_count'];
 
-	
+
 	if ($commentCount > 0) {
-		
+
 		$otherCount = $featuredEventCommentCount - 1;
-		
+
 		$subject = $emailData['featured_event']->event_display_name . " and $otherCount Others Commented On " . $emailData['featured_event']->post_title;
-		
+
 		if ($featuredEventCommentCount == 1) {
 			$subject = $emailData['featured_event']->event_display_name . " Commented On " . $emailData['featured_event']->post_title;
 		}
 		if ($featuredEventCommentCount == 2) {
 			$subject = $emailData['featured_event']->event_display_name . " and Another Commented On " . $emailData['featured_event']->post_title;
-		}		
-		
+		}
+
 	} else {//If there are no comments
-	
+
 		$shareCount = $emailData['featured_shared_event_count'];
-	
+
 		$subject = $emailData['featured_shared_event']->post_title . " was shared $shareCount times";
-		
-		
-		
+
+
+
 	}
 
-	
-	
+
+
 /*
 	echo "<pre>";
 	print_r($emailData);
 	echo "</pre>";
 */
-		
-	
-	
+
+
+
 	return $subject;
 }
 
 function generateEmailHTML($emailData) {
-	
-//echo json_encode($emailData);	
+
+//echo json_encode($emailData);
 
 $display_name = $emailData['display_name'];
 $user_score = $emailData['user_score'];
@@ -61,32 +61,32 @@ $templateHeader = <<<EOFEOFEOF
 
 
 <style type="text/css">
-/* ------------------------------------- 
-		GLOBAL 
+/* -------------------------------------
+		GLOBAL
 ------------------------------------- */
-* { 
+* {
 	margin:0;
 	padding:0;
 }
 * { font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif; }
 
-img { 
-	max-width: 100%; 
+img {
+	max-width: 100%;
 }
 .collapse {
 	margin:0;
 	padding:0;
 }
 body {
-	-webkit-font-smoothing:antialiased; 
-	-webkit-text-size-adjust:none; 
-	width: 100%!important; 
+	-webkit-font-smoothing:antialiased;
+	-webkit-text-size-adjust:none;
+	width: 100%!important;
 	height: 100%;
 }
 
 
-/* ------------------------------------- 
-		ELEMENTS 
+/* -------------------------------------
+		ELEMENTS
 ------------------------------------- */
 
 
@@ -116,7 +116,7 @@ p.callout {
 table.social {
 /* 	padding:15px; */
 	background-color: #ebebeb;
-	
+
 }
 .social .soc-btn {
 	padding: 3px 7px;
@@ -132,13 +132,13 @@ a.tw { background-color: #1daced!important; }
 a.gp { background-color: #DB4A39!important; }
 a.ms { background-color: #000!important; }
 
-.sidebar .soc-btn { 
+.sidebar .soc-btn {
 	display:block;
 	width:100%;
 }
 
-/* ------------------------------------- 
-		HEADER 
+/* -------------------------------------
+		HEADER
 ------------------------------------- */
 table.head-wrap { width: 100%;}
 
@@ -146,14 +146,14 @@ table.head-wrap { width: 100%;}
 .header.container table td.label { padding: 15px; padding-left:0px;}
 
 
-/* ------------------------------------- 
-		BODY 
+/* -------------------------------------
+		BODY
 ------------------------------------- */
 table.body-wrap { width: 100%;}
 
 
-/* ------------------------------------- 
-		FOOTER 
+/* -------------------------------------
+		FOOTER
 ------------------------------------- */
 table.footer-wrap { width: 100%;	clear:both!important;
 }
@@ -161,12 +161,12 @@ table.footer-wrap { width: 100%;	clear:both!important;
 .footer-wrap .container td.content p {
 	font-size:10px;
 	font-weight: bold;
-	
+
 }
 
 
-/* ------------------------------------- 
-		TYPOGRAPHY 
+/* -------------------------------------
+		TYPOGRAPHY
 ------------------------------------- */
 h1,h2,h3,h4,h5,h6 {
 font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif; line-height: 1.1; margin-bottom:15px; color:#000;
@@ -187,10 +187,10 @@ h6 { font-weight:900; font-size: 14px; text-transform: uppercase; color:#444;}
 
 .collapse { margin:0!important;}
 
-p, ul { 
-	margin-bottom: 10px; 
-	font-weight: normal; 
-	font-size:14px; 
+p, ul {
+	margin-bottom: 10px;
+	font-weight: normal;
+	font-size:14px;
 	line-height:1.6;
 }
 p.lead { font-size:17px; }
@@ -201,8 +201,8 @@ ul li {
 	list-style-position: inside;
 }
 
-/* ------------------------------------- 
-		SIDEBAR 
+/* -------------------------------------
+		SIDEBAR
 ------------------------------------- */
 ul.sidebar {
 	background:#ebebeb;
@@ -228,9 +228,9 @@ ul.sidebar li a h1,ul.sidebar li a h2,ul.sidebar li a h3,ul.sidebar li a h4,ul.s
 
 
 
-/* --------------------------------------------------- 
+/* ---------------------------------------------------
 		RESPONSIVENESS
-		Nuke it from orbit. It's the only way to be sure. 
+		Nuke it from orbit. It's the only way to be sure.
 ------------------------------------------------------ */
 
 /* Set a max-width, and make it display as block so it will automatically stretch to that width, but will also shrink down on a phone or something */
@@ -246,7 +246,7 @@ ul.sidebar li a h1,ul.sidebar li a h2,ul.sidebar li a h3,ul.sidebar li a h4,ul.s
 	padding:15px;
 	max-width:600px;
 	margin:0 auto;
-	display:block; 
+	display:block;
 }
 
 /* Let's make sure tables in the content area are 100% wide */
@@ -259,9 +259,9 @@ ul.sidebar li a h1,ul.sidebar li a h2,ul.sidebar li a h3,ul.sidebar li a h4,ul.s
 	float:left;
 }
 .column tr td { padding: 15px; }
-.column-wrap { 
-	padding:0!important; 
-	margin:0 auto; 
+.column-wrap {
+	padding:0!important;
+	margin:0 auto;
 	max-width:600px!important;
 }
 .column table { width:100%;}
@@ -275,24 +275,24 @@ ul.sidebar li a h1,ul.sidebar li a h2,ul.sidebar li a h3,ul.sidebar li a h4,ul.s
 .clear { display: block; clear: both; }
 
 
-/* ------------------------------------------- 
+/* -------------------------------------------
 		PHONE
 		For clients that support media queries.
-		Nothing fancy. 
+		Nothing fancy.
 -------------------------------------------- */
 @media only screen and (max-width: 600px) {
-	
+
 	a[class="btn"] { display:block!important; margin-bottom:10px!important; background-image:none!important; margin-right:0!important;}
 
 	div[class="column"] { width: auto!important; float:none!important;}
-	
+
 	table.social div[class="column"] {
 		width:auto!important;
 	}
 
 }
 
-/* ------------------------------------------- 
+/* -------------------------------------------
 		IMO DESIGN
 -------------------------------------------- */
 
@@ -308,7 +308,7 @@ h4 {
 
 }
 
-.btn { 
+.btn {
 	margin-top:10px;
 }
 
@@ -357,10 +357,12 @@ p.light-gray {
 						</td>&#13;
 					</tr></table></div><!-- /content -->&#13;
 			&#13;
-			
+
 EOFEOFEOF;
 
-
+//*************START FOREACH POST**************
+//*************START FOREACH POST**************
+//*************START FOREACH POST**************
 foreach ($emailData['posts'] as $post) {
 
 $post_title = $post['post_title'];
@@ -399,44 +401,49 @@ $templateContentTop = <<<EOFEOFEOFGG
 						</td>&#13;
 						<td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin-top: 0; margin-right: 0; margin-bottom: 0; margin-left: 0; padding-top: 0; padding-right: 0; padding-bottom: 0; padding-left: 0;">&#13;
 							<h4 style="font-family: 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; line-height: 1.1; color: #000; font-weight: 500; font-size: 23px; margin-top: 0; margin-right: 0; margin-bottom: 0px; margin-left: 0; padding-top: 0; padding-right: 0; padding-bottom: 0; padding-left: 0;"><a href="$post_url" style="color:#000000;text-decoration:none;font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif; line-height: 1.1; margin-bottom:15px; color:#000;">$post_title</a><small style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 60%; color: #6f6f6f; line-height: 0; text-transform: none; margin-top: 0; margin-right: 0; margin-bottom: 0; margin-left: 0; padding-top: 0; padding-right: 0; padding-bottom: 0; padding-left: 0;"></small></h4><p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-weight: normal; font-size: 14px; line-height: 1.6; margin-top: 0; margin-right: 0; margin-bottom: 10px; margin-left: 0; padding-top: 0; padding-right: 0; padding-bottom: 0; padding-left: 0;">$post_comment_string</p>&#13;
-							
+
 EOFEOFEOFGG;
 
 $shareCount = 0;
 $commentCount = 0;
 $templateContentMiddle = "";
+	//*************START FOREACH POST EVENT**************
+	//*************START FOREACH POST EVENT**************
+	//*************START FOREACH POST EVENT**************
 foreach ($post['events'] as $event) {
 
-	
+
 	$profileURL = "http://www.northamericanwhitetail.com/profile/" . $event->event_username;
 	if ($event->event_type == 'share')
 		$shareCount++;
-	
+
 	if ($event->event_type == "comment") {
-		
+
 		$commentCount++;
 		$templateContentMiddle .= <<<EOFEOFEOFSS
-		
-		
+
+
 <p class="" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-weight: normal; font-size: 14px; line-height: 1.6; margin-top: 0; margin-right: 0; margin-bottom: 10px; margin-left: 0; padding-top: 0; padding-right: 0; padding-bottom: 0; padding-left: 0;"> &#13;
 								&#13;
 								<a href="$profileURL"><img src="http://www.northamericanwhitetail.com/avatar?uid=$event->uid" class="avatar" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; max-width: 100%; vertical-align: middle; float: left; margin-top: 0; margin-right: 5px; margin-bottom: 0; margin-left: 0; padding-top: 0; padding-right: 0; padding-bottom: 0; padding-left: 0;" align="left" height=25 width=25 /></a><b style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin-top: 0; margin-right: 0; margin-bottom: 0; margin-left: 0; padding-top: 0; padding-right: 0; padding-bottom: 0; padding-left: 0;"><a href="$profileURL" style="text-decoration:none;color:#000000;">$event->event_display_name</a></b><br style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin-top: 0; margin-right: 0; margin-bottom: 0; margin-left: 0; padding-top: 0; padding-right: 0; padding-bottom: 0; padding-left: 0;" /><small style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin-top: 0; margin-right: 0; margin-bottom: 0; margin-left: 0; padding-top: 0; padding-right: 0; padding-bottom: 0; padding-left: 0;">commented $event->time_ago_string</small>&#13;
 							</p>&#13;
-							
+
 EOFEOFEOFSS;
 
 
 	}
-	
-}
 
+}
+	//*************END FOREACH POST EVENT**************
+	//*************END FOREACH POST EVENT**************
+	//*************END FOREACH POST EVENT**************
 
 if ($commentCount > 0) {
 	$shareMargin = "30px";
 	$goButtonText = "Join the Conversation";
 }
 else {
-	$shareMargin = "0px";	
+	$shareMargin = "0px";
 	$goButtonText = "Check it Out";
 }
 
@@ -447,31 +454,25 @@ if ($shareCount>0) {
 	else
 		$shareCountText = "$shareCount People";
 
-	
+
 	$templateContentMiddle .= <<<EOFEOFEOFSS
 
 
-							
+
 							<p class="share-message" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-weight: normal; font-size: 14px; line-height: 1.6; margin-top: 0; margin-right: 0; margin-bottom: 10px; margin-left: $shareMargin; padding-top: 0; padding-right: 0; padding-bottom: 0; padding-left: 0;"> &#13;
 								&#13;
 								<b style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin-top: 0; margin-right: 0; margin-bottom: 0; margin-left: 0; padding-top: 0; padding-right: 0; padding-bottom: 0; padding-left: 0;">$shareCountText</b><br style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin-top: 0; margin-right: 0; margin-bottom: 0; margin-left: 0; padding-top: 0; padding-right: 0; padding-bottom: 0; padding-left: 0;" /><small style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; margin-top: 0; margin-right: 0; margin-bottom: 0; margin-left: 0; padding-top: 0; padding-right: 0; padding-bottom: 0; padding-left: 0;">shared your post</small>&#13;
 							</p>&#13;
 
-							
+
 EOFEOFEOFSS;
 
-	
+
 }
-
-
-
-
-
-
 
 $templateContentBottom = <<<EOFEOFEOFD
 
-							
+
 							<a href="$post_url" class="btn" style="margin: 0;padding: 10px 16px;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;color: #FFF;text-decoration: none;background-color: #c22d26;font-weight: bold;margin-right: 10px;text-align: center;cursor: pointer;display: inline-block;margin-top: 10px;font-size:14px;">$goButtonText »</a>&#13;
 						</td>&#13;
 					</tr></table></div>&#13;
@@ -480,11 +481,11 @@ EOFEOFEOFD;
 
 
 
-$templateContent .= $templateContentTop . $templateContentMiddle . $templateContentBottom;	
+$templateContent .= $templateContentTop . $templateContentMiddle . $templateContentBottom;
 }
 
-		
-			
+
+
 $templateFooter = <<<EOFEOFEOFS
 
 			<!-- content -->&#13;
