ee049f57d5e9cae97eec2839bd86a701a4dadabf
Aaron Baker <aaron.baker@imoutdoors.com>
2013-11-06 15:57:38 -0500

    Adds BWP minify. Updates to user profiles and BRIGHTCOVE FIX FOR BROCK

--- wp-content/plugins/bc_embed/plugin.php
+++ wp-content/plugins/bc_embed/plugin.php
@@ -174,6 +174,83 @@ the rest of the HTML is processed and the page load is complete, remove the line
 
 
 
+/**
+ * FISHING Embed Handler for Brightcove videos.
+ */
+function wp_embed_handler_brightcove_fishing ( $matches, $attr, $url, $rawattr ) {
+    //Set width and height for the default video player
+    $width = 300;
+    $height = 169;
+
+    // videoid should be alpha numeric. remove all and any non numbers from the string.
+    // we don't want to use an int conversion, becuase ids are very large and will overflow.
+    $videoid = preg_replace('/[^0-9]/', '', esc_attr($matches[1]));
+    global $the_ID;
+    $video_link = !empty($the_ID) ? get_permalink($the_ID) :  site_url() . $_SERVER['REQUEST_URI'];
+
+
+    $adServerURL = "http://ad.doubleclick.net/pfadx/" .  get_option("dart_domain", _imo_dart_guess_domain())  ."/video";
+
+
+    $output = '
+
+<!-- Start of Brightcove Player -->
+
+<div style="display:none">
+
+</div>
+
+<!--
+By use of this code snippet, I agree to the Brightcove Publisher T and C
+found at https://accounts.brightcove.com/en/terms-and-conditions/.
+-->
+
+<script language="JavaScript" type="text/javascript" src="http://admin.brightcove.com/js/BrightcoveExperiences.js"></script>
+
+<object id="myExperience%3$s" class="BrightcoveExperience">
+  <param name="bgcolor" value="#FFFFFF" />
+  <param name="width" value="480" />
+  <param name="height" value="270" />
+  <param name="playerID" value="2436822682001" />
+  <param name="playerKey" value="AQ~~,AAAA-01d-uE~,FiwRPPEEyN5QUjsXPxydyLn0dbP3yaNm" />
+  <param name="isVid" value="true" />
+  <param name="isUI" value="true" />
+  <param name="dynamicStreaming" value="true" />
+
+  <param name="@videoPlayer" value="%3$s" />
+
+  <param name="media_delivery" value="http" />
+  <param name="adServerURL" value="%8$s" />
+</object>
+
+<!--
+This script tag will cause the Brightcove Players defined above it to be created as soon
+as the line is read by the browser. If you wish to have the player instantiated only after
+the rest of the HTML is processed and the page load is complete, remove the line.
+-->
+<script type="text/javascript">brightcove.createExperiences();</script>
+
+<!-- End of Brightcove Player -->';
+
+
+
+
+    $embed = sprintf( $output,
+         get_option("bc_player_id", _BC_DEFAULT_PLAYER_ID ),
+        _BC_PUBLISHER_ID,
+        $videoid,
+        $width,
+        $height,
+        get_option("bc_player_key", _BC_DEFAULT_PLAYER_KEY),
+
+        $video_link,
+  $adServerURL
+    );
+    return apply_filters( 'embed_brightcove', $embed, $matches, $attr, $url, $rawattr );
+}
+
+
+
 
 
 
@@ -193,6 +270,7 @@ function _bc_embed_format_tag($brightcove_ID) {
 }
 
 wp_embed_register_handler( "brightcove", '#http://brightcove=([^]]*)#i', "wp_embed_handler_brightcove");
+wp_embed_register_handler( "brightcove", '#http://brightcovefishing=([^]]*)#i', "wp_embed_handler_brightcove_fishing");
 wp_embed_register_handler( "brightcovemobile", '#http://brightcovemobile=([^]]*)#i', "wp_embed_handler_brightcove_mobile");
 
 /******************************************************************************************
