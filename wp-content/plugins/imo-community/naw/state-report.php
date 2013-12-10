a30f8311da328911f2bd36fda28b4f3a7cb8f401
Aaron Baker <aaron.baker@imoutdoors.com>
2013-12-10 15:47:08 -0500

    State pages for new NAW community update

---
+++ wp-content/plugins/imo-community/naw/state-report.php
@@ -0,0 +1,184 @@
+<?php
+
+/**
+ * Template Name: report
+ * Description: Community Rut Reports Page
+ *
+ * @package carrington-business
+ *
+ * This file is part of the Carrington Business Theme for WordPress
+ * http://crowdfavorite.com/wordpress/themes/carrington-business/
+ *
+ * Copyright (c) 2008-2011 Crowd Favorite, Ltd. All rights reserved.
+ * http://crowdfavorite.com
+ *
+ * **********************************************************************
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * **********************************************************************
+ */
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+
+//Gallery Scripts
+wp_enqueue_script('flexslider-js',plugins_url('imo-flex-gallery/jquery.flexslider.js'));
+wp_enqueue_script('flex-gallery-js',plugins_url('imo-flex-gallery/flex-gallery.js'));
+wp_enqueue_script('jquery-mobile',plugins_url('imo-flex-gallery/jquery.mobile.custom.min.js'));
+wp_enqueue_script('jquery-ui-slide-effect',plugins_url('imo-flex-gallery/jquery-ui-slide-effect.min.js'));
+wp_enqueue_script('jquery-scrollface',plugins_url('imo-flex-gallery/jquery.scrollface.min.js'));
+wp_enqueue_script('jquery-buffet',plugins_url('imo-flex-gallery/jquery.buffet.min.js'));
+wp_enqueue_script('jquery-mousewheel',plugins_url('imo-flex-gallery/jquery.mousewheel.min.js'));
+wp_enqueue_script('perfect-scrollbar-js',plugins_url('imo-flex-gallery/perfect-scrollbar-0.4.3.with-mousewheel.min.js'));
+wp_enqueue_style('ajax-gallery-css',plugins_url('imo-flex-gallery/flex-gallery.css','imo-flex-gallery'));
+wp_enqueue_style('perfect-scrollbar-css',plugins_url('imo-flex-gallery/perfect-scrollbar-0.4.3.min.css'));
+
+get_header();
+imo_sidebar("community");
+
+the_post();
+$displayStyle = "display:none;";
+$loginStyle = "";
+
+if ( is_user_logged_in() ) {
+
+	$displayStyle = "";
+	$loginStyle = "display:none;";
+
+	wp_get_current_user();
+
+	$current_user = wp_get_current_user();
+    if ( !($current_user instanceof WP_User) )
+         return;
+}
+
+
+include 'common-templates.php';
+$crop = "/convert?w=650&h=650&fit=crop&rotate=exif";
+if(mobile()){
+	$crop = "/convert?w=458&h=458&fit=crop&rotate=exif";
+}
+
+
+
+$state = get_query_var("state");
+
+$stateSlugToAbbv = array("alabama"=>"AL",
+"alaska"=>"AK","arizona"=>"AZ","arkansas"=>"AR","california"=>"CA","colorado"=>"CO","connecticut"=>"CT","delaware"=>"DE","district-of-columbia"=>"DC","florida"=>"FL","georgia"=>"GA","hawaii"=>"HI","idaho"=>"ID","illinois"=>"IL","indiana"=>"IN","iowa"=>"IA","kansas"=>"KS","kentucky"=>"KY","louisiana"=>"LA","maine"=>"ME","maryland"=>"MD","massachusetts"=>"MA","michigan"=>"MI","minnesota"=>"MN","mississippi"=>"MS","missouri"=>"MO","montana"=>"MT","nebraska"=>"NE","nevada"=>"NV","new-hampshire"=>"NH","new-jersey"=>"NJ","new-mexico"=>"NM","new-york"=>"NY","north-carolina"=>"NC","north-dakota"=>"ND","ohio"=>"OH","oklahoma"=>"OK","oregon"=>"OR","pennsylvania"=>"PA","rhode-island"=>"RI","south-carolina"=>"SC","south-dakota"=>"SD","tennessee"=>"TN","texas"=>"TX","utah"=>"UT","vermont"=>"VT","virginia"=>"VA","washington"=>"WA","west-virginia"=>"WV","wisconsin"=>"WI","wyoming"=>"WY","alberta"=>"AB","british-columbia"=>"BC","manitoba"=>"MB","new-brunswick"=>"NB","newfoundland-and-labrador"=>"NL","northwest-territories"=>"NT","nova-scotia"=>"NS","nunavut"=>"NU","ontario"=>"ON","prince-edward-island"=>"PE","quebec"=>"QC","saskatchewan"=>"SK","yukon"=>"YT");
+
+
+ $shortState = $stateSlugToAbbv[$state];
+echo "<h1>STATE: $state: $shortState</h1>";
+
+?>
+<!-- *********************************************************** -->
+<!-- ***************** UNDERSCORE TEMPLATE ********************* -->
+<!-- *********************************************************** -->
+<script type="text/template" id="post-template">
+<%
+if(post.score == 1){
+	niceScore = post.score + ' Point';
+}else{
+	niceScore = post.score + ' Points';
+}
+%>
+	<div class="dif-post">
+       <% if(post.img_url){ %>
+	        <div class="feat-img">
+	            <a href="/community/<%= post.id %>"><img class="feat-img" src="<%= post.img_url %>" alt="<%= post.title %>" title="<%= post.img_url %>" /></a>
+	        </div>
+        <% }else{ %>
+        	 <div class="feat-img">
+	            <a href="/community/<%= post.id %>"><img class="feat-img" src="<?php echo plugins_url('images/crosshair.jpg' , __FILE__ ); ?>" alt="<%= post.title %>" title="<%= post.img_url %>" /></a>
+	        </div>
+        <% } %>
+        <div class="dif-post-text">
+            <h3><a href="/photos/<%= post.id %>"><%= post.title %></a></h3>
+            <div class="profile-panel">
+                <div class="profile-photo">
+                    <a href="/profile/<%= post.user_nicename %>"><img src="/avatar?uid=<%= post.user_id %>" alt="<%= post.user_nicename %>" title="<%= post.user_nicename %>" /></a>
+                </div>
+                <div class="profile-data">
+                    <h4><a href="/profile/<%= post.user_nicename %>"><%= post.display_name %></a></h4>
+                    <ul class="prof-tags">
+                        <!--<li><a href="#"><%= post.state %></a></li>-->
+                        <li><a href="/<%= post.post_type %>" style="text-transform:capitalize;"><%= post.post_type %></a></li>
+                    </ul>
+                    <ul class="replies">
+                        <li><a href="/photos/<%= post.id %>#reply_field"><%= post.comment_count %> Reply</a></li>
+						<li><%= niceScore %></li>
+                    </ul>
+                    <ul class="prof-like">
+                    	<li>
+                    		<div addthis:url="http://<?php echo $_SERVER['SERVER_NAME']; ?>/photos/<%= post.id %>" addthis:title="<%= post.title %>" class="addthis_toolbox addthis_default_style ">
+								<a class="addthis_button_facebook_like"fb:like:layout="button_count"></a>
+							</div>
+
+                       </li>
+                    </ul>
+                </div>
+            </div>
+            <% if (post.master == 1) {  %><span class="badge"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/pic/badge-ma.png" alt="Master Angler" /></span><% } %>
+        </div>
+    </div>
+</script>
+<!-- *********************************************************** -->
+<!-- *********************************************************** -->
+<!-- *********************************************************** -->
+
+<div class="page-community">
+    <div class="general general-com">
+    	<div class="custom-title clearfix">
+    		<img src="<?php echo plugins_url('images/naw-plus.png' , __FILE__ ); ?>" alt="NAW Community" class="custom-tite-logo">
+            <div class="title-crumbs">
+            	<h1>State Rut Reports</h1>
+                <div class="sponsor"><?php imo_dart_tag("240x60"); ?></div>
+			</div>
+        </div>
+
+		<div class="custom-slider-section">
+            <?php //echo do_shortcode('[imo-slideshow community=true]'); ?>
+        </div>
+        <div class="photo-link-area">
+            <div id="fileupload">
+                <div class="fileupload-buttonbar ">
+                    <label class="upload-button share-photo">
+                        <span class="add-photo-link">Share Your Photo</span>
+                        <input id="image-upload" class="common-image-upload" type="file" name="photo-upload">
+                    </label>
+                </div>
+            </div>
+        </div>
+
+        <div class="btn-group btn-bar">
+          <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+            <span class="menu-title browse-community">Browse by State</span> <span class="caret"></span>
+          </button>
+          <ul class="dropdown-menu filter" role="menu">
+            <li><a href="" class="filter-menu" order_by="created" id="filter-menu-default">Latest</a></li>
+            <li><a href="" class="filter-menu" order_by="view_count" >Popular</a></li>
+            <li><a href="" class="filter-menu" order_by="score_today" >Trending Today</a></li>
+<!--             <li><a href="" class="filter-menu" order_by="score_week" >Trending This Week</a></li> -->
+            <li><a href="" class="filter-menu" order_by="created" post_type="new-york" >New York</a></li>
+            <li><a href="" class="filter-menu" order_by="created" post_type="california" >California</a></li>
+
+          </ul>
+        </div>
+
+        <div class="dif-posts">
+			<div id="posts-container" state="<?php echo $shortState; ?>"></div>
+         </div>
+         <div data-position="<?php echo $dataPos = $dataPos + 1; ?>" class="pager-holder js-responsive-section">
+            <a href="#" class="btn-base load-more" style="display:block;">Load More</a>
+
+            <a href="#" class="go-top jq-go-top">go top</a>
+
+            <img src="/wp-content/themes/imo-mags-parent/images/ajax-loader.gif" id="ajax-loader" style="display:none;"/>
+        </div>
+		<?php social_footer(); ?>
+		<div class="hr mobile-hr"></div>
+		<a href="#" class="back-top jq-go-top">back to top</a>
+
+    </div>
+</div>
+
+<?php get_footer(); ?>
