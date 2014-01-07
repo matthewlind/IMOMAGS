31151f416f6931b925a1cb47607363604e7bb4a8
Aaron Baker <aaron.baker@imoutdoors.com>
2014-01-07 13:06:15 -0500

    Troubleshooting for MA emails

--- community-api/index.php
+++ community-api/index.php
@@ -1109,15 +1109,15 @@ function sendMAEmail($params) {
 					->html_message($string)
 					->send();
 
-	// $result = $postmark->to("aaron.baker@IMOutdoors.com")
-	// 				->subject("New Master Angler Entry")
-	// 				->html_message($string)
-	// 				->send();
+	$result = $postmark->to("aaron.baker@IMOutdoors.com")
+					->subject("New Master Angler Entry")
+					->html_message($string)
+					->send();
 
-	// $result = $postmark->to("jeff.simpson@IMOutdoors.com")
-	// 				->subject("New Master Angler Entry")
-	// 				->html_message($string)
-	// 				->send();
+	$result = $postmark->to("jeff.simpson@IMOutdoors.com")
+					->subject("New Master Angler Entry")
+					->html_message($string)
+					->send();
 
 	// $result = $postmark->to("berry.blanton@IMOutdoors.com")
 	// 				->subject("New Master Angler Entry")
