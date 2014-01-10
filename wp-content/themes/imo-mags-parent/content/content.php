ef2f4a1b0ce8f8c772c10e2765983a350cafe7ca
Fox Bowden <fox.bowden@imoutdoors.com>
2014-01-10 10:13:08 -0500

    removing acidental file movement

--- wp-content/themes/imo-mags-parent/content/content.php
+++ wp-content/themes/imo-mags-parent/content/content.php
@@ -1,7 +1,3 @@
-<?php
-
-?>
-
 <div id="post-<?php the_ID(); ?>" <?php post_class('article-brief clearfix'); ?>>
     
     	<a href="<?php the_permalink(); ?>" ><?php the_post_thumbnail('list-thumb'); ?></a>
