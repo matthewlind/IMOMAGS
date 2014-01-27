685af6efdcd28cbf257b132b7878cb7871d2f9a8
Fox Bowden <fox.bowden@imoutdoors.com>
2014-01-27 14:07:55 -0500

    removing older themes from root folder

--- wp-content/themes/imo-mags-gunsandammo/page-right-home.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
-
-// NOTE: this file is here for compatibility reasons - active templates are in the pages/ dir 
-
-cfct_page('right-sidebar-homepage');
-
-?>
