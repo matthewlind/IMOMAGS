a3ecffe5ed4b3e727c84cb92cb2e56d186dacb7d
Aaron Baker <aaron.baker@imoutdoors.com>
2014-05-30 12:53:38 -0400

    The beginning of a Flyfisherman Community

---
+++ wp-content/themes/flyfisherman/wordpress-community.php
@@ -0,0 +1,144 @@
+<?php
+
+register_activation_hook( __FILE__, 'imo_wordpress_community_flush' );
+register_deactivation_hook( __FILE__, 'imo_wordpress_community_flush' );
+
+
+
+//G&F Wordpress Community Config
+add_action("init","ff_wp_community_init",2);
+function ff_wp_community_init() {
+
+    wp_enqueue_script( 'bootstrap-dropdown', get_stylesheet_directory_uri() . '/js/bootstrap-dropdown.js', array("jquery",'underscore'), '0.1', true );
+    wp_enqueue_script( 'ff-wp-community-listing', get_stylesheet_directory_uri() . '/js/community-listing.js', array("jquery",'underscore','bootstrap-dropdown'), '0.1', true );
+
+}
+
+
+function imo_wordpress_community_flush() {
+
+	flush_rewrite_rules( false );
+}
+
+
+function imo_wordpress_534_add_custom_query_var( $vars ){
+  $vars[] = "category_name_2";
+  $vars[] = "category_name_3";
+  return $vars;
+}
+add_filter( 'query_vars', 'imo_wordpress_534_add_custom_query_var' );
+
+add_action('init', 'category_cpt_rewrites');
+function category_cpt_rewrites() {
+
+    $rule = "photos" . '/fish-photos/(.+?)/?$';
+    $rewrite = 'index.php?post_type=' . "reader_photos" . '&category_name=$matches[1]&category_name_2=fish-photos';
+    add_rewrite_rule($rule,$rewrite,'top');
+
+    $rule = "photos" . '/scenic-photos/(.+?)/?$';
+    $rewrite = 'index.php?post_type=' . "reader_photos" . '&category_name=$matches[1]&category_name_2=scenic-photos';
+    add_rewrite_rule($rule,$rewrite,'top');
+
+    $rule = "photos" . '/flies/(.+?)/?$';
+    $rewrite = 'index.php?post_type=' . "reader_photos" . '&category_name=$matches[1]&category_name_2=flies';
+    add_rewrite_rule($rule,$rewrite,'top');
+
+    $rule = "photos" . '/fish-photos';
+    $rewrite = 'index.php?post_type=' . "reader_photos" . '&category_name=fish-photos';
+    add_rewrite_rule($rule,$rewrite,'top');
+
+    $rule = "photos" . '/scenic-photos';
+    $rewrite = 'index.php?post_type=' . "reader_photos" . '&category_name=scenic-photos';
+    add_rewrite_rule($rule,$rewrite,'top');
+
+    $rule = "photos" . '/flies';
+    $rewrite = 'index.php?post_type=' . "reader_photos" . '&category_name=flies';
+    add_rewrite_rule($rule,$rewrite,'top');
+
+ //    $rule = "photos" . '/hunting';
+ //    $rewrite = 'index.php?post_type=' . "reader_photos" . '&category_name=hunting';
+ //    add_rewrite_rule($rule,$rewrite,'top');
+
+
+
+    // $rule = "photos" . '/fishing';
+    // $rewrite = 'index.php?post_type=' . "reader_photos" . '&category_name=fishing';
+    // add_rewrite_rule($rule,$rewrite,'top');
+
+	// $rule2 = "photos" . '/(.+?)/?$';
+ //    $rewrite2 = 'index.php?post_type=' . "reader_photos" . '&category_name=$matches[1]';
+ //    add_rewrite_rule($rule2,$rewrite2,'top');
+
+	// $rule2 = "photos" . '/(.+?)/(.+?)/?$';
+ //    $rewrite2 = 'index.php?post_type=' . "reader_photos" . '&category_name=$matches[1]' . '&category_name_2=gar';
+ //    add_rewrite_rule($rule2,$rewrite2,'top');
+
+ //    $rule2 = "photos" . '/(.+?)/(.+?)/(.+?)/?$';
+ //    $rewrite2 = 'index.php?post_type=' . "reader_photos" . '&category_name=$matches[1]' . '&category_name_2=$matches[2]' . '&category_name_3=$matches[3]';
+ //    add_rewrite_rule($rule2,$rewrite2,'top');
+
+
+    // $rule = "photos" . '/flytest';
+    // $rewrite = 'index.php?post_type=' . "reader_photos" . '&category_name=fish-photos&category_name_2=bass';
+    // add_rewrite_rule($rule,$rewrite,'top');
+
+
+}
+
+
+// add_filter( 'wp_unique_post_slug', 'imo_wordpress_community_post_slug', 10, 4 );
+// function imo_wordpress_community_post_slug( $slug, $post_ID, $post_status, $post_type ) {
+
+// 		if ($post_type == "reader_photos") {
+
+// 			if (strpos($slug,"photo-") === FALSE && $post_ID == 0) {
+
+// 				$slug = "photo-" . $slug;
+// 			}
+
+// 		}
+
+
+//     return $slug;
+// }
+
+
+
+add_action('init', 'cptui_register_my_cpt_reader_photos');
+function cptui_register_my_cpt_reader_photos() {
+	register_post_type(
+		'reader_photos', array(
+			'label' => 'Reader Photos',
+			'description' => 'Upload a photo to Fly Fisherman!',
+			'public' => true,
+			'show_ui' => true,
+			'show_in_menu' => true,
+			'capability_type' => 'reader_post',
+			'map_meta_cap' => true,
+			'hierarchical' => false,
+			'rewrite' => array('slug' => 'photos'),
+			'query_var' => true,
+			'has_archive' => true,
+			'supports' => array('title','editor','excerpt','trackbacks','custom-fields','comments','revisions','thumbnail','author','page-attributes'),
+			'taxonomies' => array('category'),
+			'labels' => array (
+			  'name' => 'Reader Photos',
+			  'singular_name' => 'Reader Photo',
+			  'menu_name' => 'Reader Photos',
+			  'add_new' => 'Add Reader Photo',
+			  'add_new_item' => 'Add New Reader Photo',
+			  'edit' => 'Edit',
+			  'edit_item' => 'Edit Reader Photo',
+			  'new_item' => 'New Reader Photo',
+			  'view' => 'View Reader Photo',
+			  'view_item' => 'View Reader Photo',
+			  'search_items' => 'Search Reader Photos',
+			  'not_found' => 'No Reader Photos Found',
+			  'not_found_in_trash' => 'No Reader Photos Found in Trash',
+			  'parent' => 'Parent Reader Photo',
+			)
+		)
+	);
+
+
+}
