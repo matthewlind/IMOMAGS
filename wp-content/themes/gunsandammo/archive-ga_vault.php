6ac196ea4aacf162b44620e822caf271637b1452
Fox Bowden <fox.bowden@imoutdoors.com>
2014-07-23 14:32:59 -0400

    reverting ga vault

---
+++ wp-content/themes/gunsandammo/archive-ga_vault.php
@@ -0,0 +1,89 @@
+<?php
+
+$dataPos = 0;
+
+get_header(); ?>
+        <?php imo_sidebar(); ?>
+        <div id="primary" class="general">
+            <div id="content" role="main" class="general-frame">
+                
+                <?php if ( have_posts() ) : ?>
+    
+                    <div data-position="<?php echo $dataPos = $dataPos + 1; ?>" class="page-header clearfix js-responsive-section">
+                        <h1 class="page-title">
+                        <div class="icon"></div>
+                        	<span><?php post_type_archive_title(); ?></span>
+                        </h1>
+						<div class="sponsor"><?php echo get_imo_dart_tag("240x60",1,false,array("page"=>"ga_vault","sect"=>"ga_vault")); ?></div>
+					</div>
+                    <?php if(function_exists('wpsocialite_markup')){ wpsocialite_markup(); } ?>
+                            
+                    <?php 
+                    if(function_exists('z_taxonomy_image_url')){
+                    	if (z_taxonomy_image_url()) echo '<div class="category-img"><img src="'.z_taxonomy_image_url().'" alt="'.single_cat_title( '', false ).'" title="'.single_cat_title( '', false ).'" /></div>'; 
+                    } 
+                	$category_description = category_description();
+                        if ( ! empty( $category_description ) )
+                            echo apply_filters( 'category_archive_meta', '<div data-position="' . $dataPos = $dataPos + 1 . '" class="category-archive-meta taxdescription js-responsive-section">' . $category_description . '</div>' );
+                    ?>
+                        
+                    <!--<div data-position="<?php echo $dataPos = $dataPos + 1; ?>" class="filter-by jq-filter-by js-responsive-section">               
+                        <strong>filter by:</strong>
+                        <ul class="filter-links">
+                            <li><a href="#">Latest</a></li>
+                            <li><a href="#">Most Viewed</a></li>
+                            <li><a href="#">Most Discussed</a></li>
+                            <li><a href="#">Most Shared</a></li>
+                        </ul>
+                    </div>-->
+                    
+                    <div data-position="<?php echo $dataPos = $dataPos + 1; ?>" class="posts-list js-responsive-section main-content-preppend">
+						<?php $i = 1; while ( have_posts() ) : the_post(); ?>
+        
+                            <?php
+                                /* Include the Post-Format-specific template for the content.
+                                 * If you want to overload this in a child theme then include a file
+                                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                                 */
+                                get_template_part( 'content/content', get_post_format() );
+                               							
+								<?php if ( (($i - (($paged -1) * 2 ))%6) == 0 ): ?>
+                        	
+		                        <?php if ( mobile() ){ ?>
+		                        <div class="image-banner posts-image-banner">
+		                            <?php imo_dart_tag("300x250",array("pos"=>"mob")); ?> 
+		                        </div>
+		                        <?php } ?>
+	                        <?php endif;?>
+							
+	                        <?php $i++; endwhile; ?>
+        
+                    </div>
+    
+                    <div data-position="<?php echo $dataPos = $dataPos + 1; ?>" class="pager-holder js-responsive-section">
+                        <a href="#" class="btn-base">Load More</a>
+                        <div class="next-link" style="display:none;"><?php next_posts_link(); ?></div>
+                        <a href="#" class="go-top jq-go-top">go top</a>
+
+                        <img src="/wp-content/themes/imo-mags-parent/images/ajax-loader.gif" id="ajax-loader" style="display:none;"/>
+                    </div>
+                <?php else : ?>
+    
+                    <div id="post-0" class="post no-results not-found">
+                        <div class="entry-header">
+                            <h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+                        </div><!-- .entry-header -->
+    
+                        <div class="entry-content">
+                            <p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
+                            <?php get_search_form(); ?>
+                        </div><!-- .entry-content -->
+                    </div><!-- #post-0 -->
+    
+                <?php endif; ?>
+                <?php social_footer(); ?>
+                <a href="#" class="back-top jq-go-top">back to top</a>
+                
+            </div><!-- #content -->
+        </div><!-- #primary -->
+<?php get_footer(); ?>
--- wp-content/themes/gunsandammo/header.php
+++ wp-content/themes/gunsandammo/header.php
@@ -189,7 +189,7 @@
 
 <div id="page" class="snap-content smooth-menu">
 <?php if (mobile() == false && tablet() == false) { imo_dart_tag("1x1",false,array("pos"=>"skin")); } ?>
-	<div class="hfeed wrapper" data-role="content" role="main">
+	<div class="hfeed wrapper <?php if(get_field("full_width") == true){ echo ' full-width full-content'; }else if(has_post_format( 'video' )){ echo ' full-width full-content'; } ?>" data-role="content" role="main">
 	    <div class="layout-frame">
 	        <div id="branding" class="header clearfix" role="banner">
 	
