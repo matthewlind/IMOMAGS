5b8c1dfc8ab8dcc950b027b24dfdee73caf7394a
Fox Bowden <fox.bowden@imoutdoors.com>
2014-08-05 10:36:07 -0400

    updating google analytics code

--- wp-content/themes/shootingtimes/head-includes.php
+++ wp-content/themes/shootingtimes/head-includes.php
@@ -3,36 +3,7 @@
 /**
  *head-includes.php makes special things special.
  */
-
 ?>
-
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-5816642-20']);
-  _gaq.push(['_setDomainName', 'none']);
-  _gaq.push(['_setAllowLinker', true]);
-  _gaq.push(['_trackPageview']);
-  _gaq.push(['_setAccount', 'UA-2409437-13']);
-  _gaq.push(['_setDomainName', '.shootingtimes.com']);
-  <?php
-  	if (is_single()) {
-		$author = get_the_author();
-		echo " _gaq.push(['_setCustomVar', 1,'author','". addslashes($author) . "', 3]);";
-		
-		//$category = get_the_category();
-		//echo " _gaq.push(['_setCustomVar', 2,'category','". $category[0]->cat_name . "', 3]);";
-	}
-  ?>
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 <!-- BEGIN Tynt Script -->
 <script type="text/javascript">
 if(document.location.protocol=='http:'){
