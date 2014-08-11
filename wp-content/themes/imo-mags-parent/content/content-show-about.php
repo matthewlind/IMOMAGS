bccc742b57b09c5928f735823a7b0af993b03588
artem.kovalyk <artre8@gmail.com>
2014-08-11 15:11:39 -0400

    tv-show about page

---
+++ wp-content/themes/imo-mags-parent/content/content-show-about.php
@@ -0,0 +1,18 @@
+<div class="about-hosts-wrap">
+	<div class="host-item">
+		<div class="host-head">
+			<div class="host-photo">
+				<img src="/wp-content/themes/petersenshunting/images/b2b/b2b-map.jpg">
+			</div>
+			<div class="host-info">
+				<h2>Mike Schoby</h2>
+				<p><span>Favorite gun</span>Condor Longfowler Shotgun 12-Gauge 2-3/4</p>
+				<img src="/wp-content/themes/petersenshunting/images/b2b/b2b-map.jpg">
+			</div>
+		</div><!-- end of .host-head -->
+		<div class="host-copy">
+		 	<p> </p>
+		</div>
+	</div><!-- end of .host-item -->
+
+</div>
