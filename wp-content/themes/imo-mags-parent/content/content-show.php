23dd54a471706bfb28e4ffe7880a6d816dba6428
artem.kovalyk <artre8@gmail.com>
2014-08-18 17:52:44 -0400

    tv-show, video-player-area styles and other

--- wp-content/themes/imo-mags-parent/content/content-show.php
+++ wp-content/themes/imo-mags-parent/content/content-show.php
@@ -40,61 +40,68 @@ $video_id = get_post_meta(get_the_ID(), '_video_id', TRUE); ?>
 <div id="show-destination" playerID="<?php echo get_field("tv_player_id","options"); ?>" adServerURL="<?php echo $adServerURL; ?>" videoLink="<?php echo $videoLink; ?>">
 	<?php get_template_part( 'content/tv-show/show-header' ); ?>
 		<div class="video-player-area">
-			<div id="video-gallery" class="video-player-wrap">
-	
+			<div id="video-gallery" class="video-player-wrap clearf">
 				<script type="text/javascript" src="http://admin.brightcove.com/js/BrightcoveExperiences.js"></script> 
 				<div id="player"></div>
-				
-				<div id="description-area">
-					<div class="unify">
-						<div class="content-height">
-							<span class="show-video-date"><?php the_time('F jS, Y'); ?></span>
-							<h1 class="video-title" data-videoid="<?php echo $video_id; ?>" data-slug="<?php echo $post->post_name;?>"><?php the_title(); ?></h1>
-							<div class="video-description"><?php the_content(); ?></div>
-						</div>
-						<div class="video-more-content" style="display:none;"><div class="more-link">Read More</div></div>
-						<div class="social-share">
-							<div class="share-results">
-								<span>2K</span>				
-								<div class="shares"><span>SHARES</span></div>		
-							</div>
-							<div class="social-share-btns">
-								<a class="reload-fb" href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&amp;t=<?php the_title(); ?>" title="Share on Facebook." target="_blank">
-									<div class="facebook-share">
-										<i class="fa fa-facebook"></i>
-									</div>
-								</a>
-								<a class="reload-twitter" href="http://twitter.com/home/?status=<?php the_title(); ?> - <?php the_permalink(); ?>" title="Tweet this!" target="_blank">
-									<div class="twitter-share">
-										<i class="fa fa-twitter"></i>
-									</div>	
-								</a>
-								<a class="reload-google" href="https://plus.google.com/share?url=<?php the_permalink(); ?>" onclick="javascript:window.open(this.href,
-			  '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
-									<div class="google-share">
-										<i class="fa fa-google-plus"></i>
-									</div>
-								</a>
-							</div>
-						
-						</div><!-- end of .social-share -->
-					</div><!-- end of .unify -->
-										
-				</div><!-- end of #description-area -->
-				<div class="video-player-sidebar">
-					<div class="new-show"></div>
-					<div class="ad-block">
-						<?php imo_dart_tag("300x250"); ?>
-					</div>
+				<div class="new-show">
+					<img src="/wp-content/themes/imo-mags-parent/images/temp/new-show.jpg">
 				</div>
+			</div><!-- end of .video-player-wrap -->
+			<div id="description-area">
+				<div class="unify">
+					<div class="content-height">
+						<span class="show-video-date"><?php the_time('F jS, Y'); ?></span>
+						<h1 class="video-title" data-videoid="<?php echo $video_id; ?>" data-slug="<?php echo $post->post_name;?>"><?php the_title(); ?></h1>
+						<div class="video-description"><?php the_content(); ?></div>
+					</div>
+					<div class="social-share clearf">
+						<div class="share-results">
+							<span>2K</span>				
+							<div class="shares"><span>SHARES</span></div>		
+						</div>
+						<div class="social-share-btns">
+							<a class="reload-fb" href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&amp;t=<?php the_title(); ?>" title="Share on Facebook." target="_blank">
+								<div class="facebook-share">
+									<i class="fa fa-facebook"></i>
+								</div>
+							</a>
+							<a class="reload-twitter" href="http://twitter.com/home/?status=<?php the_title(); ?> - <?php the_permalink(); ?>" title="Tweet this!" target="_blank">
+								<div class="twitter-share">
+									<i class="fa fa-twitter"></i>
+								</div>	
+							</a>
+							<a class="reload-google" href="https://plus.google.com/share?url=<?php the_permalink(); ?>" onclick="javascript:window.open(this.href,
+		  '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
+								<div class="google-share">
+									<i class="fa fa-google-plus"></i>
+								</div>
+							</a>
+						</div><!-- end of .social-share-btns -->
+					</div><!-- end of .social-share -->
+
+					
+					<div class="video-more-content" style="display:none;">
+						<div class="more-link">Read More</div>
+					</div>
+				</div><!-- end of .unify -->
+					<div class="ad-block">
+							<?php imo_dart_tag("300x250"); ?>
+					</div>
+			</div><!-- end of #description-area -->
+			<!--
+<div class="video-player-sidebar">
+				
+				
 			</div>
+-->
+			
 			<!-- this widget is located in imo-mags-parent/widgets -->
 			<?php get_template_part( 'widgets/sportsmanLocator' ); ?>
 		</div><!-- end of #video-player-area -->
 		
 	</div><!-- end of #shows_player_area -->
 	<?php endwhile; ?> 
-	<div id="show-featured">
+	<div id="show-featured" class="clearf">
 		
 		<?php query_posts(array( 
 		    'tax_query' => array(
@@ -110,10 +117,8 @@ $video_id = get_post_meta(get_the_ID(), '_video_id', TRUE); ?>
 
 		<div class="thumbs-full">
 			<ul id="video-filter">
-			
 				<li><a slug="all" class="video-thumb-active video-ajax">Most Recent</a></li>
 				<li><a slug="bestshots" class="video-ajax">Bestshots</a></li>
-	
 			</ul>
 			<ul id="video-thumbs">
 				<?php while (have_posts()) : the_post(); $i++; 
@@ -137,26 +142,29 @@ $video_id = get_post_meta(get_the_ID(), '_video_id', TRUE); ?>
 			
 				<?php endwhile; ?>
 			</ul>
-			<a class="paginate-videos">Load more videos</a>
-		</div>
-		<div id="upcoming">
-			<div class="container tiled-grid clr">
-				<div class="tonight-bg"></div>
-				<div class="tiled-grid-entry on-tonight clr span_1_of_4 col col-1">
-					<h2>On Tonight</h2>
-					<a class="" href="/schedule">Full Schedule</a>
-				</div>
-				<div class="tonight-schedule">
-					<ul class="slides">
-						<?php echo do_shortcode('[tscschedule format="tonight"]'); ?>
-					</ul>
-				</div>
+			<div class="btn-grey-sm paginate-videos">
+				<a href="">Load more videos <i class="fa fa-long-arrow-down"></a></i>
 			</div>
-		</div>
+		</div><!-- end of .thumbs-full -->
+		
 		<div id="imo-store">
 			
 		</div>
-	</div>
+	</div><!-- end of #show-featured -->
+	<div id="upcoming">
+		<div class="container tiled-grid clr">
+			<div class="tonight-bg"></div>
+			<div class="tiled-grid-entry on-tonight clr span_1_of_4 col col-1">
+				<h3>On Tonight</h3>
+				<a class="" href="/schedule">Full Schedule <i class="fa fa-angle-double-right"></i></a>
+			</div>
+			<div class="tonight-schedule">
+				<ul class="slides">
+					<?php echo do_shortcode('[tscschedule format="tonight"]'); ?>
+				</ul>
+			</div>
+		</div>
+	</div><!-- end of #upcoming -->
 	<?php get_template_part( 'content/show-sponsors' ); ?>
 </div>
 	
