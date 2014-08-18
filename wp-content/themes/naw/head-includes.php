6fdd3d81fb0d451d5a919928456844250c42c653
Fox Bowden <fox.bowden@imoutdoors.com>
2014-08-18 14:09:37 -0400

    reverting analytics code for testing

--- wp-content/themes/naw/head-includes.php
+++ wp-content/themes/naw/head-includes.php
@@ -6,6 +6,33 @@
 $appArgument = "whitetailplus://www.northamericanwhitetail.com" . $_SERVER['REQUEST_URI'];
 ?>
 <meta name="apple-itunes-app" content="app-id=582721507">
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-5816642-20']);
+  _gaq.push(['_setDomainName', 'none']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+  _gaq.push(['_setAccount', 'UA-2409437-15']);
+  _gaq.push(['_setDomainName', '.northamericanwhitetail.com']);
+  <?php
+
+		$author = get_the_author();
+		echo " _gaq.push(['_setCustomVar', 1,'author','". addslashes($author) . "', 3]);";
+
+		//$category = get_the_category();
+		//echo " _gaq.push(['_setCustomVar', 2,'category','". $category[0]->cat_name . "', 3]);";
+
+  ?>
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <!-- BEGIN Tynt Script -->
 <script type="text/javascript">
 if(document.location.protocol=='http:'){
