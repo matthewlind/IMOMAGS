0ba5b340e69306856e4324faad6cb52d0d66d47e
Ben Gray <bendoylegray@gmail.com>
2014-09-22 10:37:55 -0400

    Resolved issue with logo padding above "final round"

--- wp-content/plugins/bowmadness/plugin.php
+++ wp-content/plugins/bowmadness/plugin.php
@@ -226,7 +226,7 @@ function jsGAMRender($mobile) {
 	// go into the db and manually change the last match to "8".
 	
 	
-	if($madnessround >= 8) {		 
+	if($madnessround == 8) {		 
 	$outp.= '<div class="ga-madness-votestats" style="margin-bottom:20px;clear:both;"></div>'
 		 .  '<div class="regions region-final" style="display:block;">'
 		 .  '  <div class="finalsadvert" style="margin-top:0px;">'
@@ -239,7 +239,7 @@ function jsGAMRender($mobile) {
 		 .  '  <div class="final-wrapper">'
 		 .  '    <h2>Final Round</h2>'
 		 .  '    <div class="column column5 match92"></div>'
-		 .  '    <div class="column column6 match94" style="xpadding-top:20px;"></div>'
+		 .  '    <div class="column column6 match94" style="padding-top:20px;"></div>'
 		 .  '    <div class="column column7 match93"></div>'
 		 .  '  </div>'		 
 		 .  '</div>';
