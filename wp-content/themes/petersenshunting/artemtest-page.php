b064a406fc261785a59438e3e702aa297b42d662
artem.kovalyk <artre8@gmail.com>
2014-10-15 15:36:34 -0400

    Bowhunter video-widget html and styles

--- wp-content/themes/petersenshunting/artemtest-page.php
+++ wp-content/themes/petersenshunting/artemtest-page.php
@@ -19,9 +19,34 @@ get_header(); ?>
 <?php imo_sidebar(); ?>
 	
 	<div id="primary" class="general">	
-	<div class="widget widget_channel-video-widget">
-	
+	<div class="widget widget_channel-video-widget video-widget-bowhunter clearfix">
+	<!-- vw = video-widget -->
+		<div class="vw-head">
+			<img class="vw-logo" src="/wp-content/themes/petersenshunting/images/logos/bowhunter-tv-logo.png">
+			<p><span>FRIDAYS</span> 8:00pm/ET</p>
+			<img class="vw-logo-sportsman" src="/wp-content/themes/petersenshunting/images/logos/SPMN-white.png">
+		</div>
+		<div class="vw-video-area" style="background: url('/wp-content/themes/petersenshunting/images/video-widget/form_2.jpg') no-repeat;">
+			<i class="icon-youtube"></i>
+		</div>
+		<div class="btn-wrap">
+			<a class="vw-btn" href="">MORE VIDEO AND SHOWTIMES</a>
+		</div>
+		<div class="vw-line"></div>
+		<div class="vw-get-sportsman clearfix">
+			<img src="/wp-content/themes/petersenshunting/images/logos/spmn-widget-logo.png">
+			<a href="">GET SPORTSMAN CHANNEL NOW</a>
+			<i class="icon-arrow-right2"></i>
+		</div>
 	</div>
+	
+	
+	
+	
+	
+	
+	
+	
 	<p class="stupid-p">
 	Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.
 	</p>
