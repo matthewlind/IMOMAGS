fcf0cede6ed589db249b6bbbc8fa63bcf09d9628
artem.kovalyk <artre8@gmail.com>
2014-10-20 12:39:58 -0400

    widgets

--- wp-content/themes/petersenshunting/artemtest-page.php
+++ wp-content/themes/petersenshunting/artemtest-page.php
@@ -19,29 +19,12 @@ get_header(); ?>
 <?php imo_sidebar(); ?>
 	
 	<div id="primary" class="general">	
-	<div class="widget widget_channel-video-widget video-widget-bowhunter clearfix">
-	<!-- vw = video-widget -->
-		<div class="vw-head">
-			<img class="vw-logo" src="/wp-content/themes/petersenshunting/images/logos/bowhunter-tv-logo.png">
-			<p><span>FRIDAYS</span> 8:00pm/ET</p>
-			<img class="vw-logo-sportsman" src="/wp-content/themes/petersenshunting/images/logos/SPMN-white.png">
-		</div>
-		<div class="vw-video-area" style="background: url('/wp-content/themes/petersenshunting/images/video-widget/form_2.jpg') no-repeat;">
-			<i class="icon-youtube"></i>
-		</div>
-		<div class="btn-wrap">
-			<a class="vw-btn" href="">MORE VIDEO AND SHOWTIMES</a>
-		</div>
-		<div class="vw-line"></div>
-		<div class="vw-get-sportsman clearfix">
-			<img src="/wp-content/themes/petersenshunting/images/logos/spmn-widget-logo.png">
-			<a href="">GET SPORTSMAN CHANNEL NOW</a>
-			<i class="icon-arrow-right2"></i>
-		</div>
-	</div>
+
+	<?php get_template_part("widgets/video-bowhunter-widget"); ?>
 	
+	<div class="gap-test"></div>
 	
-	
+	<?php get_template_part("widgets/spmn-news-widget"); ?>
 	
 	
 	
