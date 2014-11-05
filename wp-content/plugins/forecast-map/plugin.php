ccaae199d17ba63971f129755b84fdd3fc19c62f
Fox Bowden <fox.bowden@imoutdoors.com>
2014-11-05 12:09:31 -0500

    fix for broken images in WP image edit

--- wp-content/plugins/forecast-map/plugin.php
+++ wp-content/plugins/forecast-map/plugin.php
@@ -1,9 +1,5 @@
 <?php
 /*  Copyright 2012 Aaron Baker
-
-*/
-
-/*
 Plugin Name: Forecast Map
 Plugin URI: https://imomags.com
 Description: Helps setup an interactive map of US States
@@ -13,13 +9,8 @@ Version: 0.1
 Stable tag: 0.1
 License: IMO
 */ 
-
-
 add_action('wp_enqueue_scripts', 'imo_forecast_map_init', 50);
-
-
 function imo_forecast_map_init() {
-	
 	//mobile map touch fix
 	if(mobile() == true){ ?>
 		<script type="text/javascript">
@@ -30,11 +21,6 @@ function imo_forecast_map_init() {
 			var $mobile = false;
 		</script>
 	<?php }
-	
 	wp_enqueue_style( 'map-style', plugin_dir_url( __FILE__ ) . '/css/map-style.css' );
-	wp_enqueue_script('imo-usa-map-forecast-js',plugins_url('js/imo-usa-map-forecast.js', __FILE__)); ?>
-
-	
+	wp_enqueue_script('imo-usa-map-forecast-js',plugins_url('js/imo-usa-map-forecast.js', __FILE__)); ?>	
 <?php } ?>
-
-
