c4a4271c7c610f25b38ba51db1bf23a2817979d4
Fox Bowden <fox.bowden@imoutdoors.com>
2014-11-13 10:58:24 -0500

    adding tune in widget

--- wp-content/themes/imo-mags-parent/head-includes.php
+++ wp-content/themes/imo-mags-parent/head-includes.php
@@ -152,6 +152,7 @@ googletag.defineSlot('/4930/<?php echo $dartDomain; ?>/Screen_Shift_1x1', [1, 1]
 googletag.defineSlot('/4930/<?php echo $dartDomain; ?>/Site_Skin_1x1', [1, 1], 'div-site_skin_1x1').addService(googletag.pubads());
 googletag.defineSlot('/4930/<?php echo $dartDomain; ?>/Sponsor_Logo_240x60', [240, 60], 'div-sponsor_logo_240x60').addService(googletag.pubads());
 googletag.defineSlot('/4930/<?php echo $dartDomain; ?>/Deer_Forecast_Sponsor_Logo_240x60', [240, 60], 'div-Deer_Forecast_Sponsor_Logo_240x60').addService(googletag.pubads());
+googletag.defineSlot('/4930/<?php echo $dartDomain; ?>/Polar_Native_Ad_2x2', [2, 2], 'div-polar_native_ad_2x2').addService(googletag.pubads());
 <?php if(is_page( 'battle-of-the-bows' )){ ?>
 
 //Battle of the Bows
