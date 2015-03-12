337947e53aa1a061ae4b3cb78bad46daa46e46e6
Ben Gray <bendoylegray@gmail.com>
2015-03-12 11:00:18 -0400

    Ad code in place.  Brackets in place.  Popup working.  Captcha working.

--- wp-content/themes/gunsandammo/header.php
+++ wp-content/themes/gunsandammo/header.php
@@ -89,6 +89,39 @@
 	<script src="<?php echo get_template_directory_uri(); ?>/js/flash_heed.js" type="text/javascript"></script>
 	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/snap.js"></script>
 	<script type='text/javascript'>
+//		var googletag = googletag || {};
+//		googletag.cmd = googletag.cmd || [];
+//		(function() {
+//		var gads = document.createElement('script');
+//		gads.async = true;
+//		gads.type = 'text/javascript';
+//		var useSSL = 'https:' == document.location.protocol;
+//		gads.src = (useSSL ? 'https:' : 'http:') +
+//		'//www.googletagservices.com/tag/js/gpt.js';
+//		var node = document.getElementsByTagName('script')[0];
+//		node.parentNode.insertBefore(gads, node);
+//		})();
+	</script>
+	 
+	<script type='text/javascript'>
+//		googletag.cmd.push(function() {
+//			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [1, 1], 'div-gpt-ad-1386782139095-0').addService(googletag.pubads());
+//			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [240, 60], 'div-gpt-ad-1386782139095-1').addService(googletag.pubads());
+//			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [300, 120], 'div-gpt-ad-1386782139095-2').addService(googletag.pubads());
+//			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [300, 250], 'div-gpt-ad-1386782139095-3').addService(googletag.pubads());
+//			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [300, 300], 'div-gpt-ad-1386782139095-4').addService(googletag.pubads());
+//			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [300, 600], 'div-gpt-ad-1386782139095-5').addService(googletag.pubads());
+//			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [300, 602], 'div-gpt-ad-1386782139095-6').addService(googletag.pubads());
+//			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [728, 90], 'div-gpt-ad-1386782139095-7').addService(googletag.pubads());
+//			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [1080, 90], 'div-gpt-ad-1386782139095-8').addService(googletag.pubads());
+//			googletag.pubads().enableSingleRequest();
+//			googletag.pubads().enableVideoAds();
+//			googletag.enableServices();
+//		});
+	</script>
+	
+		
+	<script type='text/javascript'> // ADS FOR GAMADNESS 2015
 		var googletag = googletag || {};
 		googletag.cmd = googletag.cmd || [];
 		(function() {
@@ -96,29 +129,26 @@
 		gads.async = true;
 		gads.type = 'text/javascript';
 		var useSSL = 'https:' == document.location.protocol;
-		gads.src = (useSSL ? 'https:' : 'http:') +
+		gads.src = (useSSL ? 'https:' : 'http:') + 
 		'//www.googletagservices.com/tag/js/gpt.js';
 		var node = document.getElementsByTagName('script')[0];
 		node.parentNode.insertBefore(gads, node);
 		})();
-	</script>
-	 
-	<script type='text/javascript'>
+		</script>
+		
+		<script type='text/javascript'>
 		googletag.cmd.push(function() {
-			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [1, 1], 'div-gpt-ad-1386782139095-0').addService(googletag.pubads());
-			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [240, 60], 'div-gpt-ad-1386782139095-1').addService(googletag.pubads());
-			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [300, 120], 'div-gpt-ad-1386782139095-2').addService(googletag.pubads());
-			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [300, 250], 'div-gpt-ad-1386782139095-3').addService(googletag.pubads());
-			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [300, 300], 'div-gpt-ad-1386782139095-4').addService(googletag.pubads());
-			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [300, 600], 'div-gpt-ad-1386782139095-5').addService(googletag.pubads());
-			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [300, 602], 'div-gpt-ad-1386782139095-6').addService(googletag.pubads());
-			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [728, 90], 'div-gpt-ad-1386782139095-7').addService(googletag.pubads());
-			googletag.defineSlot('/4930/<?php echo $dartDomain; ?>', [1080, 90], 'div-gpt-ad-1386782139095-8').addService(googletag.pubads());
-			googletag.pubads().enableSingleRequest();
-			googletag.pubads().enableVideoAds();
-			googletag.enableServices();
+		googletag.defineSlot('/4930/imo.gunsandammo/guns_and_ammo_madness/gam_bracket_medium_rectangle', [300, 250], 'div-gpt-ad-1426097842267-0').addService(googletag.pubads());
+		googletag.defineSlot('/4930/imo.gunsandammo/guns_and_ammo_madness/gam_presenting_sponsor', [240, 60], 'div-gpt-ad-1426097842267-1').addService(googletag.pubads());
+		googletag.defineSlot('/4930/imo.gunsandammo/guns_and_ammo_madness/gam_region_sponsor_1', [240, 60], 'div-gpt-ad-1426097842267-2').addService(googletag.pubads());
+		googletag.defineSlot('/4930/imo.gunsandammo/guns_and_ammo_madness/gam_region_sponsor_2', [240, 60], 'div-gpt-ad-1426097842267-3').addService(googletag.pubads());
+		googletag.defineSlot('/4930/imo.gunsandammo/guns_and_ammo_madness/gam_region_sponsor_3', [240, 60], 'div-gpt-ad-1426097842267-4').addService(googletag.pubads());
+		googletag.defineSlot('/4930/imo.gunsandammo/guns_and_ammo_madness/gam_region_sponsor_4', [240, 60], 'div-gpt-ad-1426097842267-5').addService(googletag.pubads());
+		googletag.pubads().enableSingleRequest();
+		googletag.enableServices();
 		});
 	</script>
+	
 	<?php if ( defined('JETPACK_SITE') && mobile() == false && tablet() == false): ?>
 		<script type='text/javascript' src='http://ads.jetpackdigital.com/sites/<?php print JETPACK_SITE; ?>/jpd.js'></script>
 	<?php endif; ?>
