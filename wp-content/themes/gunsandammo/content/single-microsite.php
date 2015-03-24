4d7ec7790457734f6b23a935ed0826cf78843852
artem.kovalyk <artre8@gmail.com>
2015-03-24 18:47:48 -0400

    logo link, css tweaks

--- wp-content/themes/gunsandammo/content/single-microsite.php
+++ wp-content/themes/gunsandammo/content/single-microsite.php
@@ -12,6 +12,10 @@ echo get_template_part( 'header', 'shoot101' );
 */
 
 	$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+	
+	$postID = get_the_ID();
+	$byline = get_post_meta($postID, 'ecpt_byline', true);
+	$acf_byline = get_field("byline",$postID); 
 ?>
 
 <div id="fb-root"></div>
@@ -23,66 +27,17 @@ echo get_template_part( 'header', 'shoot101' );
 		
 	</div>
 	<article class="m-article clearfix">
-			<div class="m-social-buttons">
-				<ul>
-					<li><a class="icon-facebook" target="_blank" href="http://www.facebook.com/sharer.php?s=100&amp;p[title]=<?php echo urlencode(the_title());?>&amp;p[summary]=<?php echo urlencode(the_title()) ?>&amp;p[url]=<?php echo urlencode(get_permalink()); ?>&amp;p[images][0]=<?php echo urlencode($image[0])?>" ></a></li>
-					<li><a href="http://twitter.com/intent/tweet?status=<?php print(urlencode(the_title())); ?>+<?php print(urlencode(get_permalink())); ?>" class="icon-twitter" target="_blank"></a></li>
-					<li><a class="icon-mail" target="_blank"></a></li>
-				</ul>
-			</div>
-		
-		<h1>SIGHTING SOLUTIONS: 10 GREAT OPTICS AT EVERY PRICE POINT</h1>
+		<?php echo get_template_part("content/social", "buttons"); ?>
+		<h1><?php the_title();?></h1>
 		<h3>HIT THE BULLSEYE WITHOUT HITTING YOUR WALLET</h3>
-		<span class="a-author">Peter Wolf</span>
-		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.</p>
-		<blockquote>
-			Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
-		</blockquote>
-<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. </p>
-<?php the_content(); ?>
-
-		
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		<span class="m-post-byline">Words by Peter Wolf &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;</span><?php if ($acf_byline) { ?><span class="m-post-byline">Photos by <?php echo $acf_byline;?></span><?php } ?>
+		<?php the_content(); ?>
+		<?php echo get_template_part("content/social", "buttons"); ?>
 	</article>
 </div><!-- end .m-article-wrap -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<div class="m-more-wrap">
+	
+</div>
 
 
 
--- wp-content/themes/gunsandammo/header-shoot101.php
+++ wp-content/themes/gunsandammo/header-shoot101.php
@@ -141,41 +141,35 @@
 
 <body <?php body_class(); ?>  >
 
-<!--
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
--->
-<div class="top-panel">
-	<a href="http://www.gunsandammo.com/" class="icon-arrow-left">Back to Guns & Ammo</a>
-</div>
-<div class="s101 <?php /* if (is_category( 'shoot101' )) { echo "cat-shoot101"; } */ if ( in_category( 'shoot101' )) { echo "cat-shoot101";}?>">
-	
-	<header class="main-header">
+	<div class="top-panel">
+		<a href="http://www.gunsandammo.com/" class="icon-arrow-left">Back to Guns & Ammo</a>
+	</div>
+	<div class="s101 <?php /* if (is_category( 'shoot101' )) { echo "cat-shoot101"; } */ if ( in_category( 'shoot101' )) { echo "cat-shoot101";}?>">
 		
-		<div class="menu-area clearfix">
-			<div class="shoot101-logo">
-				<img  src="/wp-content/themes/gunsandammo/images/shoot101/Shoot101-logo-light.png">
-			</div>
-<!-- 			<div class="nav-container"> -->
-				<nav id="site-navigation" class="main-nav" role="navigation">
-					<?php wp_nav_menu( array( 'theme_location' => 'shoot101_menu', 'container' => '0' ) ); ?>
-				</nav>
-				<div class="social-buttons">
-					<ul>
-						<li><a href="http://www.facebook.com/sharer/sharer.php?u=<?php print(urlencode(get_permalink())); ?>&title=<?php print(urlencode(the_title())); ?>" class="icon-facebook"></a></li>
-						<li><a href="http://twitter.com/intent/tweet?status=<?php print(urlencode(the_title())); ?>+<?php print(urlencode(get_permalink())); ?>" class="icon-twitter"></a></li>
-						<li><a class="icon-mail"></a></li>
-					</ul>
+		<header class="main-header">
+			
+			<div class="menu-area clearfix">
+				<?php
+				    $category_id = get_cat_ID( 'shoot101' );				
+				    $category_link = get_category_link( $category_id );
+				?>
+				<div class="shoot101-logo">
+					<a href="<?php echo esc_url( $category_link ); ?>" title="shoot101"><img  src="/wp-content/themes/gunsandammo/images/shoot101/Shoot101-logo-light.png"></a>
 				</div>
-<!-- 			</div> -->
-		</div>
-	</header>
+	<!-- 			<div class="nav-container"> -->
+					<nav id="site-navigation" class="main-nav" role="navigation">
+						<?php wp_nav_menu( array( 'theme_location' => 'shoot101_menu', 'container' => '0' ) ); ?>
+					</nav>
+					<div class="social-buttons">
+						<ul>
+							<li><a href="http://www.facebook.com/sharer/sharer.php?u=<?php print(urlencode(get_permalink())); ?>&title=<?php print(urlencode(the_title())); ?>" class="icon-facebook"></a></li>
+							<li><a href="http://twitter.com/intent/tweet?status=<?php print(urlencode(the_title())); ?>+<?php print(urlencode(get_permalink())); ?>" class="icon-twitter"></a></li>
+							<li><a class="icon-mail"></a></li>
+						</ul>
+					</div>
+	<!-- 			</div> -->
+			</div>
+		</header>
 
 
 
