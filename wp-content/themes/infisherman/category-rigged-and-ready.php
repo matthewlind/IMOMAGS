0c49b2093e32e5e378101f30d1998d8937b5b7e7
artem.kovalyk <artre8@gmail.com>
2015-04-16 17:05:32 -0400

    home page regions html css start

--- wp-content/themes/infisherman/category-rigged-and-ready.php
+++ wp-content/themes/infisherman/category-rigged-and-ready.php
@@ -1,14 +1,44 @@
 <?php get_header(); ?>
 
+<div class="m-home-wrap">
+	<ul class="m-regn clearfix">
+		<li class="m-ne">
+			<div class="m-direction">
+				<div class="m-triangle"></div>
+				<span>Northeast</span>
+			</div>
+		</li>
+		<li class="m-se">
+			<div class="m-direction">
+				<div class="m-triangle"></div>
+				<span>Southeast</span>
+			</div>
+		</li>
+		<li class="m-mw">
+			<div class="m-direction">
+				<div class="m-triangle"></div>
+				<span>Midwest</span>
+			</div>
+		</li>
+		<li class="m-sw">
+			<div class="m-direction">
+				<div class="m-triangle"></div>
+				<span>Southwest</span>
+			</div>
+		</li>
+		<li class="m-nw">
+			<div class="m-direction">
+				<div class="m-triangle"></div>
+				<span>Northwest</span>
+			</div>
+		</li>
+	</ul>
+		
+</div>				
 				
 				
-				<h1>url: <?php bloginfo('url'); ?></h1>
-				<h1>uri: <?php echo get_template_directory_uri(); ?></h1>
-				<h1>get_template: <?php echo get_template() ?></h1>
 				
 				
-				<p><?php $cat = get_query_var('cat');
-  $yourcat = get_category ($cat);
-  echo 'the slug is '. $yourcat->slug;?></p>
+	
 
 <?php get_footer(); ?>
