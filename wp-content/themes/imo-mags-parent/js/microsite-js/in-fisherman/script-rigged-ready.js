b1babb000e719d5bd331880ce0e620a1c36857fa
artem.kovalyk <artre8@gmail.com>
2015-05-14 14:28:37 -0400

    rigged ready js

--- wp-content/themes/imo-mags-parent/js/microsite-js/in-fisherman/script-rigged-ready.js
+++ wp-content/themes/imo-mags-parent/js/microsite-js/in-fisherman/script-rigged-ready.js
@@ -3,13 +3,27 @@ jQuery(document).ready(function($) {
 	var mapShift = $(".m-map-shift"),
 		mapRelative = $(".m-map-relative");
 	
+/*
 	$(".m-nav").click(function(e){
 		e.stopPropagation();
 		mapShift.addClass("m-map-shift-down");
 		mapRelative.css("height", "230px");
 	});
-	
+*/
+	$(".m-nav").on("click", function(e){
+		e.stopPropagation();
+		mapShift.addClass("m-map-shift-down");
+		mapRelative.css("height", "230px");
+	});
+
+/*
 	$(".m-map-shift > i, body").click(function(){
+		mapShift.removeClass("m-map-shift-down");
+		mapRelative.css("height", "0");
+	});
+*/
+	
+	$(".m-map-shift > i, body").on("click", function(){ 
 		mapShift.removeClass("m-map-shift-down");
 		mapRelative.css("height", "0");
 	});
