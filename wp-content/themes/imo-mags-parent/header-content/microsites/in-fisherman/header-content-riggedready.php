f9c09dfcfec009060356fc97e4eae293f0e7cce4
artem.kovalyk <artre8@gmail.com>
2015-05-19 11:25:42 -0400

    rigged last changes

--- wp-content/themes/imo-mags-parent/header-content/microsites/in-fisherman/header-content-riggedready.php
+++ wp-content/themes/imo-mags-parent/header-content/microsites/in-fisherman/header-content-riggedready.php
@@ -1,12 +1,3 @@
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=432932696867322";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
 <div class="top-panel">
 	<div class="top-panel-absolute">
 		<a href="<?php echo site_url(); ?>" class="icon-arrow-left">Back to In-Fisherman</a>
