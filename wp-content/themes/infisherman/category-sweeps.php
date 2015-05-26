364cb4ff4664acc457bad4934b8dbb8c1b0f1259
artem.kovalyk <artre8@gmail.com>
2015-05-26 16:58:02 -0400

    Rigged sweepstakes

---
+++ wp-content/themes/infisherman/category-sweeps.php
@@ -0,0 +1,85 @@
+<?php
+	$microsite = true;
+	get_header(); 
+	
+?>
+<div class="m-article-wrap clearfix">	
+			<div class="m-article-image">
+				<div class="m-direction">
+					<span>WIN!</span>
+				</div>
+				<div class="m-sweep-text">
+					<h1>Get Rigged & Ready<br><span>TO FISH COSTA RICA!</span></h1>
+					<p>Win an all-expense paid 7 Night 6 Day Fishing trip for two in Nosara, Costa Rica home to some of the best Sport Fishing in Central America.</p>
+				</div>
+				
+				
+			</div>
+	
+	<article class="m-article clearfix">
+		<script type="text/javascript">
+        var cnt_id = "f83efcdb-18776";
+        </script>
+        <script type="text/javascript" src="https://www.viralsweep.com/external/widget.js"></script>
+		
+
+	</article>
+</div><!-- .m-article-wrap -->
+					
+<div class="m-truck-container m-truck-region">
+	<div class="m-truck">
+		<div class="m-truck-title clearfix">
+			<h3>RIGGED & READY</h3>
+			<div title="track tips"></div>
+		</div>
+		<p>Explore the RAM 1500<br> Outdoorsman<sup>&reg;</sup></p>
+		<a href="/rigged-ready/2015-ram-1500-outdoorsman/" class="blue-round-btn">
+			<span class="m-text-under">READ NOW</span>
+			<div class="m-btn-horizontal"></div>
+			<div class="m-btn-vertical"></div>
+			<span class="m-text-over">READ NOW</span>
+		</a>
+	</div>
+</div><!-- .m-truck-container -->		
+<div class="m-loc">
+	<h2>Choose A Region</h2>
+	<div class="m-loc-wrap">
+		<ul class="clearfix">
+			<li>
+				<a href="/rigged-ready/ne/">
+					<div class="m-loc-circle <?php if (is_category("ne")) { echo "m-shiny-circle"; }?>"></div>
+					<span>NORTHEAST</span>
+				</a>
+			</li>
+			<li>
+				<a href="/rigged-ready/se/">
+					<div class="m-loc-circle <?php if (is_category("se")) { echo "m-shiny-circle"; }?>"></div>
+					<span>SOUTHEAST</span>
+				</a>
+			</li>
+			<li>
+				<a href="/rigged-ready/mw/">
+					<div class="m-loc-circle <?php if (is_category("mw")) { echo "m-shiny-circle"; }?>"></div>
+					<span>MIDWEST</span>
+				</a>
+			</li>
+			<li>
+				<a href="/rigged-ready/sw/">
+					<div class="m-loc-circle <?php if (is_category("sw")) { echo "m-shiny-circle"; }?>"></div>
+					<span>SOUTHWEST</span>
+				</a>
+			</li>
+			<li>
+				<a href="/rigged-ready/nw/">
+					<div class="m-loc-circle <?php if (is_category("nw")) { echo "m-shiny-circle"; }?>"></div>
+					<span>NORTHWEST</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+</div><!-- .m-loc -->			
+
+				
+	
+
+<?php get_footer(); ?>
