1c4f2f40e3b7ea80f952054effc26258ba32da8f
artem.kovalyk <artre8@gmail.com>
2015-06-01 16:54:07 -0400

    sweepstakes banner rigged

--- wp-content/themes/infisherman/category-rigged-ready.php
+++ wp-content/themes/infisherman/category-rigged-ready.php
@@ -46,21 +46,38 @@
 			</a>
 		</li>
 	</ul>
-	<div class="m-truck-container">
-		<div class="m-truck">
-			<div class="m-truck-title clearfix">
-				<h3>RIGGED & READY</h3>
-				<div title="track tips"></div>
+	<div class="m-banners">
+		<div class="m-truck-container">
+			<div class="m-truck">
+				<div class="m-truck-title clearfix">
+					<h3>RIGGED & READY</h3>
+					<div title="track tips"></div>
+				</div>
+				<p>Explore the RAM 1500<br> Outdoorsman<sup>&reg;</sup></p>
+				<a href="/rigged-ready/2015-ram-1500-outdoorsman/" class="blue-round-btn">
+					<span class="m-text-under">READ NOW</span>
+					<div class="m-btn-horizontal"></div>
+					<div class="m-btn-vertical"></div>
+					<span class="m-text-over">READ NOW</span>
+				</a>
 			</div>
-			<p>Explore the RAM 1500<br> Outdoorsman<sup>&reg;</sup></p>
-			<a href="/rigged-ready/2015-ram-1500-outdoorsman/" class="blue-round-btn">
-				<span class="m-text-under">READ NOW</span>
-				<div class="m-btn-horizontal"></div>
-				<div class="m-btn-vertical"></div>
-				<span class="m-text-over">READ NOW</span>
-			</a>
-		</div>
-	</div>	
-</div>				
+		</div><!-- .m-truck-container -->
+		<div class="m-truck-container">
+			<div class="m-truck">
+				<div class="m-truck-title clearfix">
+					<h3>RIGGED & READY</h3>
+					<div title="track tips"></div>
+				</div>
+				<p>Explore the RAM 1500<br> Outdoorsman<sup>&reg;</sup></p>
+				<a href="/rigged-ready/2015-ram-1500-outdoorsman/" class="blue-round-btn">
+					<span class="m-text-under">READ NOW</span>
+					<div class="m-btn-horizontal"></div>
+					<div class="m-btn-vertical"></div>
+					<span class="m-text-over">READ NOW</span>
+				</a>
+			</div>
+		</div><!-- .m-truck-container -->	
+	</div><!-- .m-banners -->
+</div><!-- .m-home-wrap -->				
 				
 <?php get_footer(); ?>
