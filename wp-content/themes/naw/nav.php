d4034c8fc7a3ccbc70a7f6fef7ded69e499af826
Fox Bowden <fox.bowden@imoutdoors.com>
2015-07-30 12:05:03 -0400

    updates to NAW community

--- wp-content/themes/naw/nav.php
+++ wp-content/themes/naw/nav.php
@@ -1,14 +1,20 @@
 <div class="community-header">
 	<div class="header-section community-logo">
-		<a href="/photos/"><img src="/wp-content/themes/naw/images/naw-community.jpg" alt="Reader Photos" title="Reader Photos" /></a>
-		<div class="community-mobile-menu"><div class="select-arrow"></div>Photos Menu</div>
+		<a href="/community/"><img src="/wp-content/themes/naw/images/naw-community.jpg" alt="Reader Photos" title="Reader Photos" /></a>
+		<div class="community-mobile-menu"><div class="select-arrow"></div>Community Menu</div>
 	</div>
-	<?php $photosMenu = get_field("photos_menu", "options"); ?>
+	<?php 
+		$photosMainMenu = get_field("community_main_menu", "options"); 
+		$photosMenu = get_field("community_menu", "options"); 
+	?>
 	<div class="header-section menu-main">
 		<ul class="">
-			<li class="sub-list"><a href="/photos?rut-report" class="photo-menu" slug="rut-report">Rut Report</a></li>
-			<li class="sub-list"><a href="/photos?general-discussion" class="photo-menu" slug="general-discussion">General Discusion</a></li>
-			<li class="sub-list"><a href="/photos?question" class="photo-menu" slug="question">Q&A</a></li>	
+			<?php if( $photosMainMenu ){ 
+				foreach( $photosMainMenu as $menu ){  
+					$categoryList = get_term_by('id', $menu, 'category'); ?>
+					<li class="sub-list"><a href="/community?<?php echo $categoryList->slug; ?>" class="photo-menu" slug="<?php echo $categoryList->slug; ?>"><?php echo $categoryList->name; ?></a></li>
+				<?php } ?>
+			<?php } ?>
 		</ul>
 	</div>
 	<div class="header-section menu-hunt">
@@ -16,7 +22,7 @@
 			<?php if( $photosMenu ){ 
 				foreach( $photosMenu as $menu ){  
 					$categoryList = get_term_by('id', $menu, 'category'); ?>
-					<li class="sub-list"><a href="/photos?<?php echo $categoryList->slug; ?>" class="photo-menu" slug="<?php echo $categoryList->slug; ?>"><?php echo $categoryList->name; ?></a></li>
+					<li class="sub-list"><a href="/community?<?php echo $categoryList->slug; ?>" class="photo-menu" slug="<?php echo $categoryList->slug; ?>"><?php echo $categoryList->name; ?></a></li>
 				<?php } ?>
 			<?php } ?>
 		</ul>
@@ -27,58 +33,58 @@
 			<div class="select-arrow"></div>
 			<select id="state-select">
 				<option value="" selected>Browse by State</option>
-				<option value="/photos?arizona">Arizona</option>
-				<option value="/photos?Alabama">Alabama</option>
-				<option value="/photos?Alaska">Alaska</option>
-				<option value="/photos?Arizona">Arizona</option>
-				<option value="/photos?Arkansas">Arkansas</option>
-				<option value="/photos?California">California</option>
-				<option value="/photos?Colorado">Colorado</option>
-				<option value="/photos?Connecticut">Connecticut</option>
-				<option value="/photos?Delaware">Delaware</option>
-				<option value="/photos?District-Of-Columbia">District Of Columbia</option>
-				<option value="/photos?Florida">Florida</option>
-				<option value="/photos?Georgia">Georgia</option>
-				<option value="/photos?Hawaii">Hawaii</option>
-				<option value="/photos?Idaho">Idaho</option>
-				<option value="/photos?Illinois">Illinois</option>
-				<option value="/photos?Indiana">Indiana</option>
-				<option value="/photos?Iowa">Iowa</option>
-				<option value="/photos?Kansas">Kansas</option>
-				<option value="/photos?Kentucky">Kentucky</option>
-				<option value="/photos?Louisiana">Louisiana</option>
-				<option value="/photos?Maine">Maine</option>
-				<option value="/photos?Maryland">Maryland</option>
-				<option value="/photos?Massachusetts">Massachusetts</option>
-				<option value="/photos?Michigan">Michigan</option>
-				<option value="/photos?Minnesota">Minnesota</option>
-				<option value="/photos?Mississippi">Mississippi</option>
-				<option value="/photos?Missouri">Missouri</option>
-				<option value="/photos?Montana">Montana</option>
-				<option value="/photos?Nebraska">Nebraska</option>
-				<option value="/photos?Nevada">Nevada</option>
-				<option value="/photos?New-Hampshire">New Hampshire</option>
-				<option value="/photos?New-Jersey">New Jersey</option>
-				<option value="/photos?New-Mexico">New Mexico</option>
-				<option value="/photos?New-York">New York</option>
-				<option value="/photos?North-Carolina">North Carolina</option>
-				<option value="/photos?North-Dakota">North Dakota</option>
-				<option value="/photos?Ohio">Ohio</option>
-				<option value="/photos?Oklahoma">Oklahoma</option>
-				<option value="/photos?Oregon">Oregon</option>
-				<option value="/photos?Pennsylvania">Pennsylvania</option>
-				<option value="/photos?Rhode-Island">Rhode Island</option>
-				<option value="/photos?South-Carolina">South Carolina</option>
-				<option value="/photos?South-Dakota">South Dakota</option>
-				<option value="/photos?Tennessee">Tennessee</option>
-				<option value="/photos?Texas">Texas</option>
-				<option value="/photos?Utah">Utah</option>
-				<option value="/photos?Vermont">Vermont</option>
-				<option value="/photos?Virginia">Virginia</option>
-				<option value="/photos?Washington">Washington</option>
-				<option value="/photos?West-Virginia">West Virginia</option>
-				<option value="/photos?Wisconsin">Wisconsin</option>
-				<option value="/photos?Wyoming">Wyoming</option>
+				<option value="/community?arizona">Arizona</option>
+				<option value="/community?Alabama">Alabama</option>
+				<option value="/community?Alaska">Alaska</option>
+				<option value="/community?Arizona">Arizona</option>
+				<option value="/community?Arkansas">Arkansas</option>
+				<option value="/community?California">California</option>
+				<option value="/community?Colorado">Colorado</option>
+				<option value="/community?Connecticut">Connecticut</option>
+				<option value="/community?Delaware">Delaware</option>
+				<option value="/community?District-Of-Columbia">District Of Columbia</option>
+				<option value="/community?Florida">Florida</option>
+				<option value="/community?Georgia">Georgia</option>
+				<option value="/community?Hawaii">Hawaii</option>
+				<option value="/community?Idaho">Idaho</option>
+				<option value="/community?Illinois">Illinois</option>
+				<option value="/community?Indiana">Indiana</option>
+				<option value="/community?Iowa">Iowa</option>
+				<option value="/community?Kansas">Kansas</option>
+				<option value="/community?Kentucky">Kentucky</option>
+				<option value="/community?Louisiana">Louisiana</option>
+				<option value="/community?Maine">Maine</option>
+				<option value="/community?Maryland">Maryland</option>
+				<option value="/community?Massachusetts">Massachusetts</option>
+				<option value="/community?Michigan">Michigan</option>
+				<option value="/community?Minnesota">Minnesota</option>
+				<option value="/community?Mississippi">Mississippi</option>
+				<option value="/community?Missouri">Missouri</option>
+				<option value="/community?Montana">Montana</option>
+				<option value="/community?Nebraska">Nebraska</option>
+				<option value="/community?Nevada">Nevada</option>
+				<option value="/community?New-Hampshire">New Hampshire</option>
+				<option value="/community?New-Jersey">New Jersey</option>
+				<option value="/community?New-Mexico">New Mexico</option>
+				<option value="/community?New-York">New York</option>
+				<option value="/community?North-Carolina">North Carolina</option>
+				<option value="/community?North-Dakota">North Dakota</option>
+				<option value="/community?Ohio">Ohio</option>
+				<option value="/community?Oklahoma">Oklahoma</option>
+				<option value="/community?Oregon">Oregon</option>
+				<option value="/community?Pennsylvania">Pennsylvania</option>
+				<option value="/community?Rhode-Island">Rhode Island</option>
+				<option value="/community?South-Carolina">South Carolina</option>
+				<option value="/community?South-Dakota">South Dakota</option>
+				<option value="/community?Tennessee">Tennessee</option>
+				<option value="/community?Texas">Texas</option>
+				<option value="/community?Utah">Utah</option>
+				<option value="/community?Vermont">Vermont</option>
+				<option value="/community?Virginia">Virginia</option>
+				<option value="/community?Washington">Washington</option>
+				<option value="/community?West-Virginia">West Virginia</option>
+				<option value="/community?Wisconsin">Wisconsin</option>
+				<option value="/community?Wyoming">Wyoming</option>
 			</select>
 		</div>
 	</div>
