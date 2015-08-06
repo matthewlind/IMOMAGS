e66e41192321f0899343dc750e07ac3ad8bfbc54
Fox Bowden <fox.bowden@imoutdoors.com>
2015-08-06 11:40:39 -0400

    removing community authors from post settings

--- wp-content/themes/naw/functions.php
+++ wp-content/themes/naw/functions.php
@@ -20,28 +20,6 @@ function custom_post_author_archive( &$query )
 }
 add_action( 'pre_get_posts', 'custom_post_author_archive' );
 
-
-add_filter('wp_dropdown_users', 'MySwitchUser');
-function MySwitchUser($output)
-{
-
-    //global $post is available here, hence you can check for the post type here
-    $users = get_users();
-
-    $output = "<select id=\"post_author_override\" name=\"post_author_override\" class=\"\">";
-
-    //Leave the admin in the list
-    //$output .= "<option value=\"1\">Admin</option>";
-    foreach($users as $user)
-    {
-        $sel = ($post->post_author == $user->ID)?"selected='selected'":'';
-        $output .= '<option value="'.$user->ID.'"'.$sel.'>'.$user->user_login.'</option>';
-    }
-    $output .= "</select>";
-
-    return $output;
-}
-
 add_action( 'init', 'my_add_shortcodes' );
 
 function my_add_shortcodes() {
