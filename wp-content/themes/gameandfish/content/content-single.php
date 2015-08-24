f2e19c40b0cac158c3537e6debeffb90a052b300
Fox Bowden <fox.bowden@imoutdoors.com>
2015-08-24 14:25:51 -0400

    fixes for e commerce widget

--- wp-content/themes/gameandfish/content/content-single.php
+++ wp-content/themes/gameandfish/content/content-single.php
@@ -53,7 +53,7 @@ $acf_byline = get_field("byline",$postID);
         		
         <?php //the_widget('imo_related_footer_widget'); ?>
 
-	    <?php imo_ad_placement("e_commerce_widget"); ?>	
+	    <?php imo_ad_placement("e_commerce_widget_564x252"); ?>	
 
 	    <?php if ( function_exists('yarpp_plugin_activate') ): ?>
 		    <?php if ( isset_related_posts() ): ?>
