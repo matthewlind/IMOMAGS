478cd94e4584490ecba2c4854dd4e51488ec5d02
artem.kovalyk <artre8@gmail.com>
2015-09-01 14:48:20 -0400

    one css file for rigged ready and deer zone

--- wp-content/themes/imo-mags-parent/css/styles-microsites.php
+++ wp-content/themes/imo-mags-parent/css/styles-microsites.php
@@ -60,7 +60,9 @@
 		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_directory' ); ?>/css/microsite-css/gameandfish/microsite-crossbows.css" />
 				
 <?php	} elseif ( is_category($deer_zone_cat) || in_category($deer_zone_cat)) { ?>
+		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_directory' ); ?>/css/microsite-css/rigged-ready-default.css" />
 		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_directory' ); ?>/css/microsite-css/gameandfish/microsite-deer-zone.css" />
+		
 <?php // This meta is inserted to display featured share image for facebook
 				if ( is_category('sweeps')) { ?>
 <!-- 					<meta property="og:image" content="http://www.in-fisherman.com/files/2015/06/RR-sweeps-no-btn-968x504FB.jpg" /> -->
@@ -82,6 +84,7 @@
 		
 		if ( is_category($rigged_cat) || in_category($rigged_cat)) {	
 	?>
+		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_directory' ); ?>/css/microsite-css/rigged-ready-default.css" />
 		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_directory' ); ?>/css/microsite-css/in-fisherman/microsite-rigged-ready.css" />
 			<?php // This meta is inserted to display featured share image for facebook
 				if ( is_category('sweeps')) { ?>
