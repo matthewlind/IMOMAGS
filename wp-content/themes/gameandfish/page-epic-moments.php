95415371c98568890c96793873b64bd6983fca70
artem.kovalyk <artre8@gmail.com>
2015-09-25 15:24:41 -0400

    honda epic moments

--- wp-content/themes/gameandfish/page-epic-moments.php
+++ wp-content/themes/gameandfish/page-epic-moments.php
@@ -7,15 +7,12 @@
 	<div class="m-article-image">
 		<div class="m-sweep-text">
 			<img class="epic-logo" src="<?php bloginfo('stylesheet_directory');?>/images/epic-moments/logo-epic-moment.png">
-			<p>We’ve all had those memorable, never-to-happen-again outdoors-experiences with family and friends that are worth sharing with fellow sportsmen – and we can’t wait to hear about yours. If it’s truly epic, you could <span>WIN</span> another...</p>
+			<p>We’ve all had those memorable, never-to-happen-again outdoors-experiences with family and friends that are worth sharing with fellow sportsmen – and we can’t wait to hear about yours. If it’s truly epic, you could <span>WIN</span> your own epic moment, fishing with a pro brought to you by the all new Honda Pioneer 1000, Game & Fish and Major League Fishing.</p>
 			<div class="m-sponsors clearfix">
 				<img src="<?php bloginfo('stylesheet_directory');?>/images/epic-moments/MLF_Logo.png">
 				<img src="<?php bloginfo('stylesheet_directory');?>/images/epic-moments/honda-logo.jpg">
+				<img src="<?php bloginfo('stylesheet_directory');?>/images/epic-moments/pioneer1000.png">
 			</div>
-		</div>
-		<div class="m-ATV">
-			<img src="<?php bloginfo('stylesheet_directory');?>/images/epic-moments/pioneer1000.png">
-			<img src="<?php bloginfo('stylesheet_directory');?>/images/epic-moments/ATV.png">
 		</div>
 	</div><!-- .m-article-image -->
 	<article class="m-article clearfix">
@@ -39,25 +36,142 @@
 		        <script type="text/javascript" src="https://www.viralsweep.com/external/widget.js"></script>
 			</div>
 			<div class="m-sweep-right">
-				<p>Fish 3 Full Action Packed Days with Fishing Nosara. Take a break to explore the Rain Forest Canopy , take a Horseback adventure into the Mountains , Snorkel the perfect beaches and reefs around  Nosara and explore the area in your street legal Safari Cart.</p>
+				<p>Share your Epic Moment & you could WIN a trip to a Major League Fishing Bass Pro Summit event!</p>
 				<div class="video-container">
 					<iframe width="560" height="315" src="https://www.youtube.com/embed/HdWgmCikSas" frameborder="0" allowfullscreen></iframe>
 				</div>
-				<p>Trip is available in October or November 2015. Arrive Saturday, depart the following Saturday. Trip includes Roundtrip Airfare for 2 from nearest major city in 48 states, all meals, 7 nights lodging at Nosara Paradise Rentals, airport transfers in Costa Rica, Rain Forest and Horseback adventures, 3 full days blue water fishing with Fishing Nosara, use of Safari cart during stay and gratuities.
-				</p>
+				<div class="epic-ad-300x600">
+					
+				</div>
 			</div>
 		</div><!-- .m-sweep-blocks -->
-		<div class="m-social-wrap">
-			<ul class="share-count social-buttons">
-				<li>
-					<a href="http://www.facebook.com/sharer.php?u=<?php echo site_url() . $_SERVER['REQUEST_URI']; ?>&t=Rigged and Ready RAM Outdoorsman Sweepstakes" class="socialite facebook-like reload-fb" data-href="<?php echo site_url() . $_SERVER['REQUEST_URI']; ?>" data-send="false" data-layout="button_count" data-share="true" data-action="like" data-width="60" data-show-faces="false" rel="nofollow" target="_blank"><span class="vhidden"></span></a> 
-				</li>
-			    <li>
-			         <a href="http://twitter.com/share" class="socialite twitter-share reload-twitter" data-text="Rigged and Ready RAM Outdoorsman Sweepstakes" data-url="<?php echo site_url() . $_SERVER['REQUEST_URI']; ?>" rel="nofollow" target="_blank"><span class="vhidden"></span></a> 
-			    </li>
-			</ul>
-		</div><!-- .m-social-wrap-->
+		<div class="editors-block">
+			<h1>Editors Epic Moments</h1>
+			<div class="editors-articles clearfix">
+				<div class="epic-article">
+					<a href="">
+						<div class="epic-header clearfix">
+							<div class="epic-author"></div>
+							<div class="epic-info">
+								<span>Mike Schoby</span>
+								<h1>Hunting Around The Harvest Lorem Ipusm Dolor</h1>
+							</div>
+						</div>
+						<div class="epic-image"></div>
+					</a>
+				</div>
+				<div class="epic-article">
+					<a href="">
+						<div class="epic-header clearfix">
+							<div class="epic-author"></div>
+							<div class="epic-info">
+								<span>Mike Schoby</span>
+								<h1>Hunting Around The Harvest Lorem Ipusm Dolor</h1>
+							</div>
+						</div>
+						<div class="epic-image"></div>
+					</a>
+				</div>
+				<div class="epic-article">
+					<a href="">
+						<div class="epic-header clearfix">
+							<div class="epic-author"></div>
+							<div class="epic-info">
+								<span>Mike Schoby</span>
+								<h1>Hunting Around The Harvest Lorem Ipusm Dolor</h1>
+							</div>
+						</div>
+						<div class="epic-image"></div>
+					</a>
+				</div>
+				<div class="epic-article">
+					<a href="">
+						<div class="epic-header clearfix">
+							<div class="epic-author"></div>
+							<div class="epic-info">
+								<span>Mike Schoby</span>
+								<h1>Hunting Around The Harvest Lorem Ipusm Dolor</h1>
+							</div>
+						</div>
+						<div class="epic-image"></div>
+					</a>
+				</div>
+				<div class="epic-article">
+					<a href="">
+						<div class="epic-header clearfix">
+							<div class="epic-author"></div>
+							<div class="epic-info">
+								<span>Mike Schoby</span>
+								<h1>Hunting Around The Harvest Lorem Ipusm Dolor</h1>
+							</div>
+						</div>
+						<div class="epic-image"></div>
+					</a>
+				</div>
+				<div class="epic-article">
+					<a href="">
+						<div class="epic-header clearfix">
+							<div class="epic-author"></div>
+							<div class="epic-info">
+								<span>Mike Schoby</span>
+								<h1>Hunting Around The Harvest Lorem Ipusm Dolor</h1>
+							</div>
+						</div>
+						<div class="epic-image"></div>
+					</a>
+				</div>
+				<div class="epic-article">
+					<a href="">
+						<div class="epic-header clearfix">
+							<div class="epic-author"></div>
+							<div class="epic-info">
+								<span>Mike Schoby</span>
+								<h1>Hunting Around The Harvest Lorem Ipusm Dolor</h1>
+							</div>
+						</div>
+						<div class="epic-image"></div>
+					</a>
+				</div>
+				<div class="epic-article">
+					<a href="">
+						<div class="epic-header clearfix">
+							<div class="epic-author"></div>
+							<div class="epic-info">
+								<span>Mike Schoby</span>
+								<h1>Hunting Around The Harvest Lorem Ipusm Dolor</h1>
+							</div>
+						</div>
+						<div class="epic-image"></div>
+					</a>
+				</div>
+				<div class="epic-article">
+					<a href="">
+						<div class="epic-header clearfix">
+							<div class="epic-author"></div>
+							<div class="epic-info">
+								<span>Mike Schoby</span>
+								<h1>Hunting Around The Harvest Lorem Ipusm Dolor</h1>
+							</div>
+						</div>
+						<div class="epic-image"></div>
+					</a>
+				</div>
+			</div><!-- .editors-articles -->
+			<div class="article-bottom">
+				<div class="m-social-wrap">
+					<span>Like This Page</span>
+					<ul class="share-count social-buttons">
+						<li>
+							<a href="http://www.facebook.com/sharer.php?u=<?php echo site_url() . $_SERVER['REQUEST_URI']; ?>&t=Rigged and Ready RAM Outdoorsman Sweepstakes" class="socialite facebook-like reload-fb" data-href="<?php echo site_url() . $_SERVER['REQUEST_URI']; ?>" data-send="false" data-layout="button_count" data-share="true" data-action="like" data-width="60" data-show-faces="false" rel="nofollow" target="_blank"><span class="vhidden"></span></a> 
+						</li>
+					    <li>
+					         <a href="http://twitter.com/share" class="socialite twitter-share reload-twitter" data-text="Rigged and Ready RAM Outdoorsman Sweepstakes" data-url="<?php echo site_url() . $_SERVER['REQUEST_URI']; ?>" rel="nofollow" target="_blank"><span class="vhidden"></span></a> 
+					    </li>
+					</ul>
+				</div><!-- .m-social-wrap-->
+				<div class="ad-300x250"></div>
+			</div><!-- .article-bottm -->
+		</div><!-- .editors-block-->
 	</article>
-<!-- 	<script src="<?php bloginfo('stylesheet_directory');?>/js/sweeps.js"></script> -->
 </div><!-- .m-article-wrap -->
 <?php get_footer(); ?>
