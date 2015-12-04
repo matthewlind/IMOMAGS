4950ae75b88369837bf7963d985e1c106e54d4dd
artem.kovalyk <artre8@gmail.com>
2015-12-04 13:31:39 -0500

    footer, image sprites

--- wp-content/themes/imo-mags-parent/footer/footer-default.php
+++ wp-content/themes/imo-mags-parent/footer/footer-default.php
@@ -22,22 +22,22 @@ _deprecated_file( sprintf( __( 'Theme without %1$s' ), basename(__FILE__) ), '3.
 		                        <div class="column">
 		                            <h5>Hunt</h5>
 		                            <ul class="intermedia-list">
-		                                <li><a target="_blank" href="http://www.bowhuntingmag.com/"><span>Petersen's Bowhunting</span></a></li>
-		                                <li><a target="_blank" href="http://www.northamericanwhitetail.com/"><span>North American Whitetail</span></a></li>
-		                                <li><a target="_blank" href="http://www.gundogmag.com/"><span>Gun Dog</span></a></li>
-		                                <li><a target="_blank" href="http://www.wildfowlmag.com/"><span>Wildfowl</span></a></li>
-		                                <li><a target="_blank" href="http://www.bowhunter.com/"><span>Bowhunter</span></a></li>
-		                                <li><a target="_blank" href="http://www.petersenshunting.com/"><span>Petersen's Hunting</span></a></li>
+		                                <li class="f-p-bowhunting"><a target="_blank" href="http://www.bowhuntingmag.com/"><span>Petersen's Bowhunting</span></a></li>
+		                                <li class="f-na-whitetaile"><a target="_blank" href="http://www.northamericanwhitetail.com/"><span>North American Whitetail</span></a></li>
+		                                <li class="f-gun-dog"><a target="_blank" href="http://www.gundogmag.com/"><span>Gun Dog</span></a></li>
+		                                <li class="f-wildfowl"><a target="_blank" href="http://www.wildfowlmag.com/"><span>Wildfowl</span></a></li>
+		                                <li class="f-bowhunter"><a target="_blank" href="http://www.bowhunter.com/"><span>Bowhunter</span></a></li>
+		                                <li class="f-hunting"><a target="_blank" href="http://www.petersenshunting.com/"><span>Petersen's Hunting</span></a></li>
 		                            </ul>
 		                        </div>
 		                        <div class="column">
 		                            <h5>Shoot</h5>
 		                            <ul class="intermedia-list">
-		                                <li><a target="_blank" href="http://www.handgunsmag.com/"><span>Handguns</span></a></li>
-		                                <li><a target="_blank" href="http://www.rifleshootermag.com/"><span>Rifleshooter</span></a></li>
-		                                <li><a target="_blank" href="http://www.shootingtimes.com/"><span>Shooting Times</span></a></li>
-		                                <li><a target="_blank" href="http://www.shotgunnews.com/"><span>Shotgun News</span></a></li>
-		                                <li><a target="_blank" href="http://www.gunsandammo.com/"><span>Guns &amp; Ammo</span></a></li>
+		                                <li class="f-handguns"><a target="_blank" href="http://www.handgunsmag.com/"><span>Handguns</span></a></li>
+		                                <li class="f-rifleshooter"><a target="_blank" href="http://www.rifleshootermag.com/"><span>Rifleshooter</span></a></li>
+		                                <li class="f-shooting-times"><a target="_blank" href="http://www.shootingtimes.com/"><span>Shooting Times</span></a></li>
+		                                <li class="f-firearms-news"><a target="_blank" href="http://www.shotgunnews.com/"><span>Firearms News</span></a></li>
+		                                <li class="f-guns-and-ammo"><a target="_blank" href="http://www.gunsandammo.com/"><span>Guns &amp; Ammo</span></a></li>
 		                            </ul>
 		                        </div>
 		                    </div>
@@ -45,20 +45,20 @@ _deprecated_file( sprintf( __( 'Theme without %1$s' ), basename(__FILE__) ), '3.
 		                        <div class="column column3">
 		                            <h5>Fish</h5>
 		                            <ul class="intermedia-list">
-		                                <li><a href="http://www.in-fisherman.com" target="_blank"><span>In-Fisherman</span></a></li>
-		                                <li><a target="_blank" href="http://www.flyfisherman.com"><span>Fly Fisherman</span></a></li>
-		                                <li><a target="_blank" href="http://www.floridasportsman.com/"><span>Florida Sportsman</span></a></li>
-		                                <li><a target="_blank" href="http://www.bassfan.com/"><span>BassFan</span></a></li>
+		                                <li class="f-in-fisherman"><a href="http://www.in-fisherman.com" target="_blank"><span>In-Fisherman</span></a></li>
+		                                <li class="f-fly-fisherman"><a target="_blank" href="http://www.flyfisherman.com"><span>Fly Fisherman</span></a></li>
+		                                <li class="f-florida-sportsman"><a target="_blank" href="http://www.floridasportsman.com/"><span>Florida Sportsman</span></a></li>
+		                                <li class="f-bass-fan"><a target="_blank" href="http://www.bassfan.com/"><span>BassFan</span></a></li>
 		                            </ul>
 		                        </div>
 		                        <div class="column column4">
 		                            <h5>TV & More</h5>
 		                            <ul class="intermedia-list">
-		                            	<li><a target="_blank" href="http://www.outdoorchannel.com/"><span>Outdoor Channel</span></a></li>
-		                            	<li><a target="_blank" href="http://www.thesportsmanchannel.com/"><span>The Sportsman Channel</span></a></li>
-		                            	<li><a target="_blank" href="http://www.worldfishingnetwork.com/"><span>World Fishing Network</span></a></li>
-		                                <li><a target="_blank" href="http://www.gameandfishmag.com/"><span>Game &amp; Fish</span></a></li>
-		                                <li><a target="_blank" href="http://store.imoutdoors.com/"><span>The IMO Store</span></a></li>
+		                            	<li class="f-outdoor-channel"><a target="_blank" href="http://www.outdoorchannel.com/"><span>Outdoor Channel</span></a></li>
+		                            	<li class="f-sportsman-ch"><a target="_blank" href="http://www.thesportsmanchannel.com/"><span>The Sportsman Channel</span></a></li>
+		                            	<li class="f-wfn"><a target="_blank" href="http://www.worldfishingnetwork.com/"><span>World Fishing Network</span></a></li>
+		                                <li class="f-game-and-fish"><a target="_blank" href="http://www.gameandfishmag.com/"><span>Game &amp; Fish</span></a></li>
+		                                <li class="f-imo-store"><a target="_blank" href="http://store.imoutdoors.com/"><span>The IMO Store</span></a></li>
 		                            </ul>
 		                        </div>
 		                    </div>
