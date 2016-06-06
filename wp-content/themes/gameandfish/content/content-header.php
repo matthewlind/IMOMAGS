0d7c69874385f8be075299778f52fe546a8c13d7
Fox Bowden <fox.bowden@imoutdoors.com>
2016-06-06 15:01:09 -0400

    adding iframe ads to other site headers

--- wp-content/themes/gameandfish/content/content-header.php
+++ wp-content/themes/gameandfish/content/content-header.php
@@ -356,14 +356,23 @@
            <a href="#">X</a>
         </div>
         <div class="content-banner-section">
-			<div class="mob-mdl-banner">
-				<?php imo_ad_placement("320_atf"); ?>
-			</div>
-			<div class="mdl-banner">
-				<?php 
-				imo_ad_placement("leaderboard"); 
-				imo_ad_placement("billboard"); 
-				?>
+			<?php if(mobile()){ ?>
+		        	<div class="mob-mdl-banner">
+						<?php iframe_ad("320_atf"); ?>
+					</div>
+				<?php } ?>
+				<div class="mdl-banner">
+					<?php 
+					if(!mobile() && !tablet()){
+					 	iframe_ad("billboard");
+					 }
+					if(tablet()){
+						iframe_ad("leaderboard");	
+					 }
+					 
+					
+					?>
+
 			</div>
         </div>
 		<div id="main" class="main clearfix js-responsive-layout">
