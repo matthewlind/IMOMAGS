e93694fe7f032208e6bd2f8d9f24eedc1a26e75a
artem.kovalyk <artre8@gmail.com>
2016-07-07 15:30:55 -0400

    bigger bucks microsite

--- wp-content/plugins/osg-gallery/plugin.php
+++ wp-content/plugins/osg-gallery/plugin.php
@@ -69,7 +69,6 @@ function print_gallery_script() {
 
 add_shortcode( 'imo-slideshow', 'imo_flex_gallery' );
 
-/*
 // [slideshow gallery=GALLERY_ID]
 function imo_flex_gallery( $atts ) {
 	global $add_gallery_script;
@@ -165,7 +164,6 @@ function imo_flex_gallery( $atts ) {
 
 wp_enqueue_style('flexslider-css',get_template_directory_uri().'/plugins/flexslider/flexslider.css');
 wp_enqueue_style('gallery-css',plugins_url('css/style.css', __FILE__));
-*/
 
 
 
