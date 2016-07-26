5bf2a06e65d35b6f6fc7dd4a911a3b5b5abe98ba
artem.kovalyk <artre8@gmail.com>
2016-07-26 12:00:31 -0400

    bigger bucks bugs

--- wp-content/themes/naw/category-bigger-bucks.php
+++ wp-content/themes/naw/category-bigger-bucks.php
@@ -3,8 +3,7 @@
 	$microsite_default = true;
 	get_header();
 	
-	//include('../imo-mags-parent/content/microsite-category/microsite-category-default.php');
+	include(get_template_directory() . '/content/microsite-category/microsite-category-default.php');
 	
-	get_template_part( '../imo-mags-parent/content/microsite-category/microsite-category', 'default' ); 
 	get_template_part( '../imo-mags-parent/footer/footer', 'microsite' ); 
 ?>
