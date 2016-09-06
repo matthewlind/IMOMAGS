05aeb74b5a9abca82dec4196d822c583f6a10770
artem.kovalyk <artre8@gmail.com>
2016-09-06 15:01:47 -0400

    remington pass

--- wp-content/themes/gunsandammo/page-remington200.php
+++ wp-content/themes/gunsandammo/page-remington200.php
@@ -1,3 +1,27 @@
-<?php get_template_part('remington/index'); ?>
+<?php 
+
+$user = "";
+$pass = "";
+
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+}
+
+if($user == "remington" && $pass == "Bicentennial200@") {
+    get_template_part('remington/index');
+}
+else {
+    if(isset($_POST)) {?>
+
+            <form method="POST" action="<?php echo htmlspecialchars(get_permalink()); ?>">
+            User <input type="text" name="user"></input><br/>
+            Pass <input type="password" name="pass"></input><br/>
+            <input type="submit" name="submit" value="Go"></input>
+            </form>
+    <?php }
+}
+?>
+
 
 
