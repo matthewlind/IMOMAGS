bd5301bfc202b3baec7a9317a67d88882d8e17d8
artem.kovalyk <artre8@gmail.com>
2016-10-17 21:50:20 -0400

    Microsite video page, option for YouTube video

--- wp-content/themes/imo-mags-parent/page-templates/microsite-video.php
+++ wp-content/themes/imo-mags-parent/page-templates/microsite-video.php
@@ -9,64 +9,140 @@ $microsite_default = true;
 get_header();
 
 $feat_video_id = get_field("feat_video_id");
+$v_source = get_field("video_service");
+$y_feat_title = "";
+
 ?>
 <div class="content" id="uppp">
 	<div class="mv-video-atf">
 		<div class="mv-player-wrap">
 			<div class="mv-player">
+				<?php if ($v_source == 'youtube') { 
+					$y_feat_title = get_field('youtube_video_title');
+				?>
 				<!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
 			    <div id="player"></div>
-			
 			    <script>
-			      // 2. This code loads the IFrame Player API code asynchronously.
-			      var tag = document.createElement('script');
-			
-			      tag.src = "https://www.youtube.com/iframe_api";
-			      var firstScriptTag = document.getElementsByTagName('script')[0];
-			      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-			
-			      // 3. This function creates an <iframe> (and YouTube player)
-			      //    after the API code downloads.
-			      var player;
-			      function onYouTubeIframeAPIReady() {
-			        player = new YT.Player('player', {
-			          height: '390',
-			          width: '640',
-			          videoId: '<?php echo $feat_video_id; ?>',
-			          events: {
-			            'onReady': onPlayerReady,
-			            'onStateChange': onPlayerStateChange
-			          }
-			        });
-			      }
-			
-			      // 4. The API will call this function when the video player is ready.
-			      function onPlayerReady(event) {
-			        event.target.playVideo();
-			      }
-			
-			      // 5. The API calls this function when the player's state changes.
-			      //    The function indicates that when playing a video (state=1),
-			      //    the player should play for six seconds and then stop.
-			      var done = false;
-			      function onPlayerStateChange(event) {
-			        if (event.data == YT.PlayerState.PLAYING && !done) {
-			          setTimeout(stopVideo, 10000);
-			          done = true;
-			        }
-			      }
-			      function stopVideo() {
-			        player.stopVideo();
-			      }
-				  	function loadVideo(videoID) {
-						if(player) { player.loadVideoById(videoID, 1, "large"); }
-						jQuery("html, body").animate({scrollTop: 0}, 1000, "swing");
+					// 2. This code loads the IFrame Player API code asynchronously.
+					var tag = document.createElement('script');
+					tag.src = "https://www.youtube.com/iframe_api";
+					var firstScriptTag = document.getElementsByTagName('script')[0];
+					firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+					// 3. This function creates an <iframe> (and YouTube player)
+					//    after the API code downloads.
+					var player;
+					function onYouTubeIframeAPIReady() {
+						player = new YT.Player('player', {
+							height: '390',
+							width: '640',
+							videoId: '<?php echo $feat_video_id; ?>',
+							events: {
+							'onReady': onPlayerReady,
+							'onStateChange': onPlayerStateChange
+							}
+						});
 					}
+					// 4. The API will call this function when the video player is ready.
+					function onPlayerReady(event) {
+						event.target.playVideo();
+					}
+					// 5. The API calls this function when the player's state changes.
+					//    The function indicates that when playing a video (state=1),
+					//    the player should play for ten seconds and then stop.
+					var done = false;
+					function onPlayerStateChange(event) {
+						if (event.data == YT.PlayerState.PLAYING && !done) {
+							setTimeout(stopVideo, 10000);
+							done = true;
+						}
+					}
+					function stopVideo() {
+						player.stopVideo();
+					}
+	
+					jQuery(document).ready(function($) {
+						$("#mv_list > li").click(function(){
+							var d = $(this),
+								vid = d.data('vid'),
+								title = d.find("h5").text();
+								
+							if(player) { player.loadVideoById(vid, 1, "large"); }
+							$("html, body").animate({scrollTop: 80}, 1000, "swing");
+							console.log("title: "+title);
+							$("#mv_title").text(title);
+						});
+					
+					});
 			    </script>
+			    
+			    <?php } else { ?>
+			    <!-- Start of Brightcove Player -->
+				<!--By use of this code snippet, I agree to the Brightcove Publisher T and C found at https://accounts.brightcove.com/en/terms-and-conditions/. -->
+				<script language="JavaScript" type="text/javascript" src="http://admin.brightcove.com/js/BrightcoveExperiences.js"></script>
+				<div id="mv_player">
+					<object id="myExperience" class="BrightcoveExperience">
+						<param name="bgcolor" value="#FFFFFF" />
+						<param name="playerID" value="5127750281001" />
+						<param name="playerKey" value="AQ~~,AAAAALyrRUk~,m8Wuv4JIiTobaElEYjriRAAaiqhciIls" />
+						<param name="isVid" value="true" />
+						<param name="isUI" value="true" />
+						<param name="dynamicStreaming" value="true" />
+						<param name="@videoPlayer" value="<?php echo $feat_video_id; ?>" />
+						<!-- smart player api params -->
+						<param name="includeAPI" value="true" />
+						<param name="templateLoadHandler" value="onTemplateLoaded" />
+						<param name="templateReadyHandler" value="BCL.onTemplateReady" />
+					</object>
+					<!-- 
+					This script tag will cause the Brightcove Players defined above it to be created as soon as the line is read by the browser. If you wish to have the player instantiated only after the rest of the HTML is processed and the page load is complete, remove the line.
+					-->
+					<script type="text/javascript">brightcove.createExperiences();</script>
+				</div>
+				<!-- End of Brightcove Player -->
+				<script>
+					var videoPlayer;
+					
+					// On page load, add title and description text
+					function onTemplateLoaded(id) {
+						var player = brightcove.api.getExperience(id);
+						videoPlayer = player.getModule(brightcove.api.modules.APIModules.VIDEO_PLAYER);
+						
+						setTimeout(function(){
+							videoPlayer.getCurrentVideo( function(video) {
+								var display_name = video.displayName,
+								long_desc = video.longDescription;
+								jQuery("#mv_title").text(display_name);
+								if (long_desc == null) { jQuery("#mv_description").text(""); } else { jQuery("#mv_description").text(long_desc);}
+								
+							});
+						}, 400)
+					}
+					
+					jQuery(document).ready(function($) {
+						$("#mv_list > li").click(function(){
+							var d = $(this),
+								vid = d.data('vid'),
+								title = d.find("h5").text();
+								
+							videoPlayer.loadVideoByID(vid);
+							setTimeout(function(){
+								videoPlayer.getCurrentVideo( function(video) {
+									display_name = video.displayName,
+									long_desc = video.longDescription;
+									jQuery("#mv_title").text(display_name);
+									if (long_desc == null) { jQuery("#mv_description").text("");} else {jQuery("#mv_description").text(long_desc);}
+								});
+							}, 400)
+							$("html, body").animate({scrollTop: 80}, 1000, "swing");
+						});
+					
+					});
+				</script>			    
+			    <?php } ?>
 			</div>
 			
 			<div id="mv_info" class="mv-info clearfix">
-				<h1 id="mv_title"></h1>
+				<h1 id="mv_title"><?php echo $y_feat_title; ?></h1>
 				<p id="mv_description"></p>
 				<div id="mv_ad" class="mv-ad">
 					<script>
@@ -104,11 +180,11 @@ $feat_video_id = get_field("feat_video_id");
 						}
 					}
 			?>
-					<li class="<?php echo $video_cats; ?>" onclick="loadVideo('<?php echo $video_id; ?>')">
+					<li class="<?php echo $video_cats; ?>" data-vid="<?php echo $video_id; ?>">
 						<div class="mv-video-thumb" style="background-image: url(<?php echo $vm_image; ?>);">
 							<i class="icon-play"></i>
 						</div>
-						<h5><?php echo $title; ?></h5>
+						<h5 class="mv-thumb-title"><?php echo $title; echo $video_source;?></h5>
 					</li>		
 			<?php
 			    }
