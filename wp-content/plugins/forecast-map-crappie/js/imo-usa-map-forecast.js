3606ae44eacdc19f33dc2a4e089a2fe9cc722793
Jeff Burrows <jeff.burrows@imoutdoors.com>
2017-02-19 17:52:00 -0500

    New file for G&F crappies forecast plugin

---
+++ wp-content/plugins/forecast-map-crappie/js/imo-usa-map-forecast.js
@@ -0,0 +1,255 @@
+jQuery(document).ready(function () {
+////////////////////////////////////
+
+var colors = new Array();
+
+colors[8] = "#000000";
+colors[7] = "#444444";
+colors[6] = "#444444";
+colors[5] = "#666666";
+colors[4] = "#888888";
+colors[3] = "#aaaaaa";
+colors[2] = "#cccccc";
+colors[1] = "#eeeeee";
+
+var stateAbbrev = new Object();
+stateAbbrev.AL="alabama";
+stateAbbrev.AK="alaska";
+stateAbbrev.AZ="arizona";
+stateAbbrev.AR="arkansas";
+stateAbbrev.CA="california";
+stateAbbrev.CO="colorado";
+stateAbbrev.CT="connecticut";
+stateAbbrev.DE="delaware";
+stateAbbrev.DC="district-of-columbia";
+stateAbbrev.FL="florida";
+stateAbbrev.GA="georgia";
+stateAbbrev.HI="hawaii";
+stateAbbrev.ID="idaho";
+stateAbbrev.IL="illinois";
+stateAbbrev.IN="indiana";
+stateAbbrev.IA="iowa";
+stateAbbrev.KS="kansas";
+stateAbbrev.KY="kentucky";
+stateAbbrev.LA="louisiana";
+stateAbbrev.ME="maine";
+stateAbbrev.MD="maryland";
+stateAbbrev.MA="massachusetts";
+stateAbbrev.MI="michigan";
+stateAbbrev.MN="minnesota";
+stateAbbrev.MS="mississippi";
+stateAbbrev.MO="missouri";
+stateAbbrev.MT="montana";
+stateAbbrev.NE="nebraska";
+stateAbbrev.NV="nevada";
+stateAbbrev.NH="new-hampshire";
+stateAbbrev.NJ="new-jersey";
+stateAbbrev.NM="new-mexico";
+stateAbbrev.NY="new-york";
+stateAbbrev.NC="north-carolina";
+stateAbbrev.ND="north-dakota";
+stateAbbrev.OH="ohio";
+stateAbbrev.OK="oklahoma";
+stateAbbrev.OR="oregon";
+stateAbbrev.PA="pennsylvania";
+stateAbbrev.RI="rhode-island";
+stateAbbrev.SC="south-carolina";
+stateAbbrev.SD="south-dakota";
+stateAbbrev.TN="tennessee";
+stateAbbrev.TX="texas";
+stateAbbrev.UT="utah";
+stateAbbrev.VT="vermont";
+stateAbbrev.VA="virginia";
+stateAbbrev.WA="washington";
+stateAbbrev.WV="west-virginia";
+stateAbbrev.WI="wisconsin";
+stateAbbrev.WY="wyoming";
+
+
+String.prototype.capitalize = function(){
+	return this.replace( /(^|\s)([a-z])/g , function(m,p1,p2){ return p1+p2.toUpperCase(); } );
+};
+
+
+function getMapForContainer(containerNameString) {
+    var R = Raphael(containerNameString),
+	  attr = {
+	  "fill": "#d3d3d3",
+	  "stroke": "#555",
+	  "stroke-opacity": "1",
+	  "stroke-linejoin": "round",
+	  "stroke-miterlimit": "4",
+	  "stroke-width": "0.75",
+	  "stroke-dasharray": "none"
+	  },
+    usRaphael = {};
+    R.setViewBox(0,0,680,680,false);
+    
+    //Draw Map and store Raphael paths
+    for (var state in usMap) {
+		attr.fill = colors[1];
+		usRaphael[state] = R.path(usMap[state]).attr(attr);
+    }
+    
+    //Do Work on Map
+    for (var state in usRaphael) {
+    	if($mobile == true){
+    		usRaphael[state].color = "#eee";
+    	}else{
+	    	usRaphael[state].color = "#FF6600";
+    	}
+       (function (st, state) {
+	       	if(state == "md" || state == "de" || state == "nj" || state == "id" || state == "ut" || state == "mt" || state == "nv"){
+		       	st[0].style.cursor = "cursor";
+		       	st[0].style.opacity = "0.3";
+	       	}else{
+		       	st[0].style.cursor = "pointer";
+			    st[0].onmouseover = function () {
+					st.animate({fill: st.color}, 50);
+					st.toFront();
+					R.safari();
+					jQuery("p.state-name").text(stateAbbrev[state.toUpperCase()].replace("-"," "));
+		       	}
+		        
+			};
+		
+			var $region = stateAbbrev[state.toUpperCase()];
+			if($region == "connecticut"){$region = "new-england"}
+			if($region == "maine"){$region = "new-england"}
+			if($region == "massachusetts"){$region = "new-england"}
+			if($region == "new-hampshire"){$region = "new-england"}
+			if($region == "vermont"){$region = "new-england"}
+			if($region == "rhode-island"){$region = "new-england"}
+			if($region == "kansas"){$region = "great-plains"}
+			if($region == "nebraska"){$region = "great-plains"}
+			if($region == "north-dakota"){$region = "great-plains"}
+			if($region == "south-dakota"){$region = "great-plains"}
+			if($region == "washington"){$region = "washington-oregon"}
+			if($region == "oregon"){$region = "washington-oregon"}
+			if($region == "arizona"){$region = "rocky-mountain"}
+			if($region == "new-mexico"){$region = "rocky-mountain"}
+			if($region == "colorado"){$region = "rocky-mountain"}
+			if($region == "wyoming"){$region = "rocky-mountain"}
+			
+			
+			
+			if(state != "md" && state != "de" && state != "nj" && state != "id" && state != "ut" && state != "mt" && state != "nv"){
+				
+				/*if(jQuery( "select option:selected" ).val() == 2014 ){
+				 	var postURL = "/deer-forecast/" + $region + "-deer-hunting-forecast-2014/";
+				 	jQuery(".page-title span").text("GAME & FISH 2014 DEER FORECAST");
+				 }else{
+				 	var postURL = "/deer-forecast/" + $region + "-deer-forecast-2013/#forecast";
+				 	var trophyPostURL = "/deer-forecast/" + $region + "-trophy-bucks-2013/#forecast";
+				 }*/
+				//map touch vs click mobile/tablet fix
+				
+				if($mobile == true){
+					//console.log("works");
+					
+					jQuery(st[0]).live('touchstart touchend', function(e) {
+						e.stopPropagation();
+				        e.preventDefault();
+				        e.stopImmediatePropagation();
+				        jQuery(this).toggleClass('hover_effect');
+				        if(jQuery( "select option:selected" ).val() == 2014 ){
+				        	window.open(postURL);
+				        }else{
+					        jQuery(".forecast-map .modal p.state-selection").text("Select Your " + stateAbbrev[state.toUpperCase()].replace("-"," ") + " Forecast");
+							jQuery(".forecast-map .modal a.deer-forecast").attr("href",postURL);
+							if(state == "wa" || state == "az"){
+								jQuery(".forecast-map .modal a.trophy-buck, p.delim").hide();
+							}else{
+								jQuery(".forecast-map .modal a.trophy-buck, p.delim").show();
+							}
+							jQuery(".forecast-map .modal a.trophy-buck").attr("href",trophyPostURL);
+							jQuery(".overlay, .forecast-map .modal").show();
+						
+							jQuery(".overlay, .forecast-map .close").live('touchstart touchend', function(e) {
+								jQuery(".overlay, .forecast-map .modal").fadeOut();
+							});
+						}
+						
+					});
+						
+				}else{
+				
+					jQuery(st[0]).click(function() {
+						if(jQuery( "select option:selected" ).val() == 2014 ){
+							window.open(postURL);
+						}else{
+							jQuery(".forecast-map .modal p.state-selection").text("Select Your " + stateAbbrev[state.toUpperCase()].replace("-"," ") + " Forecast");
+							jQuery(".forecast-map .modal a.deer-forecast").attr("href",postURL);
+							if(state == "wa" || state == "az"){
+								jQuery(".forecast-map .modal a.trophy-buck, p.delim").hide();
+							}else{
+								jQuery(".forecast-map .modal a.trophy-buck, p.delim").show();
+							}
+							if(state == "wa" || state == "or"){
+								jQuery(".forecast-map .modal a.deer-forecast").html("Places for Blacktail");
+							}else if(state == "nm" || state == "ca" || state == "az"){
+								jQuery(".forecast-map .modal a.deer-forecast").html("Places for Mule Deer");
+							}else{
+								jQuery(".forecast-map .modal a.deer-forecast").html("Places for Whitetail");
+							}
+							jQuery(".forecast-map .modal a.trophy-buck").attr("href",trophyPostURL);
+							jQuery(".overlay, .forecast-map .modal").fadeIn();
+						}
+					});
+					jQuery(".overlay, .forecast-map .close").click(function() {
+						jQuery(".overlay, .forecast-map .modal").fadeOut();
+					});
+				}
+					
+			
+				st[0].onmouseout = function () {
+			    	colorcode = 1;
+			    	st.animate({fill: colors[colorcode]}, 500);
+					st.toFront();
+					R.safari();
+					jQuery("p.state-name").text("Select Your State");
+				};
+			}           
+		})(usRaphael[state], state);
+	}
+	
+	function getStateData(state) {
+		var $output = jQuery("<div class='popup-state-data'></div>");
+		var report;
+		report = 0;
+		/*if (stateData[state.toUpperCase()]) {
+			currentStateData = stateData[state.toUpperCase()];
+			if (currentStateData.report)
+				report = currentStateData.report;
+			else
+				report = 0;
+		} else {
+			report = 0;
+		}*/
+
+		//$output.find("ul").append("<li>" + report + " Rut Reports</li>");
+		//console.log("$output");
+		//console.log($output);
+
+		$output.prepend("<h2>" + stateAbbrev[state.toUpperCase()].capitalize() + "</h2>");
+
+		return $output;
+	}
+	
+//});
+
+}; //end function getMapForContainer
+
+
+if (jQuery("#us-map-forecast-crappie").length > 0) {
+	
+	jQuery("#us-map-forecast-crappie").load("/wp-content/plugins/forecast-map-crappie/map.html");
+		
+	
+}
+  
+
+
+
+////////////////////////////////////
+});
---
+++ wp-content/plugins/forecast-map-crappie/js/map-interact.js
@@ -0,0 +1,161 @@
+﻿jQuery(document).ready(function () {
+	
+	
+	// Quick feature detection
+	function isTouchEnabled() {
+	 	return (('ontouchstart' in window)
+	      	|| (navigator.MaxTouchPoints > 0)
+	      	|| (navigator.msMaxTouchPoints > 0));
+	}
+	
+	jQuery(function(){
+				addEvent('map_1');
+				addEvent('map_2');
+				addEvent('map_3');
+				addEvent('map_4');
+				addEvent('map_5');
+				addEvent('map_6');
+				addEvent('map_7');
+				addEvent('map_8');
+				addEvent('map_9');
+				addEvent('map_10');
+				addEvent('map_11');
+				addEvent('map_12');
+				addEvent('map_13');
+				addEvent('map_14');
+				addEvent('map_15');
+				addEvent('map_16');
+				addEvent('map_17');
+				addEvent('map_18');
+				addEvent('map_19');
+				addEvent('map_20');
+				addEvent('map_21');
+				addEvent('map_22');
+				addEvent('map_23');
+				addEvent('map_24');
+				addEvent('map_25');
+				addEvent('map_26');
+				addEvent('map_27');
+				addEvent('map_28');
+				addEvent('map_29');
+				addEvent('map_30');
+				addEvent('map_31');
+				addEvent('map_32');
+				addEvent('map_33');
+				addEvent('map_34');
+				addEvent('map_35');
+				addEvent('map_36');
+				addEvent('map_37');
+				addEvent('map_38');
+				addEvent('map_39');
+				addEvent('map_40');
+				addEvent('map_41');
+				addEvent('map_42');
+				addEvent('map_43');
+				addEvent('map_44');
+				addEvent('map_45');
+				addEvent('map_46');
+				addEvent('map_47');
+				addEvent('map_48');
+				addEvent('map_49');
+				addEvent('map_50');
+				addEvent('map_51');
+	})
+	
+	jQuery(function(){
+		//lakes's color and border color
+		if(jQuery('#lakes').find('path').eq(0).attr('fill') != 'undefined'){
+			jQuery('#lakes').find('path').attr({'fill':map_config['default']['lakescolor']}).css({'stroke':map_config['default']['bordercolor']});
+		}
+	
+		//*//configuration for title text's shadow
+		jQuery('.tip').css({
+			'box-shadow':'1px 2px 4px '+map_config['default']['namesShadowColor'],
+			'-moz-box-shadow':'2px 3px 6px '+map_config['default']['namesShadowColor'],
+			'-webkit-box-shadow':'2px 3px 6px '+map_config['default']['namesShadowColor'],
+		});
+	
+		//configuration for map shadow
+		if(jQuery('#shadow').find('path').eq(0).attr('fill') != 'undefined'){
+			var shadowOpacity = map_config['default']['shadowOpacity'];
+			var shadowOpacity = parseInt(shadowOpacity);
+			if (shadowOpacity >=100){shadowOpacity = 1;}else if(shadowOpacity <=0){shadowOpacity =0;}else{shadowOpacity = shadowOpacity/100;}
+			
+			jQuery('#shadow').find('path').attr({'fill':map_config['default']['shadowcolor']}).css({'fill-opacity':shadowOpacity})
+		}
+	});
+	
+	function addEvent(id,relationId){
+			
+		var _obj = jQuery('#'+id);
+		var _Textobj = jQuery('#'+id+','+'#'+map_config[id]['namesId']);
+		var _h = jQuery('#map').height();
+	
+		jQuery('#'+map_config[id]['namesId']).find('tspan').attr({'fill':map_config['default']['namescolor']});
+	
+			_obj.attr({'fill':map_config[id]['upcolor'],'stroke':map_config['default']['bordercolor']});
+			_Textobj.attr({'cursor':'default'});
+			if(map_config[id]['enable'] == true){
+				if (isTouchEnabled()) {
+					//clicking effect
+					_Textobj.on('touchstart', function(e){
+						var touch = e.originalEvent.touches[0];
+						var x=touch.pageX-20, y=touch.pageY+(-38);
+						var tipw=jQuery('#tip').outerWidth(), tiph=jQuery('#tip').outerHeight(), 
+						x=(x+tipw>jQuery(document).scrollLeft()+jQuery(window).width())? x-tipw-(20*2) : x
+						y=(y+tiph>jQuery(document).scrollTop()+jQuery(window).height())? jQuery(document).scrollTop()+jQuery(window).height()-tiph-10 : y
+						jQuery('#'+id).css({'fill':map_config[id]['downcolor']});
+						jQuery('#tip').show().html(map_config[id]['name']);
+						jQuery('#tip').css({left:x, top:y})
+					})
+					_Textobj.on('touchend', function(){
+						jQuery('#'+id).css({'fill':map_config[id]['upcolor']});
+						if(map_config[id]['target'] == '_blank'){
+							if(jQuery(".forecast-map").hasClass("year-2014")){
+								window.open(map_config[id]['url']);	
+							}else if(jQuery(".forecast-map").hasClass("year-2013")){
+								window.open(map_config_2013[id]['url']);	
+							}
+						}else{
+							window.location.href=map_config[id]['url'];
+						}
+						jQuery('#tip').hide();
+					})
+				} else {
+					_Textobj.attr({'cursor':'pointer'});
+					_Textobj.hover(function(){
+						//moving in/out effect
+						jQuery('#tip').show().html(map_config[id]['name']);
+						_obj.css({'fill':map_config[id]['overcolor']})
+					},function(){
+						jQuery('#tip').hide();
+						jQuery('#'+id).css({'fill':map_config[id]['upcolor']});
+					})
+					//clicking effect
+					_Textobj.mousedown(function(){
+						jQuery('#'+id).css({'fill':map_config[id]['downcolor']});
+					})
+					_Textobj.mouseup(function(){
+						jQuery('#'+id).css({'fill':map_config[id]['overcolor']});
+						if(map_config[id]['target'] == '_blank'){
+							if(jQuery(".forecast-map").hasClass("year-2014")){
+								window.open(map_config[id]['url']);	
+							}else if(jQuery(".forecast-map").hasClass("year-2013")){
+								window.open(map_config_2013[id]['url']);
+							}
+							
+						}else{
+							window.location.href=map_config[id]['url'];
+						}
+					})
+					_Textobj.mousemove(function(e){
+						var x=e.pageX-20, y=e.pageY+(-38);
+						var tipw=jQuery('#tip').outerWidth(), tiph=jQuery('#tip').outerHeight(), 
+						x=(x+tipw>jQuery(document).scrollLeft()+jQuery(window).width())? x-tipw-(20*2) : x
+						y=(y+tiph>jQuery(document).scrollTop()+jQuery(window).height())? jQuery(document).scrollTop()+jQuery(window).height()-tiph-10 : y
+						jQuery('#tip').css({left:x, top:y})
+					})
+				}
+			}	
+	}
+});
